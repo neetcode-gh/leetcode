53a4b7b1ade2da2a16c249efdc326217882338c7
neetcode-gh <77742485+neetcode-gh@users.noreply.github.com>
2021-12-25 14:23:42 -0800

    Create 55-Jump-Game.py

---
+++ 55-Jump-Game.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        goal = len(nums) -1
+        
+        for i in range(len(nums) - 2, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+        return True if goal == 0 else False
