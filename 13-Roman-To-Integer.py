ee8458ba74360569ceda97bbfe6988eae825fd50
neetcode-gh <77742485+neetcode-gh@users.noreply.github.com>
2021-12-25 13:42:55 -0800

    Create 13-Roman-To-Integer.py

---
+++ 13-Roman-To-Integer.py
@@ -0,0 +1,11 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = { "I" : 1, "V" : 5, "X" : 10, 
+                    "L" : 50, "C" : 100, "D" : 500, "M" : 1000 }
+        res = 0
+        for i in range(len(s)):
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
+                res -= roman[s[i]]
+            else:
+                res += roman[s[i]]
+        return res
