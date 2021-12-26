5eca389af2c7c4dd2f507715edcee7ae21dd1f4a
neetcode-gh <77742485+neetcode-gh@users.noreply.github.com>
2021-12-26 14:36:14 -0800

    Create 853-Car-Fleet.py

---
+++ 853-Car-Fleet.py
@@ -0,0 +1,10 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pair = [[p, s] for p, s in zip(position, speed)]
+        
+        stack = []
+        for p, s in sorted(pair)[::-1]: # Reverse Sorted Order
+            stack.append((target - p) / s)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)    
