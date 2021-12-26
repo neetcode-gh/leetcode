4e1bd382ac293e24949fba8f4e814e968c86f6a0
neetcode-gh <77742485+neetcode-gh@users.noreply.github.com>
2021-12-25 19:22:46 -0800

    Create 778-Swim-in-Rising-Water.py

---
+++ 778-Swim-in-Rising-Water.py
@@ -0,0 +1,20 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        N = len(grid)
+        visit = set()
+        minH = [[grid[0][0], 0, 0]] # (time/max-height, r, c)
+        directions = [[0, 1], [0, -1], [1, 0], [-1 ,0]]
+        
+        visit.add((0, 0))
+        while minH:
+            t, r, c = heapq.heappop(minH)
+            if r == N - 1 and c == N - 1:
+                return t
+            for dr, dc in directions:
+                neiR, neiC = r + dr, c + dc
+                if (neiR < 0 or neiC < 0 or 
+                    neiR == N or neiC == N or
+                    (neiR, neiC) in visit):
+                    continue
+                visit.add((neiR, neiC))
+                heapq.heappush(minH, [max(t, grid[neiR][neiC]), neiR, neiC])
