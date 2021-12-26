9ace596e1647affef76fd31c7b94aeec59457a2b
neetcode-gh <77742485+neetcode-gh@users.noreply.github.com>
2021-12-25 19:43:00 -0800

    Create 416-Partition-Equal-Subset-Sum.py

---
+++ 416-Partition-Equal-Subset-Sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        if sum(nums) % 2:
+            return False
+        
+        dp = set()
+        dp.add(0)
+        target = sum(nums) // 2
+        
+        for i in range(len(nums) - 1, -1, -1):
+            nextDP = set()
+            for t in dp:
+                if (t + nums[i]) == target:
+                    return True
+                nextDP.add(t + nums[i])
+                nextDP.add(t)
+            dp = nextDP
+        return True if target in dp else False
