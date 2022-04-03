6ccec159ea70fa68e5e172af6f18d7646b5f3bcb
veer-p <102765426+veer-p@users.noreply.github.com>
2022-04-03 11:44:38 -0700

    Create 238-Product-of-Array-Except-Self.js

---
+++ javascript/238-Product-of-Array-Except-Self.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    const res = [];
+    
+    let product = 1;
+    
+    for (let i = 0; i < nums.length; i++) {
+        res[i] = product;
+        product *= nums[i];
+    }
+    product = 1;
+    for (let j = nums.length - 1; j >= 0; j--) {
+        res[j] *= product;
+        product *= nums[j];
+    }
+    
+    return res;
+    
+};
