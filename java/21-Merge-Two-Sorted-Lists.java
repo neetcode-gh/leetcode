4ca5e3b999849e22b98818ca96e68e3a8f645760
Dmitry <lbvf6000@gmail.com>
2022-04-04 00:59:49 +0300

    Added Java 21-Merge-Two-Sorted-Lists

---
+++ java/21-Merge-Two-Sorted-Lists.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        final ListNode root = new ListNode();
+        ListNode prev = root;
+        while (list1 != null && list2 != null) {
+            if(list1.val < list2.val) {
+
+
+                prev.next = list1;
+                list1 = list1.next;
+            } else {
+                prev.next = list2;
+                list2 = list2.next;
+            }
+            prev = prev.next;
+        }
+        prev.next = list1 != null ? list1 : list2;
+        return root.next;
+    }
+}
