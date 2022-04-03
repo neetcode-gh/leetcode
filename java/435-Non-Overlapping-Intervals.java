33e3ac2f917aa49a3da8061e08401fed8a7bf3fd
Peter P <peter.parianos@yahoo.com>
2022-04-03 18:52:30 -0400

    Update 435-Non-Overlapping-Intervals.java

--- java/435-Non-Overlapping-Intervals.java
+++ java/435-Non-Overlapping-Intervals.java
@@ -2,14 +2,10 @@ public class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
 		int intervalsRemoved = 0;
 
-		//sort by the first vale in the intervals least to greatest
 		Arrays.sort(intervals, (arr1,arr2) -> Integer.compare(arr1[0], arr2[0]));
-		System.out.println(Arrays.deepToString(intervals));
 
-		//get the first interval
 		int[] intervalFirst = intervals[0];
 
-		// then loop through the rest of the intervals
 		for(int i = 1; i < intervals.length; i++){
 			if(firstIntervalwithinSecond(intervalFirst, intervals[i])){
 				//mark first interval to be removed
