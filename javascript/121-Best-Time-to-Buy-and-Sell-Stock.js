67e33b60dad2da3f6d4aeac62a703070adc2be22
veer-p <102765426+veer-p@users.noreply.github.com>
2022-04-03 11:52:56 -0700

    Create 121-Best-Time-to-Buy-and-Sell-Stock.js

---
+++ javascript/121-Best-Time-to-Buy-and-Sell-Stock.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let buy = prices[0];
+    let profit = 0; 
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < buy) {
+            buy = prices[i];
+        } else {
+            profit = Math.max(prices[i] - buy, profit);
+        }
+    }
+    return profit;
+};
