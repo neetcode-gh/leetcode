c53118a92859995e4e9d9d26d80e64b04caa07dc
I345718 <rushikesh.joshi@sap.com>
2022-04-03 19:10:17 +0100

    Java Solution for 217 Contains Duplicate

---
+++ java/217-Contains-Duplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> numbers = new HashSet<Integer>();
+        for (int num : nums) {
+            if (numbers.contains(num)) return true;
+            numbers.add(num);
+        }
+        
+        return false;
+    }
+}
