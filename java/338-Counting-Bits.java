6108f4f415fd7dac7de906a21d56ac6c4290c91e
Felix <79116375+FelixRodriguezJr@users.noreply.github.com>
2022-04-04 15:23:57 -0400

    Create 338-Counting-Bits.java

---
+++ java/338-Counting-Bits.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] countBits(int n) {
+        
+        int[] ans = new int[n+1];
+        
+        for(int i = 0; i <= n; i++){
+            ans[i] = count(i);
+        }
+        return ans;
+    }
+    
+    private int count(int x){
+        int count = 0;
+        while(x != 0){
+            x &= x - 1;
+            count++;
+        }
+        return count;
+    }
+}
