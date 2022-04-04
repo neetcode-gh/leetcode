0a5be0cabea561b15deb23a72f9b72ed15756254
Fatih <fatihsen539@gmail.com>
2022-04-04 01:39:42 -0700

    should be in java directory

---
+++ go/NeetCode 150/Arrays & Hashing/217-Contains-Duplicate.go
@@ -0,0 +1,11 @@
+func containsDuplicate(nums []int) bool {
+	freqMap := make(map[int]int)
+
+	for i := 0; i < len(nums); i++ {
+		if _, ok := freqMap[nums[i]]; ok {
+			return true
+		}
+		freqMap[nums[i]] += 1
+	}
+	return false
+}
