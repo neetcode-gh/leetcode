77475089bcf931077c5f60ac5c93301a43679cb2
Thuan Le <gile@ucsc.edu>
2022-04-03 19:23:46 -0700

    Added 11 Container With Most Water and 55 Jump Game

---
+++ java/55-Jump-Game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int goal = nums.length-1;
+        for (int i = nums.length-2; i >= 0; i--) {
+            if (nums[i] + i >= goal) {
+                goal = i;
+            }
+        }
+        return goal == 0;
+    }
+}
+
