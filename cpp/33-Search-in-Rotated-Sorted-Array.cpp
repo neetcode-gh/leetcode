bca63e961b7b8dfffc53a5b400a1cded7d2de239
Md Rana Mahmud <ranamd0@gmail.com>
2022-04-04 17:42:51 +0600

    Time and Space complexity 33 cpp added

--- cpp/33-Search-in-Rotated-Sorted-Array.cpp
+++ cpp/33-Search-in-Rotated-Sorted-Array.cpp
@@ -1,18 +1,19 @@
+// Time complexity: O(log(N))
+// Space complexity: O(1)
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int start = 0, end = nums.size() - 1;
         while(start <= end){
-            int mid = end + (start - end) / 2;
-            int num = nums[mid];
-            if(num == target)
+            int mid = start + (end - start) / 2;
+            if(nums[mid] == target)
                 return mid;
-            else if(nums[start]<= nums[mid]){
-                if(target >= nums[start] and target < nums[mid])
+            else if(nums[mid] >= nums[start]){
+                if(target >= nums[start] && target < nums[mid])
                     end = mid - 1;
                 else
                     start = mid + 1;
-
             }else{
                 if(target > nums[mid] && target <= nums[end])
                     start = mid + 1;
