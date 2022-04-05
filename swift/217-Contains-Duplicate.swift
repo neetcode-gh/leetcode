b7b5b95ec3605f2f15f80bb8fb73b36997a8c676
chandra9302 <39931486+chandra9302@users.noreply.github.com>
2022-04-04 21:32:59 -0700

    Update 217-Contains-Duplicate.swift

--- swift/217-Contains-Duplicate.swift
+++ swift/217-Contains-Duplicate.swift
@@ -1,4 +1,8 @@
-class Solution {
+/**
+ * Question Link: https://leetcode.com/problems/contains-duplicate/
+ */
+
+class ContainsDuplicate {
     func containsDuplicate(_ nums: [Int]) -> Bool {
         var hashSet = Set<Int>()
         for n in nums {
