97a3877f853fd5c9bc411ce9069517fe70bef9ef
Bala2it4u <bala.effie@gmail.com>
2022-04-06 17:59:06 +0530

    Delete 217. Contains Duplicate.java

--- java/217. Contains Duplicate.java
+++
@@ -1,8 +0,0 @@
-public boolean containsDuplicate(int[] nums) {
-    Set<Integer> set = new HashSet<>(nums.length);
-    for (int x: nums) {
-        if (set.contains(x)) return true;
-        set.add(x);
-    }
-    return false;
-}
