47b598177d9c0bf0b0d7158e42124ab6885a513d
Omid Rajabi <afghan.tech.bro@gmail.com>
2022-04-07 20:04:55 -0400

    Update 217-Contains-Duplicate.py

--- 217-Contains-Duplicate.py
+++ 217-Contains-Duplicate.py
@@ -1,9 +1,3 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        hashset = set()
-        
-        for n in nums:
-            if n in hashset:
-                return True
-            hashset.add(n)
-        return False
+        return len(nums) > len(set(nums))
