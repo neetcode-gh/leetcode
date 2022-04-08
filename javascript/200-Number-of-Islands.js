a39bddc8b58cb79962417516eaef3aad177059ef
Anthony Sim <anthonyysim@gmail.com>
2022-04-07 19:04:59 -0700

    added alternate version

--- javascript/200-Number-of-Islands.js
+++ javascript/200-Number-of-Islands.js
@@ -1,3 +1,4 @@
+// Version 1
 function explore(grid, r, c, visited) {
   const rowInbounds = 0 <= r && r < grid.length;
   const colInbounds = 0 <= c && c < grid[0].length;
@@ -18,7 +19,6 @@ function explore(grid, r, c, visited) {
   return true;
 }
 
-
 var numIslands = function (grid) {
   const visited = new Set();
   let count = 0;
@@ -27,6 +27,33 @@ var numIslands = function (grid) {
     for (let c = 0; c < grid[0].length; c++) {
       if (explore(grid, r, c, visited)) {
         count += 1;
+      }
+    }
+  }
+  return count;
+};
+
+// Version 2 (Easier to understand/read)
+function dfs(grid, i, j) {
+  if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] === "0") {
+    return;
+  }
+
+  grid[i][j] = "0";
+  dfs(grid, i + 1, j);
+  dfs(grid, i - 1, j);
+  dfs(grid, i, j + 1);
+  dfs(grid, i, j - 1);
+}
+
+var numIslands = function (grid) {
+  let count = 0;
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === "1") {
+        count += 1;
+        dfs(grid, i, j);
       }
     }
   }
