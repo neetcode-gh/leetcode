1836e5f8215f612b783544e131eed8e55930ec60
BHARAT0310 <55924751+BHARAT0310@users.noreply.github.com>
2022-04-12 15:07:32 +0530

    Update 49-Group-Anagrams.py
    Run time and Memory usage of the above solution is.
    R.T = 140ms
    M.U = 18.4MB

--- 49-Group-Anagrams.py
+++ 49-Group-Anagrams.py
@@ -1,10 +1,7 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans = collections.defaultdict(list)
-        
+        hashmap = defaultdict(list)
         for s in strs:
-            count = [0] * 26
-            for c in s:
-                count[ord(c) - ord('a')] += 1
-            ans[tuple(count)].append(s)
-        return ans.values()
+            # keys can be strings, bcz they are immutable.
+            hashmap[str(sorted(s))].append(s) 
+        return hashmap.values()
