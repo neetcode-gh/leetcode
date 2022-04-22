876646424f5edd27b0bc607535bc52b04007491e
Brendon <bserrano1@outlook.com>
2022-04-22 00:31:21 -0400

    Array Some Set Solution

--- javascript/217-Contains-Duplicate.js
+++ javascript/217-Contains-Duplicate.js
@@ -2,15 +2,12 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function(nums) {
-    let map = {};
-    
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] in map) {
+ var containsDuplicate = function(nums) {
+    let set = new Set();
+    return nums.some(num => {
+        if(set.has(num))
             return true;
-        } else {
-            map[nums[i]] = i;
-        }
-    }
-    return false;
+        set.add(num);
+    });
+    
 };
