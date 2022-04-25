2e58375d057c7dc55936128bbd4f8a2aab6a140c
Adesh Nalpet Adimurthy <390.adesh@gmail.com>
2022-04-25 18:20:39 -0300

    Handle edge cases - Jump game
    A fix to handle the input [1]

--- java/55-Jump-Game.java
+++ java/55-Jump-Game.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int goal = nums.length-1;
-        for (int i = nums.length-2; i >= 0; i--) {
+        for (int i = nums.length-1; i >= 0; i--) {
             if (nums[i] + i >= goal) {
                 goal = i;
             }
