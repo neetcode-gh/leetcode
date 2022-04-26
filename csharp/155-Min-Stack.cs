3521dd723c2e84c15b599feb33616e869c3c8c3c
Jori <joriguzman@yahoo.com>
2022-04-26 23:00:34 +0800

    155-Min Stack, 704-Binary Search

---
+++ csharp/155-Min-Stack.cs
@@ -0,0 +1,28 @@
+public class MinStack {
+    private Stack<int> stack;
+    private Stack<int> minStack;
+
+    public MinStack() {
+        stack = new Stack<int>();
+        minStack = new Stack<int>();
+    }
+    
+    public void Push(int val) {
+        stack.Push(val);
+        int min = Math.Min(val, minStack.Count != 0 ? minStack.Peek() : val);
+        minStack.Push(min);
+    }
+    
+    public void Pop() {
+        stack.Pop();
+        minStack.Pop();
+    }
+    
+    public int Top() {
+        return stack.Peek();
+    }
+    
+    public int GetMin() {
+        return minStack.Peek();
+    }
+}
