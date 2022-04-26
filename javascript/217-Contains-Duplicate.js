9f90d6557ddb300d3d2982e9a4fa58cb03e72346
qasimala <58895064+qasimala@users.noreply.github.com>
2022-04-27 00:13:42 +0200

    Added two additional methods - 217 Contains Duplicates

--- javascript/217-Contains-Duplicate.js
+++ javascript/217-Contains-Duplicate.js
@@ -2,6 +2,8 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+
+//First method using Map() (exit early if true)
 var containsDuplicate = function(nums) {
     let map = {};
     
@@ -14,3 +16,23 @@ var containsDuplicate = function(nums) {
     }
     return false;
 };
+
+
+/*
+//Second method using Map() (Has to map entire array but code is more readable)
+
+const containsDuplicate = function(nums) {
+    //create a new hashmap with all the items in the array. Any duplicates will be removed.
+    let totalWithoutDuplicates = new Map(nums.map((i) => [i]));
+    
+    //check if the size of the initial array is larger than the new hashmap. 
+    return totalWithoutDuplicates.size !== nums.length;
+};
+
+//Third method using Set() (Fastest runtime at 91.95% and very readable code)
+
+var containsDuplicate = function(nums) {
+    //Pass the array into a Set() (which removes duplicates) and then compare its size to the original array. 
+    return new Set(nums).size !== nums.length;
+}
+*/
