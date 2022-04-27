51fcf965da99be1289470a99f3e77889b6b2ab43
William Leung <leungwk25@gmail.com>
2022-04-26 17:16:24 -0700

    Create 102-Binary-Tree-Level-Order-Traversal.js

---
+++ javascript/102-Binary-Tree-Level-Order-Traversal.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    
+    const result = [];
+    const queue = [root];
+    
+    while(queue.length){
+        const numNodes = queue.length;
+        const temp = [];
+        for(let i = 0; i < numNodes; i++){
+            const subtree = queue.shift();
+            temp.push(subtree.val)
+            if(subtree.left !== null) queue.push(subtree.left); 
+            if(subtree.right !== null) queue.push(subtree.right);
+        }
+        result.push(temp)
+    }
+    
+    return result;
+};
