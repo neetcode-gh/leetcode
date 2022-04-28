665a4cb975b9608f6ef6d793397d65fc2e06f7b9
redar98 <redar998@gmail.com>
2022-04-28 15:32:01 +0300

    Update 242-Valid-Anagram for Java
    Slightly more performant version of the code

--- java/242-Valid-Anagram.java
+++ java/242-Valid-Anagram.java
@@ -1,20 +1,13 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        
-        if(s.length() != t.length()) return false;
-        if(s.equals(t)) return true;
-        
-        Map<Character, Integer> sMap = new HashMap<>();
-        Map<Character, Integer> tMap = new HashMap<>();
-        
-        for(int i = 0; i < s.length(); i++) {
-            sMap.merge(s.charAt(i), 1, Integer::sum);
-            tMap.merge(t.charAt(i), 1, Integer::sum);
-        }
-        
-        for(Character c : sMap.keySet()) {
-            if(!sMap.get(c).equals(tMap.get(c))) return false;
-        }
-        return true;
+        Map<Character, Integer> sm = new HashMap<>();
+        Map<Character, Integer> tm = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++)
+            sm.compute(s.charAt(i), (k, v) -> v == null ? 1 : v + 1);
+        for (int i = 0; i < t.length(); i++)
+            tm.compute(t.charAt(i), (k, v) -> v == null ? 1 : v + 1);
+
+        return sm.equals(tm);
     }
 }
