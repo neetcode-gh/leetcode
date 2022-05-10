df90bd64a43e02f9e6ad63f6deaa22da8c654afb
basigu <basi_gu@hotmail.com>
2022-05-09 20:02:06 -0700

    Problem# 271 and 128

---
+++ csharp/128-Longest-Consecutive-Sequence.cs
@@ -0,0 +1,28 @@
+﻿public class Solution12
+{
+    public int LongestConsecutive(int[] nums)
+    {
+        var numSet = new HashSet<int>();
+        var longest = 0;
+
+        foreach(var num in nums)
+        {
+            numSet.Add(num);
+        }
+
+        foreach(var num in nums)
+        {
+            var length = 0;
+            if (!numSet.Contains(num - 1))
+            {
+                while (numSet.Contains(num + length))
+                {
+                    length += 1;
+                }
+
+                longest = Math.Max(longest, length);
+            }
+        }
+        return longest;
+    }
+}
---
+++ csharp/271-Encode-Decode-Strings.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace AlgoPractice
+{
+	public class Solution
+    {
+
+        // Encodes a list of strings to a single string.
+        public string Encode(IList<string> strs)
+        {
+            var result = string.Empty;
+            foreach(var str in strs)
+            {
+                result += $"{str.Length}#{str}";
+            }
+            return result;
+        }
+
+        // Decodes a single string to a list of strings.
+        public IList<string> Decode(string s)
+        {
+            IList<string> result = new List<string>();
+            var i = 0;
+            while(i < s.Length)
+            {
+                var j = i;
+                while(s[j] != '#')
+                {
+                    j++;
+                }
+
+                var numStr = s.Substring(i, j - i);
+                var num = int.Parse(numStr);
+
+                var word = s.Substring(j+1, num);
+                result.Add(word);
+
+                i = j + 1 + num;
+            }
+            return result;
+        }
+
+    }
+}
+
