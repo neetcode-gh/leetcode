f1ddfb4db60b413f593abd12ac92e875d23d1f65
sricharanlochan <sricharan.lochan@gmail.com>
2022-05-11 20:42:55 +0100

    Create 22-Generate-Paranthesis.java

---
+++ java/22-Generate-Paranthesis.java
@@ -0,0 +1,27 @@
+class GenerateParentheses {
+    public List<String> generateParentheses(int n) {
+        List<String> result = new ArrayList<>();
+        Stack<String> stack = new Stack<>();
+        
+        backtrack(0, 0, n, result, stack);
+        return result;
+    }
+    
+    public void backtrack(int open, int closed, int n, List<String> result, Stack<String> stack) {
+        if (open == n && closed == n) {
+            result.add(stack.stream().collect(Collectors.joining("")));
+        }
+        
+        if (open < n) {
+            stack.push("(");
+            backtrack(open + 1, closed, n, result, stack);
+            stack.pop();
+        }
+        
+        if (closed < open) {
+            stack.push(")");
+            backtrack(open, closed + 1, n, result, stack);
+            stack.pop();
+        }
+    }
+}
