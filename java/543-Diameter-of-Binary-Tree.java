e2b576fb7c0c4366351bae484187ae625d163eda
Chirag M Deekshith <chiragmdeekshith@gmail.com>
2022-05-11 22:30:54 +0530

    Diameter of Binary Tree Java code doesn't work
    The code did not work for test case 43, so the submission failed. Probably because of how the static variable and static functions work.
    I've updated the code to suit the video explanation better, and removed "static" keywords.

--- java/543-Diameter-of-Binary-Tree.java
+++ java/543-Diameter-of-Binary-Tree.java
@@ -1,32 +1,18 @@
 class Solution {
-      private static int treeDiameter = 0;
+    int result = -1;
 
     public int diameterOfBinaryTree(TreeNode root) {
-   calculateHeight(root);
-    return treeDiameter-1;
-  }
-
-  private static int calculateHeight(TreeNode currentNode) {
-    if (currentNode == null)
-      return 0;
-
-    int leftTreeHeight = calculateHeight(currentNode.left);
-    int rightTreeHeight = calculateHeight(currentNode.right);
-
-    // if the current node doesn't have a left or right subtree, we can't have
-    // a path passing through it, since we need a leaf node on each side
-    if (leftTreeHeight != 0 && rightTreeHeight != 0) {
-
-      // diameter at the current node will be equal to the height of left subtree +
-      // the height of right sub-trees + '1' for the current node
-      int diameter = leftTreeHeight + rightTreeHeight + 1;
-
-      // update the global tree diameter
-      treeDiameter = Math.max(treeDiameter, diameter);
+        dfs(root);
+        return result;
     }
 
-    // height of the current node will be equal to the maximum of the heights of
-    // left or right subtrees plus '1' for the current node
-    return Math.max(leftTreeHeight, rightTreeHeight) + 1;
-  }
+    private int dfs(TreeNode current) {
+        if (current == null) {
+            return -1;
+        }
+        int left = 1 + dfs(current.left);
+        int right = 1 + dfs(current.right);
+        result = Math.max(result, (left + right));
+        return Math.max(left, right);
+    }
 }
