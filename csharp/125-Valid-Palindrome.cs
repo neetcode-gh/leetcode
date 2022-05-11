b2fe51640fb0b8e08dd68d0616515c5cc3cd0bf6
basigu <basi_gu@hotmail.com>
2022-05-10 21:00:49 -0700

    Added solution for 125 Valid Palindrome

---
+++ csharp/125-Valid-Palindrome.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace AlgoPractice
+{
+	public class Solution
+	{
+		public bool IsPalindrome(string s)
+		{
+			var left = 0;
+			var right = s.Length - 1;
+
+			while(left < right)
+            {
+				while (left < right && !char.IsLetterOrDigit(s[left])) left++;
+				while (left < right && !char.IsLetterOrDigit(s[right])) right--;
+
+				if (char.ToLower(s[left]) != char.ToLower(s[right]))
+				{
+					return false;
+				}
+				else
+				{
+					left++;
+					right--;
+				}
+                
+            }
+			return true;
+		}
+	}
+}
+
