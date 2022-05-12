f34eb1ea87f9f35dc5eccf9e1dd28f92ac25eb93
basigu <basi_gu@hotmail.com>
2022-05-11 20:47:04 -0700

    Problem #167 and 15 solutions with two pointers

---
+++ csharp/15-3-Sum.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace AlgoPractice
+{
+	public class Solution
+	{
+		public IList<IList<int>> ThreeSum(int[] nums)
+		{
+			Array.Sort(nums);
+
+			IList<IList<int>> triplet = new List<IList<int>>();
+
+			for(var i = 0; i < nums.Length; i++)
+            {
+				if(i > 0 && nums[i] == nums[i - 1])
+                {
+					continue;
+                }
+				Search(-nums[i], i, nums.Length - 1, nums, triplet);
+            }
+			return triplet;
+		}
+
+		private void Search(int target, int left, int right, int[] nums, IList<IList<int>> triplet)
+        {
+			while(left < right)
+			{
+				var sum = nums[left] + nums[right];
+				if(sum == target)
+                {
+					triplet.Add(new List<int> { -target, nums[left], nums[right] });
+					left++;
+					right--;
+
+					while (left < right && nums[left] == nums[left - 1])
+					{
+						left++;
+					}
+
+					while (left < right && nums[right] == nums[right + 1])
+					{
+						right--;
+					}
+				}
+				else if(sum < target)
+                {
+					left++;
+					
+                }
+				else
+                {
+					right--;
+                }
+			}
+        }
+	}
+}
+
---
+++ csharp/167-Two-Sum-II.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace AlgoPractice
+{
+	public class Solution17
+	{
+		public int[] TwoSum(int[] numbers, int target)
+		{
+			var left = 0;
+			var right = numbers.Length - 1;
+
+			while(left < right)
+            {
+				if(numbers[left] + numbers[right] == target)
+                {
+					return new int[] { left + 1, right + 1 };
+                }
+				else if(numbers[left] + numbers[right] < target)
+                {
+					left++;
+                }
+                else
+                {
+					right--;
+                }
+            }
+			return new int[] { -1, -1 };
+		}
+	}
+}
+
