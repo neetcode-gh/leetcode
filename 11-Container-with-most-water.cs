3fe1f13f1dbef6b5765c268a66a54f5c613925e0
basigu <basi_gu@hotmail.com>
2022-05-12 20:47:21 -0700

    #11(Container with water) and 42(Trapping rain)

---
+++ 11-Container-with-most-water.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace AlgoPractice
+{
+	public class Solution20
+	{
+		public int MaxArea(int[] height)
+        {
+			var left = 0;
+			var right = height.Length - 1;
+			var maxArea = 0;
+
+			while(left < right)
+            {
+				var area = (right - left) * Math.Min(height[left], height[right]);
+				maxArea = Math.Max(maxArea, area);
+
+				if(height[left] < height[right])
+                {
+					left++;
+                }
+				else
+                {
+					right--;
+                }
+            }
+
+			return maxArea;
+        }
+	}
+}
+
---
+++ 42-Trapping-Rain-Water.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace AlgoPractice
+{
+	public class Solution
+	{
+		public int Trap(int[] height)
+		{
+			var left = 0;
+			var right = height.Length - 1;
+
+			var maxLeft = height[left];
+			var maxRight = height[right];
+
+			var result = 0;
+
+			while(left < right)
+            {
+				if(maxLeft < maxRight)
+                {
+					left++;
+					var currentResult = maxLeft - height[left];
+					if(currentResult > 0)
+					{
+						result += currentResult;
+					}
+					maxLeft = Math.Max(maxLeft, height[left]);
+                }
+                else
+                {
+					right--;
+					var currentResult = maxRight - height[right];
+					if(currentResult > 0)
+                    {
+						result += currentResult;
+                    }
+					maxRight = Math.Max(maxRight, height[right]);
+                }
+
+            }
+			return result;
+		}
+	}
+}
+
