0beeecd637062021ca6ad805ddc6cd020b3daa0a
basigu <basi_gu@hotmail.com>
2022-05-15 16:02:23 -0700

    198 and 213 house robber.

---
+++ 198-House-Robber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace AlgoPractice
+{
+	public class Solution
+	{
+		public int Rob(int[] nums)
+		{
+			if (nums == null || nums.Length == 0)
+			{
+				return 0;
+			}
+			else if (nums.Length == 1)
+			{
+				return nums[0];
+			}
+
+			var rob1 = 0;
+			var rob2 = 0;
+			foreach (var n in nums)
+			{
+				var temp = Math.Max(rob1 + n, rob2);
+				rob1 = rob2;
+				rob2 = temp;
+			}
+			return rob2;
+		}
+	}
+}
+
---
+++ 213-House-Robber-II.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace AlgoPractice
+{
+	public class Solution
+	{
+		public int Rob(int[] nums)
+		{
+			var startFromHouse1 = RobHelper(nums, 0, nums.Length - 2);
+			var startFromHouse2 = RobHelper(nums, 1, nums.Length - 1);
+			var result = Math.Max(startFromHouse1, startFromHouse2);
+			return result;
+		}
+
+		public int RobHelper(int[] num, int start, int end)
+        {
+			var rob1 = 0;
+			var rob2 = 0;
+			for(var i = start; i<= end; i++)
+            {
+				var temp = Math.Max(rob1 + num[i], rob2);
+				rob1 = rob2;
+				rob2 = temp;
+            }
+			return rob2;
+        }
+	}
+}
+
