2eb4f3ac9326a4b53e9c9f079c192371561a7e26
basigu <basi_gu@hotmail.com>
2022-05-15 16:51:41 -0700

    121 best time to buy stock

---
+++ 121-Best-Time-Stock.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace AlgoPractice
+{
+	public class Solution
+	{
+		public int MaxProfit(int[] prices)
+		{
+			var maxProfit = 0;
+
+			var left = 0;
+			var right = 1;
+
+			while (right < prices.Length)
+            {
+				var profit = prices[right] - prices[left];
+				if(profit > 0)
+                {
+					maxProfit = Math.Max(profit, maxProfit);
+                }
+				else
+                {
+					left = right;
+                }
+				right++;
+            }
+
+			return maxProfit;
+		}
+	}
+}
+
