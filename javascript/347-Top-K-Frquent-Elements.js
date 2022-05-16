b8b1dabfd532af1798a7172cdc2773773cb2069e
Mohamed Refaay <69407428+Mohmed-Refaay@users.noreply.github.com>
2022-05-17 00:39:16 +0200

    Update 347-Top-K-Frquent-Elements.js
    Shorted solution and takes less memory

--- javascript/347-Top-K-Frquent-Elements.js
+++ javascript/347-Top-K-Frquent-Elements.js
@@ -4,31 +4,12 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-    let map = new Map();
-    let res = [];
-    let bucket = new Array(nums.length+1);
+    const counter = {}
     
-    // storing frequency of numbers in a map
-    for (let n of nums) {
-        map.set(n, (map.has(n) ? 1 + map.get(n) : 1))
-    }
-
-    // Poppulate the bucket with numbers in frequency
-    // as the index of the bucket
-    for (let [key,value] of map.entries()) {
-        if (!Array.isArray(bucket[value])) {
-            bucket[value] = [];
-        }
-        bucket[value].push(key);
+    for(let i = 0; i < nums.length; i++){
+        counter[nums[i]] = 1 + (counter[nums[i]] || 0)
+        
     }
     
-    for (let i = bucket.length-1; i >= 0; i--) {
-        if (Array.isArray(bucket[i])) {
-            for (let n of bucket[i]) {
-                res.push(n);
-                if (res.length === k)
-                    return res;
-            }
-        }
-    }
+    return(Object.keys(counter).sort((a,b) => counter[b] - counter[a]).slice(0, k))
 };
