1ceb60fa27817a3e60e80536d8e16e5b260caa9c
basigu <basi_gu@hotmail.com>
2022-05-20 19:47:29 -0700

    #3 Longest substring

---
+++ 3-Longest-Substring.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace AlgoPractice
+{
+	public class Solution
+	{
+		public int LengthOfLongestSubstring(string s)
+		{
+			var set = new HashSet<char>();
+			var maxLength = 0;
+			var left = 0;
+			var right = 0;
+			while(right < s.Length)
+            {
+				var currentChar = s[right];
+				while (set.Contains(currentChar) && left < s.Length)
+				{
+					set.Remove(s[left++]);
+				}
+				set.Add(currentChar);
+				right++;
+				var length = right - left;
+				maxLength = Math.Max(length, maxLength);
+			}
+			return maxLength;
+		}
+	}
+}
+
