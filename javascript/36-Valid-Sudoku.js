db1b2759e03661163db3297cc6da3d272e827699
Adam Smith <imagineadamsmith@gmail.com>
2022-05-30 21:01:38 -0700

    Minor formatting updates to JS for problem 36

--- javascript/36-Valid-Sudoku.js
+++ javascript/36-Valid-Sudoku.js
@@ -2,33 +2,40 @@
  * @param {character[][]} board
  * @return {boolean}
  */
-var isValidSudoku = function (board) {
+function isValidSudoku(board) {
     const rows = {};
     const cols = {};
     const squares = {};
 
     for (let r = 0; r < 9; r++) {
         for (let c = 0; c < 9; c++) {
+            const num = board[r][c];
 
-            if (board[r][c] == '.') continue;
+            if (num === '.') {
+                continue;
+            }
 
-            const grid = ((r / 3) >> 0) + '' + ((c / 3) >> 0);
+            const grid = `${Math.floor(r / 3)}${Math.floor(c / 3)}`;
 
-            if (!cols[c]) cols[c] = new Set();
-            if (!rows[r]) rows[r] = new Set();
-            if (!squares[grid]) squares[grid] = new Set();
+            if (!cols[c]) {
+                cols[c] = new Set();
+            }
+            if (!rows[r]) {
+                rows[r] = new Set();
+            }
+            if (!squares[grid]) {
+                squares[grid] = new Set();
+            }
 
-            if (rows[r].has(board[r][c]) || cols[c].has(board[r][c]) || squares[grid].has(board[r][c])) {
+            if (rows[r].has(num) || cols[c].has(num) || squares[grid].has(num)) {
                 return false;
             }
 
-            cols[c].add(board[r][c])
-            rows[r].add(board[r][c])
-            squares[grid].add(board[r][c])
+            cols[c].add(num)
+            rows[r].add(num)
+            squares[grid].add(num)
         }
     }
 
     return true;
-
-};
-
+}
