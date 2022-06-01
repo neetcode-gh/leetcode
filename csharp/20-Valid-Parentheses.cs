5bec4b6cf1ed30316808418aae7c1c93730b9a0c
basigu <basi_gu@hotmail.com>
2022-05-31 20:15:59 -0700

    20 Valid Parentheses

---
+++ csharp/20-Valid-Parentheses.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace AlgoPractice
+{
+	public class Solution
+	{
+		public bool IsValid(string s)
+		{
+			var map = new Dictionary<char, char>
+			{
+				[')'] = '(',
+				['}'] = '{',
+				[']'] = '['
+			};
+
+			var stack = new Stack<char>();
+			foreach(char c in s)
+            {
+                if (map.ContainsKey(c))
+                {
+					var matchingPar = stack.Count == 0 ? '#' : stack.Pop();
+					if(map[c] != matchingPar)
+                    {
+						return false;
+                    }
+                }
+                else
+                {
+					stack.Push(c);
+                }
+            }
+			return stack.Count == 0;
+		}
+	}
+}
+
