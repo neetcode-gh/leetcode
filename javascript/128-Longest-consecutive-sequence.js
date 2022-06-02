6a1040d79b8bd4f169d8b6b39622402b4d173b09
Adam Smith <imagineadamsmith@gmail.com>
2022-06-01 18:11:54 -0700

    Added 2nd JS solution for problem 128

--- javascript/128-Longest-consecutive-sequence.js
+++ javascript/128-Longest-consecutive-sequence.js
@@ -1,4 +1,55 @@
 //////////////////////////////////////////////////////////////////////////////
+// Linear Search With A Hash Map
+// Time: O(n)
+// Space: O(n)
+// This solution only makes one pass over the `nums` array and is the highest
+// performing solution.
+//////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function longestConsecutive(nums) {
+   
+   if (!nums.length) {
+       return 0;
+   }
+   
+   const map = Object.create(null);
+   let max = 0;
+   
+   for (const num of nums) {
+       
+       if (num in map) {
+	   continue;
+       }
+       
+       const prev = num - 1;
+       const next = num + 1;
+       let len = 1;
+       
+       if (prev in map) {
+	   if (next in map) {
+	       len += map[prev] + map[next];
+	       map[prev - map[prev] + 1] = len;
+	       map[next + map[next] - 1] = len;
+	   } else {
+	       len += map[prev];
+	       ++map[prev - map[prev] + 1];
+	   }
+       } else if (next in map) {
+	   len += map[next];
+	   ++map[next + map[next] - 1];
+       }
+       map[num] = len;
+       max = Math.max(max, len);
+   }
+   
+   return max;
+}
+
+//////////////////////////////////////////////////////////////////////////////
 // Linear Search With A Hash Set
 // Time: O(n)
 // Space: O(n)
