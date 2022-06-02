83fc2ee6d5db93e65affe6feac96ff1d4ae3c292
Adam Smith <imagineadamsmith@gmail.com>
2022-06-01 20:42:23 -0700

    Added 2nd JS solution to problem 146

--- javascript/146-LRU-Cache.js
+++ javascript/146-LRU-Cache.js
@@ -44,3 +44,115 @@ LRUCache.prototype.put = function (key, value) {
   }
   this.cacheMap.set(key, value);
 };
+
+//////////////////////////////////////////////////////////////////////////////
+// JavaScript Object Class Implementation
+// This solution requires more code and space due to the need to separately
+// define a doubly linked list and a hash map, but has better performance due
+// to strictly maintaining constant time lookups and additions.
+//////////////////////////////////////////////////////////////////////////////
+
+class LRUNode {
+    /**
+     * @param {number} key
+     * @param {number} val
+     * @param {LRUNode=} next = `null`
+     * @constructor
+     */
+    constructor(key, val, next = null) {
+        this.key = key;
+        this.val = val;
+        this.prev = null;
+        this.next = next;
+    }
+}
+
+class LRUCache {
+    /**
+     * @param {number} capacity
+     * @constructor
+     */
+    constructor(capacity) {
+        this.head = null;
+        this.tail = null;
+        this.map = Object.create(null);
+        this.length = 0;
+        this.capacity = capacity;
+    }
+    
+    /**
+     * @param {number} key
+     * @return {number}
+     */
+    get(key) {
+        if (!(key in this.map)) {
+            return -1;
+        }
+        this.makeMostRecent(key);
+        return this.map[key].val;
+    }
+    
+    /**
+     * @param {number} key 
+     * @param {number} val
+     * @return {void}
+     */
+    put(key, val) {
+        if (key in this.map) {
+            this.map[key].val = val;
+            this.makeMostRecent(key);
+            return;
+        }
+        
+        if (this.length === this.capacity) {
+            delete this.map[this.tail.key];
+            if (this.head === this.tail) {
+                this.head = null;
+                this.tail = null;
+            } else {
+                this.tail = this.tail.prev;
+                this.tail.next = null;
+            }
+        } else {
+            ++this.length;
+        }
+        
+        const node = new LRUNode(key, val, this.head);
+        
+        if (this.head) {
+            this.head.prev = node;
+        } else {
+            this.tail = node;
+        }
+        this.head = node;
+        
+        this.map[key] = node;
+    }
+    
+    /**
+     * @param {number} key
+     * @return {void}
+     */
+    makeMostRecent(key) {
+        const node = this.map[key];
+        
+        if (node === this.head) {
+            return node.val;
+        }
+        
+        if (node.prev) {
+            node.prev.next = node.next;
+        }
+        if (node.next) {
+            node.next.prev = node.prev;
+        }
+        if (node === this.tail) {
+            this.tail = node.prev;
+        }
+        
+        node.prev = null;
+        node.next = this.head;
+        this.head.prev = node;
+        this.head = node;
+    }
+}
