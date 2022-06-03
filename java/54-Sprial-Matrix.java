87a1c3d86a0926ed3f82ad44a25cba0a9e24375b
aregawihalefom <aregawihalefom90@gmail.com>
2022-06-03 09:59:52 -0500

    Create 54-Spiral-matrix.java

---
+++ java/54-Sprial-Matrix.java
@@ -0,0 +1,48 @@
+package java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+
+        int startRow = 0;
+        int startCol = 0 ;
+        int endRow = matrix.length - 1;
+        int endCol = matrix[0].length-1;
+
+
+        List<Integer> result = new ArrayList<>();
+
+        // loop until there are element
+        while(startRow <= endRow && startCol <= endCol){
+
+            // print top row - Traverse horizontal
+            for(int i = startCol; i <= endCol ;i++){
+                result.add(matrix[startRow][i]);
+            }
+            startRow++;
+
+            // Traverse Down -- vertical
+            for(int i = startRow ; i <= endRow ; i++){
+                result.add(matrix[i][endCol]);
+            }
+            endCol--;
+            if(startRow > endRow || startCol > endCol) break;
+
+            // Traverse bottom backwards
+            for(int i = endCol; i >=startCol ; i-- ){
+                result.add(matrix[endRow][i]);
+            }
+            endRow--;
+
+            // traverse up
+            for(int i = endRow ; i >= startRow ; i--){
+                result.add(matrix[i][startCol]);
+            }
+            startCol++;
+        }
+
+        return result;
+    }
+}
