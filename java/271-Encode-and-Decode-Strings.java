9e2943b968592488085b2cddc79765248f4e9378
Anu Shibin  J <anushibin007@gmail.com>
2022-06-07 21:39:08 +0530

    Use the append method as much as possible
    Using string concatenation is not advisable when we are already using a `StringBuilder` to construct the encoded string. We can easily use the `append` method to append the "#" symbol.

--- java/271-Encode-and-Decode-Strings.java
+++ java/271-Encode-and-Decode-Strings.java
@@ -4,7 +4,8 @@ public class Solution {
         StringBuilder encodedString = new StringBuilder();
         for(String str: strs){
             int length = str.length();
-            encodedString.append(length+"#");
+            encodedString.append(length);
+            encodedString.append("#");
             encodedString.append(str);
         }
         return encodedString.toString();
