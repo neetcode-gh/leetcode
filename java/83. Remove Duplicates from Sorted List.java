00db47c5a14b54c5860f4090bd5e956cc283f0d8
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-06-14 18:00:12 +0530

    Create 83. Remove Duplicates from Sorted List.java

---
+++ java/83. Remove Duplicates from Sorted List.java
@@ -0,0 +1,20 @@
+//Three pointer approach that we use in in-place reversal of linked list.
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode p = null;
+        ListNode q = null;
+        ListNode r = head;
+        while (r!=null) {
+            if (q!=null && q.val == r.val) {
+                r = r.next;
+                q.next = r;
+            }else {
+                p = q;
+                q = r;
+                r = r.next;
+            }
+        }
+        return head;
+    }
+}
