feb48ee85840ca189405591ede52efa5191b4d31
2wheelcode <bentleyhamm@gmail.com>
2022-06-16 21:09:45 -0600

    Cleaned up my mess. I missed LeetCode num system

--- javascript/125-ValidPalindrome.js
+++ javascript/125-ValidPalindrome.js
@@ -28,3 +28,37 @@ class isValidPalindrome {
     }
     
 }
+
+
+/*
+Brute Force plan:
+1).X?() method that removes all spaces and non alpha-numeric characters.
+Can't remember the name, have to google this one -> .replace(/[^A-Za-z0-9]/g, ''),
+.replace(/\W/g, '') does not remove the underscore '_' and causes failure
+2).toLowerCase() method eliminates case sensitivity
+3)Init two pointers; front & back
+4)Create while loop;
+a)start at opposing ends of s
+b)compare each value front to back === back to front
+c)create short circuit for failure front !== back
+d)increment and decrement front & back
+Return Boolean
+*/
+
+var isPalindrome = function(s) {
+    let str = s.replace(/[^A-Za-z0-9]/g, '').toLowerCase(); 
+    //console.log(str); // if you want to see the results of the above methods
+    let front = 0;
+    let back = str.length-1;
+    
+    while (front < back) {
+        if(str[front] !== str[back]) return false;
+        //short circuit for false condition
+        front++;
+        back--;
+    }
+    return true;
+};
+
+//Runtime: 81 ms, faster than 83.30% of JavaScript online submissions for Valid Palindrome.
+//Memory Usage: 44.5 MB, less than 68.62% of JavaScript online submissions for Valid Palindrome.
--- javascript/217-Contains-Duplicate.js
+++ javascript/217-Contains-Duplicate.js
@@ -14,3 +14,25 @@ var containsDuplicate = function(nums) {
     }
     return false;
 };
+
+//Brute force. Straight forward nested for loops,
+//both for loops count right. O(n^2)
+var containsDuplicate = function(nums) {
+    for (let i=0; i<nums.length; i++) {
+        for (let j=1; j<nums.length; j++){
+            if(nums[i] === nums[j]) return true;
+        }
+    }
+    return false;
+};
+    
+//Brute force, the for loops start at opposite ends and
+//count towards each other. With j>i they don't cross over
+var containsDuplicate = function(nums) {
+    for (let i=0; i<nums.length-2; i++) {
+        for (let j=nums.length-1; j>i; j--){
+            if(nums[i] === nums[j]) return true;
+        }
+    }
+    return false;
+};
