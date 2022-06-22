0ca04e8d6f7528d85678ae2651a7d61ab6b0f98a
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-06-22 14:08:33 +0530

    Create 90-Subsets-II.java

---
+++ java/90-Subsets-II.java
@@ -0,0 +1,26 @@
+//Similar to subsets 1. Here, we'll just take care of the duplicates.
+//This video was helpful https://www.youtube.com/watch?v=mcg4qKbAmmY&t=316s&ab_channel=Fraz
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        helper(ans, 0, nums, list, false);
+        return ans;
+    }
+    
+     public void helper(List<List<Integer>> ans, int start, int[] nums, List<Integer> list, boolean ignored) {
+        if (start>=nums.length) {
+            ans.add(new ArrayList<>(list));
+        } else {
+            helper(ans, start+1, nums, list, true);
+            //if we've ignored the value earlier then we must ignore all the values after that too
+            if (start>0 && nums[start-1]==nums[start] && ignored) 
+                return;
+            list.add(nums[start]);
+            helper(ans, start+1, nums, list, false);
+            list.remove(list.size()-1);
+        }
+    }
+    
+}
