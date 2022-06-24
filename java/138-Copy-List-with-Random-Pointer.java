3895a4f3ae76a9acfc5765dc82c3ff59c1cf6556
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-06-24 17:58:23 +0530

    Create 138-Copy-List-with-Random-Pointer.java

---
+++ java/138-Copy-List-with-Random-Pointer.java
@@ -0,0 +1,17 @@
+class Solution {
+    public Node copyRandomList(Node head) {
+        Node cur = head;
+        HashMap<Node, Node> map = new HashMap<>();
+        while (cur!=null) {
+            map.put(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+        cur = head;
+        while (cur!=null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+        return map.get(head);
+    }
+}
