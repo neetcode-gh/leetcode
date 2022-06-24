753f04bdcf781adb8f819b9e4a5b627b23e74118
Pruthvi rajk <46647933+kpr9991@users.noreply.github.com>
2022-06-24 20:48:30 +0530

    Update 242-Valid-Anagrams.py

--- 242-Valid-Anagrams.py
+++ 242-Valid-Anagrams.py
@@ -1,14 +1,28 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
+        #Solution 1 (nlogn) Time, len(t)+len(s) in Memory space
+        return sorted(s)==sorted(t)
+        #Solution 2 (n) Time, len(t) or len(s) extra space
+        if len(s)!=len(t):
             return False
-        
-        countS, countT = {}, {}
-        
-        for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0)
-            countT[t[i]] = 1 + countT.get(t[i], 0)
-        for c in countS:
-            if countS[c] != countT.get(c, 0):
+        dict={}
+        for char in s:
+            dict[char]+=dict.get(char,0)+1
+        c=0
+        for char in t:
+            if dict.get(char):
+                dict[char]-=1
+                if dict[char]==0:
+                    c+=1
+                if dict[char]<0:
+                    return False
+            else:
                 return False
-        return True
+        
+        return c==len(dict)
+
+
+    
+        
+        
+        
