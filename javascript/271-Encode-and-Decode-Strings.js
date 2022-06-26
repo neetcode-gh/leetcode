3e32ae9880177c8f2e8127b15f2b77d370da86f4
Adam Smith <imagineadamsmith@gmail.com>
2022-06-25 18:49:34 -0700

    Minor format changes to JS problem 271

--- javascript/271-Encode-and-Decode-Strings.js
+++ javascript/271-Encode-and-Decode-Strings.js
@@ -2,27 +2,27 @@
  * @param {string[]} strs
  * @return {string}
  */
-var encode = function (strs) {
-    let res = "";
-    for (const s of strs) res += String(s.length) + "#" + s;
-    return res;
+function encode(strs) {
+    return strs.map(str => `${str.length}#${str}`).join('');
 }
 
 /**
  * @param {string} str
  * @return {string[]}
  */
-var decode = function (str) {
+function decode(str) {
     const res = [];
     let i = 0;
 
     while (i < str.length) {
         let j = i;
-        while (str[j] != "#") j += 1;
+        while (str[j] !== "#") {
+            ++j;
+        }
 
-        length = Number(str.slice(i, j));
-        res.push(str.slice(j + 1, j + 1 + length));
-        i = j + 1 + length;
+        const len = Number(str.slice(i, j));
+        res.push(str.slice(++j, j + len));
+        i = j + len;
     }
 
     return res;
