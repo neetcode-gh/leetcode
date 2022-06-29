fa5dd2105c6b1fa9c7b2ef79b385a3b3c16e8bd8
Adam Smith <imagineadamsmith@gmail.com>
2022-06-28 22:04:28 -0700

    Added dp JS solution for problem 97

--- javascript/97-Interleaving-String.js
+++ javascript/97-Interleaving-String.js
@@ -47,3 +47,45 @@ function isInterleave(s1, s2, s3) {
         );
     }
 }
+
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @param {string} s3
+ * @return {boolean}
+ */
+function isInterleave(s1, s2, s3) {
+    
+    const l1 = s1.length;
+    const l2 = s2.length;
+    const l3 = s3.length;
+    
+    if (l1 + l2 !== l3) {
+        return false;
+    }
+    if (!s1 || !s2 || !s3) {
+        return (!s1 && !s2 && !s3) || (
+            s1
+                ? s1 === s3
+                : s2 === s3
+        );
+    }
+    
+    const seen = new Array(l1 + 1).fill()
+        .map(() => new Array(l2 + 1));
+    seen[l1][l2] = true;
+    
+    for (let i = l1 - 1; i >= 0; --i) {
+        seen[i][l2] = seen[i + 1][l2] && s1[i] === s3[i + l2];
+    }
+    for (let j = l2 - 1; j >= 0; --j) {
+        seen[l1][j] = seen[l1][j + 1] && s2[j] === s3[l1 + j];
+    }
+    for (let i = l1 - 1; i >= 0; --i) {
+        for (let j = l2 - 1; j >= 0; --j) {
+            seen[i][j] = (seen[i + 1][j] && s1[i] === s3[i + j])
+                || (seen[i][j + 1] && s2[j] === s3[i + j]);
+        }
+    }
+    return seen[0][0];
+}
