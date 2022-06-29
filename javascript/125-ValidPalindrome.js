9a02b70ac517e294d3c6e0b9af85b65c83e6e8d9
Adam Smith <imagineadamsmith@gmail.com>
2022-06-28 23:14:10 -0700

    Cleaned up JS problem 125

--- javascript/125-ValidPalindrome.js
+++ javascript/125-ValidPalindrome.js
@@ -1,23 +1,25 @@
-var isPalindrome = function(s) {
-    let l = 0, r = s.length-1;
-    while(l<r){
-        while(l < r && !isAlphaNumeric(s[l])){
+const ALPHA_NUM = /^[a-zA-Z0-9]$/;
+
+function isPalindrome(s) {
+
+    let l = 0;
+    let r = s.length - 1;
+
+    while(l < r){
+        while(l < r && !ALPHA_NUM.test(s[l])){
             l++;
         }
-        while(r > l && !isAlphaNumeric(s[r])){
+        while(l < r && !ALPHA_NUM.test(s[r])){
             r--;
         }
-        if(s[l].toLowerCase() !== s[r].toLowerCase()) return false;
+
+        if(s[l].toLowerCase() !== s[r].toLowerCase()) {
+            return false;
+        }
+
         l++;
         r--;
     }
-    return true;
 
-    function isAlphaNumeric(c){
-        return (
-            ('A'.charCodeAt(0) <= c.charCodeAt(0) &&  c.charCodeAt(0) <= 'Z'.charCodeAt(0)) ||
-            ('a'.charCodeAt(0) <= c.charCodeAt(0) &&  c.charCodeAt(0) <= 'z'.charCodeAt(0)) ||
-            ('0'.charCodeAt(0) <= c.charCodeAt(0) &&  c.charCodeAt(0) <= '9'.charCodeAt(0))
-        )
-    }
-};
+    return true;
+}
