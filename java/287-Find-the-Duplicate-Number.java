6229f421e97202e634957c6500ddc9e19d14a556
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-07-06 21:59:51 +0530

    Create 287-Find-the-Duplicate-Number.java

---
+++ java/287-Find-the-Duplicate-Number.java
@@ -0,0 +1,25 @@
+//Fast and slow pointer approach
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int fast = nums[0];
+        int slow = nums[0];
+        boolean first = true;
+        while (first || fast!=slow) {
+            if (first) first = false;
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if (fast == slow) break;
+        }
+        int slow2 = nums[0];
+        while (slow2!=slow) {
+            if (first) first = false;
+            slow2 = nums[slow2];
+            slow = nums[slow];
+            if (slow2 == slow) return slow;
+        }
+        return slow;
+    }
+}
