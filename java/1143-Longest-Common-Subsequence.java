bbf6afa093fe959427364aef87c804649b55fca9
miladra <miladranjbari@gmail.com>
2022-07-06 22:30:08 +0430

    Add Itrative version of Longest Common Subsequence

--- java/1143-Longest-Common-Subsequence.java
+++ java/1143-Longest-Common-Subsequence.java
@@ -17,3 +17,39 @@ class Solution {
     }
     
 }
+
+
+// Itrative version
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        //O(n*m)/O(n*m)  time/memory
+        if (text1.isEmpty() || text2.isEmpty()) {
+            return 0;
+        }
+
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+
+        for (int i = 0; i <= text1.length(); i++) {
+            dp[i][0] = 0;
+        }
+
+        for (int j = 0; j <= text2.length(); j++) {
+            dp[0][j] = 0;
+        }
+
+        for (int i = 1; i <= text1.length(); i++) {
+            for (int j = 1; j <= text2.length(); j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+
+            }
+        }
+
+        return dp[text1.length()][text2.length()];
+    }
+}
+
