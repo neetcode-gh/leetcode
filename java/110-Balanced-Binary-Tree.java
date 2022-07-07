226d3d0526788417c6038fe1d02c39dd0535f199
Saiteja <62810536+saiteja-basam@users.noreply.github.com>
2022-07-07 23:43:06 +0530

    110-Balanced-Binary-Tree.java
    Betterway to solve the problem using the approach from Diameter of a Binary Tree Solution

--- java/110-Balanced-Binary-Tree.java
+++ java/110-Balanced-Binary-Tree.java
@@ -1,19 +1,14 @@
-package com.coding.patterns.tree;
-
-class BalancedBinaryTree {
-    private static Pair<Boolean, Integer> dfs(TreeNode root) {
-        if (root == null) {
-            return new Pair<Boolean, Integer>(true, 0);
-        }
-        
-        var left = dfs(root.left);
-        var right = dfs(root.right);
-        
-        var balanced = left.getKey() && right.getKey() && (Math.abs(left.getValue() - right.getValue()) <= 1);
-        
-        return new Pair<Boolean, Integer>(balanced, 1 + Math.max(left.getValue(), right.getValue()));
+class Solution {
+    public boolean result=true;
+    public boolean isBalanced(TreeNode root) {
+        dfs(root);
+        return result;
     }
-    public static boolean isBalanced(TreeNode root) {
-        return dfs(root).getKey();
+    public int dfs(TreeNode root){
+        if(root==null)return 0;
+        int rd=dfs(root.right)+1;
+        int ld=dfs(root.left)+1;
+        if(Math.abs(rd-ld)>1) result=false;
+        return Math.max(rd,ld);
     }
 }
