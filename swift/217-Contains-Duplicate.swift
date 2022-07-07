a6a3d093981987a37ae766bf9b0ccda1163e1032
Andrew Carvajal <andrew.carvajal@me.com>
2022-07-07 19:05:39 -0400

    Adds a different method to check for duplicates

--- swift/217-Contains-Duplicate.swift
+++ swift/217-Contains-Duplicate.swift
@@ -4,7 +4,26 @@
 
 class ContainsDuplicate {
     func containsDuplicate(_ nums: [Int]) -> Bool {
-        guard nums.count == Set(nums).count else { return true }
+        var hashSet = Set<Int>()
+        for n in nums {
+            if hashSet.contains(n) {
+                return true
+            }
+            hashSet.insert(n)
+        }
         return false
+    }
+    
+    func containsDuplicateByCountComparison(_ nums: [Int]) -> Bool {
+        
+        // a Set must contain unique items
+        // so, if the count of `nums` is unequal to `nums` casted as a Set,
+        // then, one or more items were removed,
+        // meaning, there were one or more duplicates in `nums`
+        if nums.count == Set(nums).count {
+            return false
+        } else {
+            return true
+        }
     }
 }
