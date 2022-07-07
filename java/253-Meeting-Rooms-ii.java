baf691d56e1fd657bd80e52148de940fc4876fd8
miladra <miladranjbari@gmail.com>
2022-07-07 12:54:14 +0430

    Add another approach fot meeting rooms

--- java/253-Meeting-Rooms-ii.java
+++ java/253-Meeting-Rooms-ii.java
@@ -32,3 +32,42 @@ public class Solution {
         return count;
     }
 }
+
+// Two pointer approach
+public class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+        if (intervals.size() == 0) {
+            return 0;
+        }
+
+        int len = intervals.size();
+        int[] startTime = new int[len];
+        int[] endTime = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            startTime[i] = intervals.get(i).start;
+            endTime[i] = intervals.get(i).end;
+        }
+
+        Arrays.sort(startTime);
+        Arrays.sort(endTime);
+
+        int res = 0;
+        int count = 0;
+        int s = 0;
+        int e = 0;
+
+        while (s < len) {
+            if (startTime[s] < endTime[e]) {
+                s++;
+                count++;
+            } else {
+                e++;
+                count--;
+            }
+            res = Math.max(res, count);
+        }
+
+        return res;
+    }
+}
