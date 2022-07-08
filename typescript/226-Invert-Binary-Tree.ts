25b2b755d569dd29ed062ce8570e22782a3d9d76
dipti <dipti.yadav@outlook.com>
2022-07-08 16:05:27 -0400

    226. Invert Binary Tree Solution

---
+++ typescript/226-Invert-Binary-Tree.ts
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+  if (!root) return root
+  const temp: TreeNode | null = root.left
+  root.left = root.right
+  root.right = temp
+
+  invertTree(root.left)
+  invertTree(root.right)
+  return root
+}
