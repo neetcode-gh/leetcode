b401a49e69dac366a0cf2449c708e2851f935b59
khvr1993 <khvr.1993@gmail.com>
2022-07-08 14:56:10 +0000

    group anagrams

---
+++ go/01_arrays_and_hashing/groupAnagrams.go
@@ -0,0 +1,23 @@
+package arraysandhashing
+
+func groupAnagrams(strs []string) [][]string {
+	var result [][]string
+	strsMap := make(map[[26]int][]string)
+	var charArray [26]int
+	var anagramGroup []string
+	for i := 0; i < len(strs); i++ {
+		for _, char := range strs[i] {
+			charIndex := char - 'a'
+			charArray[charIndex] += 1
+		}
+		anagramGroup, _ = strsMap[charArray]
+		anagramGroup = append(anagramGroup, strs[i])
+		strsMap[charArray] = anagramGroup
+		charArray = [26]int{}
+	}
+	// fmt.Println(strsMap)
+	for _, value := range strsMap {
+		result = append(result, value)
+	}
+	return result
+}
