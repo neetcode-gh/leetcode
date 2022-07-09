92184ebcd1db4b52bcdc0a8a09ca73b1a29a497a
aa0 <71089234+Ahmad-A0@users.noreply.github.com>
2022-07-09 13:16:59 +0100

    Fix indentation

--- javascript/49-Group-Anagrams.js
+++ javascript/49-Group-Anagrams.js
@@ -5,21 +5,21 @@
 const groupAnagrams = (strs) => {
   const result = [];
   const map = new Map();
-    for (let i = 0; i < strs.length; i++) {
-    
-        const hash = new Array(26).fill("\x00");
-        
-        for (let c of strs[i]){
-            const codeDiff = c.charCodeAt(0) - 'a'.charCodeAt(0);
-            hash[codeDiff] = String.fromCharCode(hash[codeDiff].charCodeAt(0) + 1);
-        }
+  for (let i = 0; i < strs.length; i++) {
 
-        const hashString = hash.join();
-        if (map.has(hashString)) {
-          map.get(hashString).push(strs[i]);
-        } else {
-          map.set(hashString, [strs[i]]); 
-        }
+      const hash = new Array(26).fill("\x00");
+
+      for (let c of strs[i]){
+          const codeDiff = c.charCodeAt(0) - 'a'.charCodeAt(0);
+          hash[codeDiff] = String.fromCharCode(hash[codeDiff].charCodeAt(0) + 1);
+      }
+
+      const hashString = hash.join();
+      if (map.has(hashString)) {
+        map.get(hashString).push(strs[i]);
+      } else {
+        map.set(hashString, [strs[i]]); 
+      }
   }
 
   for (let [key, value] of map) {
