849251a9ab1c21e36f27c7d5f817e1ca5c23b675
Sandeep Gamot <sandeep@shortcastle.com>
2022-07-09 12:27:29 +0530

    update: simplify typescript/LC-19

--- typescript/19-Remove-Nth-Node-From-End-of-List.ts
+++ typescript/19-Remove-Nth-Node-From-End-of-List.ts
@@ -11,25 +11,26 @@
  */
 
 function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
-  let dummyList: ListNode = new ListNode(0)
-  dummyList.next = head
-  let count: number = 0
-  let first: ListNode = dummyList
-  let second: ListNode = dummyList
+  let runner = head;
+  let c = 0;
 
-  while (count < n) {
-    second = second.next
-    count++
+  while (c != n && runner != null) {
+    c++;
+    runner = runner.next;
   }
-  if (second === null) {
-    dummyList.val = dummyList.next.val
-    dummyList.next = dummyList.next.next
-  }
-  while (second.next !== null) {
-    second = second.next
-    first = first.next
-  }
-  first.next = first.next.next
 
-  return dummyList.next
+  if (runner == null) {
+    return head.next;
+  }
+
+  let tail = head;
+
+  while (runner.next != null) {
+    runner = runner.next;
+    tail = tail.next;
+  }
+
+  tail.next = tail.next.next;
+
+  return head;
 }
