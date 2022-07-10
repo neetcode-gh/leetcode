c81cd11567a5c0b0818c168afe3a40b6fa58c229
aa0 <71089234+Ahmad-A0@users.noreply.github.com>
2022-07-10 17:38:34 +0100

    Workflow test
    This is to test the format workflow.

---
+++ javascript/420-Best-Number.js
@@ -0,0 +1,9 @@
+var bestNumber = function() {
+              console.log("this is a file" +
+                         
+                         " with clear formatting",
+                                                                 "errors")
+ return        420
+}
+
+                      // ^^ This is O(1)
