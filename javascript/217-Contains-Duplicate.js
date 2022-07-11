45a6b659c1052e0c43570a94e7b21737986ec1fc
Mitchell Irvin <16233245+mitchellirvin@users.noreply.github.com>
2022-07-11 09:46:31 -0400

    Update 217-Contains-Duplicate.js

--- javascript/217-Contains-Duplicate.js
+++ javascript/217-Contains-Duplicate.js
@@ -18,7 +18,7 @@ var containsDuplicate = function(nums) {
 //Second method using Map() (Has to map entire array but code is more readable)
 var containsDuplicate = function(nums) {
     //create a new hashmap with all the items in the array. Any duplicates will be removed.
-    let totalWithoutDuplicates = new Map(nums.map((i) => [i]));
+    const totalWithoutDuplicates = new Map(nums.map((i) => [i]));
     
     //check if the size of the initial array is larger than the new hashmap. 
     return totalWithoutDuplicates.size !== nums.length;
