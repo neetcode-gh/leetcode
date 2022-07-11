2d12098f0cc9aea0876a67683e328792fc638ab6
barlingo <16936512+barlingo@users.noreply.github.com>
2022-07-10 20:52:36 -0400

    improved with bucket list algorithm

--- rust/neetcode150/01_arrays_and_hashing/top_k_frequent_elements.rs
+++ rust/neetcode150/01_arrays_and_hashing/top_k_frequent_elements.rs
@@ -1,26 +1,23 @@
 #![cfg_attr(debug_assertions, allow(dead_code, unused_imports))]
-use std::collections::BTreeMap;
-use std::iter::FromIterator;
+use std::collections::HashMap;
 
 pub struct Solution;
 impl Solution {
     pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
-        let mut map: BTreeMap<i32, i32> = BTreeMap::new();
+        let mut map: HashMap<i32, usize> = HashMap::new();
         for num in &nums {
-            if map.contains_key(num) {
-                continue;
-            }
-            let num_count = nums.iter().filter(|x| *x == num).count();
-            map.entry(*num).or_insert(num_count as i32);
+            *map.entry(*num).or_insert(0) += 1;
         }
-        let mut v = Vec::from_iter(map);
-        v.sort_by(|&(_, a), &(_, b)| b.cmp(&a));
-        let mut s: Vec<i32> = Vec::with_capacity(k as usize);
-        println!("{:#?}", v);
+        let mut bucket = vec![vec![]; nums.len()];
+        for (num, count) in map {
+            bucket[count - 1].push(num);
+        }
 
-        for n in v.iter().take(k as usize) {
-            s.push(n.0);
-        }
-        s
+        bucket
+            .into_iter()
+            .rev()
+            .flatten()
+            .take(k as usize)
+            .collect()
     }
 }
