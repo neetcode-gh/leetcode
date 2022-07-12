8da3d296426b2d47b1d104d690d8422d130cf1ce
Mitchell Irvin <16233245+mitchellirvin@users.noreply.github.com>
2022-07-11 20:26:35 -0400

    Update 739-Daily-Temperatures.py

--- python/739-Daily-Temperatures.py
+++ python/739-Daily-Temperatures.py
@@ -7,5 +7,5 @@ class Solution:
             while stack and t > stack[-1][0]:
                 stackT, stackInd = stack.pop()
                 res[stackInd] = (i - stackInd)
-            stack.append([t, i])
+            stack.append((t, i))
         return res
