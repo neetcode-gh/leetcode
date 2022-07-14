9ab5ed2a94bfd611a252ad60425f90bca625a74e
aakhtar3 <aakhtar3@users.noreply.github.com>
2022-07-14 11:38:24 -0400

    refactor: js | linkedlist

--- javascript/141-Linked-List-Cycle.js
+++ javascript/141-Linked-List-Cycle.js
@@ -1,24 +1,35 @@
 /**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
-
-/**
+ * https://leetcode.com/problems/linked-list-cycle/
+ * Time O(N) | Space O(N)
  * @param {ListNode} head
  * @return {boolean}
  */
-var hasCycle = function (head) {
-    let set = new Set();
-    while (head) {
-        if (set.has(head)) {
-            return true;
-        } else {
-            set.add(head);
-            head = head.next;
-        }
+var hasCycle = function(head, seen = new Set()) {
+    while (head) {/* Time O(N) */
+        if (seen.has(head)) return true;
+
+        seen.add(head);/* Space O(N) */
+        head = head.next;
+    }
+
+    return false;
+}
+
+/**
+ * https://leetcode.com/problems/linked-list-cycle/
+ * Time O(N) | Space O(1)
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let [ slow, fast ] = [ head, head];
+
+    while (fast && fast.next) {/* Time O(N) */
+        slow = slow.next;
+        fast = fast.next.next;
+
+        const hasCycle = slow === fast;
+        if (hasCycle) return true;
     }
 
     return false;
