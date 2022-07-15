510d6bc4f4fd958236b9f1ba5eddcf2247b0f8e1
loczek <30776250+loczek@users.noreply.github.com>
2022-07-15 18:59:00 +0200

    Created 199-Binary-Tree-Right-Side-View

---
+++ typescript/199-Binary-Tree-Right-Side-View.ts
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function rightSideView(root: TreeNode | null): number[] {
+  const orders = levelOrder(root);
+
+  return orders.map((item) => item[item.length - 1]);
+}
+
+function levelOrder(root: TreeNode | null): number[][] {
+  const levels: number[][] = [];
+
+  function getHeight(node: TreeNode | null, height: number) {
+    if (!node) return 0;
+
+    if (node.left || node.right) {
+      getHeight(node.left, height + 1);
+      getHeight(node.right, height + 1);
+    }
+
+    if (levels[height]) {
+      levels[height].push(node.val);
+    } else {
+      levels[height] = [node.val];
+    }
+  }
+
+  getHeight(root, 0);
+
+  return levels;
+}
