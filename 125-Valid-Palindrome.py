857360f01f5e817b31b84a95a5485b02d4f5224b
neetcode-gh <77742485+neetcode-gh@users.noreply.github.com>
2022-07-15 01:43:34 -0700

    Revert "Update 125-Valid-Palindrome.py"

--- 125-Valid-Palindrome.py
+++ 125-Valid-Palindrome.py
@@ -1,17 +1,19 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        l = 0
-        r = len(s)-1
-        while l<r:
-            if s[l].lower()==s[r].lower():
-                l+=1
-                r-=1
-                continue
-            
-            elif not (65<=ord(s[l])<=90 or 97<=ord(s[l])<=122 or 48<=ord(s[l])<=57):
-                l+=1
-            elif  not (65<=ord(s[r])<=90 or 97<=ord(s[r])<=122 or 48<=ord(s[r])<=57):
-                r-=1
-            else:
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not self.alphanum(s[l]): 
+                l += 1
+            while l < r and not self.alphanum(s[r]): 
+                r -= 1
+            if s[l].lower() != s[r].lower(): 
                 return False
+            l += 1
+            r -= 1
         return True
+    
+    # Could write own alpha-numeric function
+    def alphanum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9'))
