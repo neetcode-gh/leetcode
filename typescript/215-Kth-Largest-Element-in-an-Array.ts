c835937fe80dd0cc25052314f9a3d583d035103a
loczek <30776250+loczek@users.noreply.github.com>
2022-07-15 18:59:56 +0200

    Created 215-Kth-Largest-Element-in-an-Array

---
+++ typescript/215-Kth-Largest-Element-in-an-Array.ts
@@ -0,0 +1,31 @@
+function findKthLargest(nums: number[], k: number): number {
+  k = nums.length - k;
+
+  function quickSelect(l: number, r: number) {
+    let pivot = nums[r];
+    let p = l;
+
+    for (let i = l; i < r; i++) {
+      if (nums[i] <= pivot) {
+        let temp = nums[p];
+        nums[p] = nums[i];
+        nums[i] = temp;
+        p += 1;
+      }
+    }
+
+    let temp = nums[p];
+    nums[p] = nums[r];
+    nums[r] = temp;
+
+    if (p > k) {
+      return quickSelect(l, p - 1);
+    } else if (p < k) {
+      return quickSelect(p + 1, r);
+    } else {
+      return nums[p];
+    }
+  }
+
+  return quickSelect(0, nums.length - 1);
+}
