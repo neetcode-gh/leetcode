3f50d162fa346de551e31ce4c1dc561138be97b7
Chaitanya Kumar <chaitanya.kmc@gmail.com>
2022-07-16 17:50:40 +0530

    Update 347-Top-K-Frquent-Elements.js
    Added more simple version of JavaScript solution

--- javascript/347-Top-K-Frquent-Elements.js
+++ javascript/347-Top-K-Frquent-Elements.js
@@ -3,6 +3,40 @@
  * @param {number} k
  * @return {number[]}
  */
+// Solution 1
+var topKFrequent = function(nums, k) {
+    // Calculating frequency of elements in nums in map object
+    // map sample output -> { '1': 3, '2': 2, '3': 1 }
+    let map = {};
+    for(let i = 0; i < nums.length; i++) {
+        if(map.hasOwnProperty(nums[i])) {
+            map[nums[i]]++;
+        } else {
+            map[nums[i]] = 1;
+        }
+    }
+
+    // Converting map object to array mapArray to sort them based on frequency
+    // mapArray sample out put -> [ [ '1', 3 ], [ '2', 2 ], [ '3', 1 ] ]
+
+    let mapArray = [];
+    for(let key in map) {
+        mapArray.push([key, map[key]])
+    }
+    
+    // Sorting mapArray array based on frequency value
+    let r = mapArray.sort((a, b) => Number(b[1]) - Number(a[1]));
+    
+    // Pushing only k number of items to result array
+    let result = [];
+    for(l = 0; l < k; l++) {
+        result.push(r[l][0]);
+    }
+    return result;
+};
+
+
+// Solution 2
 var topKFrequent = function(nums, k) {
     let map = new Map();
     let res = [];
