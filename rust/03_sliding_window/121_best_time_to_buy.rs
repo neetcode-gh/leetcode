f1999f047acfe83eaa7c40a2f533482e0435fcbb
Devesh Rawat <devsrawt@gmail.com>
2022-07-17 12:22:26 +0530

    sliding_window problems

---
+++ rust/03_sliding_window/121_best_time_to_buy.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    use super::std::cmp;
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        let mut l = 0;
+        let mut r = 1;
+        let mut max_profit = 0;
+        while r < prices.len() {
+            if prices[l] < prices[r] {
+                let profit = prices[r] - prices[l];
+                max_profit = cmp::max(profit, max_profit)
+            } else {
+                l = r
+            }
+            r += 1
+        }
+        max_profit
+    }
+}
