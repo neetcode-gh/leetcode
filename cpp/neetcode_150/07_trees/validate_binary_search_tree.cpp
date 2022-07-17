414ac353015b2c7884e644a440677137a669f69d
KC10201 <70864069+KC10201@users.noreply.github.com>
2022-07-17 19:58:10 -0400

    Update validate_binary_search_tree.cpp
    revert change

--- cpp/neetcode_150/07_trees/validate_binary_search_tree.cpp
+++ cpp/neetcode_150/07_trees/validate_binary_search_tree.cpp
@@ -1,8 +1,6 @@
 /*
     Given root of binary tree, determine if it's valid (left all < curr, right all > curr)
-
     Inorder traversal & check if prev >= curr, recursive/iterative solutions
-
     Time: O(n)
     Space: O(n)
 */
@@ -21,20 +19,29 @@
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        using def = function<bool(TreeNode*,long,long)>;
+        TreeNode* prev = NULL;
+        return inorder(root, prev);
+    }
+private:
+    bool inorder(TreeNode* root, TreeNode*& prev) {
+        if (root == NULL) {
+            return true;
+        }
         
-        def valid = [&] (auto node, long left, long right) {
-            if(!node){
-                return true;   
-            }
-            if(!(node->val < right && node->val > left)){
-                return false;
-            }
-            return (valid(node->left, left, node->val) && 
-                    valid(node->right, node->val, right));
-        };
+        if (!inorder(root->left, prev)) {
+            return false;
+        }
         
-        return valid(root, long(INT_MIN)-1, long(INT_MAX)+1);
+        if (prev != NULL && prev->val >= root->val) {
+            return false;
+        }
+        prev = root;
+        
+        if (!inorder(root->right, prev)) {
+            return false;
+        }
+        
+        return true;
     }
 };
 
