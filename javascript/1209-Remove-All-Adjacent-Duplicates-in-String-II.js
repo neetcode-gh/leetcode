1d1f2132c0806adfff8e1504ba04d8de5676f5c0
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-07-17 02:05:18 +0100

    cleaned up

--- javascript/1209-Remove-All-Adjacent-Duplicates-in-String-II.js
+++ javascript/1209-Remove-All-Adjacent-Duplicates-in-String-II.js
@@ -4,22 +4,19 @@
  * @return {string}
  */
 var removeDuplicates = function (s, k) {
-  const stack = [];
+  const stack = []; // [char, count];
 
   for (const c of s) {
-    // Construct stack
     if (stack.length !== 0 && stack[stack.length - 1][0] === c) {
       stack[stack.length - 1][1]++;
     } else {
       stack.push([c, 1]);
     }
 
-    // Remove from stack
     if (stack[stack.length - 1][1] === k) {
       stack.pop();
     }
   }
 
-  // Build output of remaining characters
   return stack.reduce((res, el) => (res += el[0].repeat(el[1])), '');
 };
