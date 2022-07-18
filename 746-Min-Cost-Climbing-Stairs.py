cdc55d6949f3ee707e9b0f4c0466f31cc0d2795d
Florian <florianlap2@gmail.com>
2022-07-18 14:29:42 +0200

    Update 746-Min-Cost-Climbing-Stairs.py
    `cost.append(0)` serves no purpose but to confuse.

--- 746-Min-Cost-Climbing-Stairs.py
+++ 746-Min-Cost-Climbing-Stairs.py
@@ -1,7 +1,5 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        cost.append(0)
-        
         for i in range(len(cost) - 3, -1, -1):
             cost[i] += min(cost[i + 1], cost[i + 2])
             
