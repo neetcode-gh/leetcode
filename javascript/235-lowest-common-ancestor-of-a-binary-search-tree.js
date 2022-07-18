a2d013890a55b007bb228de046985a7243c4dddc
aakhtar3 <aakhtar3@users.noreply.github.com>
2022-07-18 10:40:50 -0400

    refactor: js - tree

--- javascript/235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ javascript/235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -1,25 +1,45 @@
 /**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
-
-/**
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+ * Time O(N) | Space O(H)
  * @param {TreeNode} root
  * @param {TreeNode} p
  * @param {TreeNode} q
  * @return {TreeNode}
  */
-var lowestCommonAncestor = function (root, p, q) {
+ var lowestCommonAncestor = function(root, p, q) {
+    const isGreater = (p.val < root.val) && (q.val < root.val);
+    if (isGreater) return lowestCommonAncestor(root.left, p, q);
+
+    const isLess = (root.val < p.val) && (root.val < q.val);
+    if (isLess) return lowestCommonAncestor(root.right, p, q);
+
+    return root;
+};
+
+/**
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+ * Time O(N) | Space O(1)
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+ var lowestCommonAncestor = function(root, p, q) {
     while (root !== null) {
-        if (root.val < p.val && root.val < q.val) {
+        const isGreater = (root.val < p.val) && (root.val < q.val)
+        if (isGreater) {
             root = root.right;
-        } else if (root.val > p.val && root.val > q.val) {
-            root = root.left;
-        } else {
-            return root;
+            continue;
         }
+
+        const isLess = (p.val < root.val) && (q.val < root.val);;
+        if (isLess) {
+            root = root.left;
+            continue;
+        }
+
+        break;
     }
+
+    return root;
 };
--- javascript/572-Subtree-of-Another-Tree.js
+++ javascript/572-Subtree-of-Another-Tree.js
@@ -1,30 +1,96 @@
 /**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
+ * https://leetcode.com/problems/subtree-of-another-tree/
  * @param {TreeNode} root
  * @param {TreeNode} subRoot
  * @return {boolean}
  */
-var isSubtree = function (root, subRoot) {
-    // given two nodes are they the same?
-    const isSame = (n1, n2) => {
-        if (!n1 && !n2) return true;
-        if (!n1 || !n2 || n1.val !== n2.val) return false;
-        return isSame(n1.left, n2.left) && isSame(n1.right, n2.right);
-    };
+var isSubtree = function(root, subRoot) {
+    if (!root) return false
 
-    // check if subRoot is subtree of root:
-    const DFS = (node) => {
-        if (!node) return false;
-        if (isSame(node, subRoot)) return true;
-        return DFS(node.left) || DFS(node.right);
-    };
+    if (isSame(root, subRoot)) return true
 
-    return DFS(root);
+    const hasLeftTree = isSubtree(root.left, subRoot)
+    const hasRightTree = isSubtree(root.right, subRoot)
+
+    return hasLeftTree || hasRightTree
 };
+
+const isSame = (root, subRoot) => {
+    const hasReachedEnd = !(root && subRoot)
+    if (hasReachedEnd) return root === subRoot
+
+    const isMismatch = root.val !== subRoot.val
+    if (isMismatch) return false
+
+    const isLeftSame = isSame(root.left, subRoot.left)
+    const isRightSame = isSame(root.right, subRoot.right)
+
+    return isLeftSame && isRightSame
+}
+
+const hash = (val) => require('crypto')
+    .createHash('md5')
+    .update(val)
+    .digest('hex')
+
+const merkle = (root) => {
+    if (!root) return '#'
+
+    const { left, val, right } = root
+
+    const leftMerkle = merkle(left)
+    const rightMerkle = merkle(right)
+
+    const merkleVal = [ leftMerkle, val, rightMerkle ].join('')
+    const merkleHash = hash(merkleVal)
+
+    root.merkle = merkleHash
+
+    return root.merkle
+}
+
+const search = (root, subRoot) => {
+    if (!root) return false
+
+    const hasSamePath = root.merkle === subRoot.merkle
+    if (hasSamePath) return true
+
+    const left = search(root.left, subRoot)
+    const right = search(root.right, subRoot)
+
+    return left || right
+}
+
+var isSubtree = function(root, subRoot) {
+    [ root, subRoot ].forEach(merkle)
+
+    return search(root, subRoot)
+}
+
+const hashify = (root, hash, postOrderKey) => {
+    if (!root) return '#'
+
+    const left = hashify(root.left, hash, postOrderKey)
+    const right = hashify(root.right, hash, postOrderKey)
+
+    const key = [left, root.val, right].join('')
+
+    if (!hash.has(key)) {
+        hash.set(key, postOrderKey[0])
+        postOrderKey[0]++
+    }
+
+    return hash.get(key)
+}
+
+var isSubtree = function(root, subRoot, hash = new Map (), postOrderKey = [0]) {
+    hashify(root, hash, postOrderKey)
+
+    const hashKey = [
+        hashify(subRoot.left, hash, postOrderKey),
+        subRoot.val,
+        hashify(subRoot.right, hash, postOrderKey)
+    ].join('')
+
+    return hash.has(hashKey)
+}
