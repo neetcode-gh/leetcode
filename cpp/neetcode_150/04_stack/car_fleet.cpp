62a0851b6d94df3ed7943279a84601029c5b9822
Arindam-7 <itsnotarindam@gmail.com>
2022-07-19 13:48:32 +0530

    added stack soln of car fleet

--- cpp/neetcode_150/04_stack/car_fleet.cpp
+++ cpp/neetcode_150/04_stack/car_fleet.cpp
@@ -12,26 +12,26 @@
 class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        int n = position.size();
-        
-        vector<pair<int, double>> cars;
-        for (int i = 0; i < n; i++) {
-            double time = (double) (target - position[i]) / speed[i];
-            cars.push_back({position[i], time});
+        vector<pair<int, double>> vec;
+        for(int i = 0; i < position.size(); i++){
+            double time = (double)(target - position[i])/speed[i];
+            vec.push_back({position[i], time});
         }
-        sort(cars.begin(), cars.end());
         
-        double maxTime = 0.0;
-        int result = 0;
+        sort(vec.begin(), vec.end());
         
-        for (int i = n - 1; i >= 0; i--) {
-            double time = cars[i].second;
-            if (time > maxTime) {
-                maxTime = time;
-                result++;
+        stack<double> s;
+        
+        for(int i = 0; i < position.size(); i++){
+            double time = vec[i].second;
+
+            while(!s.empty() && time >= s.top()){
+                s.pop();
             }
+
+            s.push(time);
         }
-        
-        return result;
+        return s.size();
     }
 };
+
