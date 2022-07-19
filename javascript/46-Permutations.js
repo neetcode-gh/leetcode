f4e93864294d8d8e43feaff28c400c9fef82a11a
aakhtar3 <aakhtar3@users.noreply.github.com>
2022-07-19 07:31:17 -0400

    refactor: js-subset

--- javascript/46-Permutations.js
+++ javascript/46-Permutations.js
@@ -1,25 +1,66 @@
-function permute(nums) {
-    const res = [];
+/**
+ * https://leetcode.com/problems/permutations/solution/
+ * Time O(N!) | Space(N!)
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+ var permute = function(nums) {
+    return dfs(nums)
+}
 
-    if (nums.length === 1) {
-        return [nums.slice()];
-    }
+var dfs = function(nums, permutation = [], permutations = []) {
+    const isBaseCase = nums.length === permutation.length
+    if (isBaseCase) return permutations.push(permutation.slice())
 
-    for (const i of nums) {
-        let n = nums.shift();
+    for (let i = 0; i < nums.length; i++) {
+        if (permutation.includes(nums[i])) continue;
 
-        let perms = permute(nums);
+        backTrack(nums, i, permutation, permutations);
+     }
 
-        for (const perm of perms) {
-            perm.push(n);
+    return permutations;
+}
+
+const backTrack = (nums, i, permutation, permutations) => {
+    permutation.push(nums[i])
+        dfs(nums, permutation, permutations)
+    permutation.pop()
+}
+
+/**
+ * https://leetcode.com/problems/permutations/solution/
+ * Time O(N!) | Space(N!)
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    return bfs(nums)
+}
+
+const bfs = (nums, levels = [[]], permutations = []) => {
+    for (const num of nums) {
+        for (let i = (levels.length - 1); 0 <= i; i--) {
+            const previousLevel = levels.shift()
+
+            for (let index = 0; index < (previousLevel.length + 1); index++) {
+                const level = reArrangeSet(previousLevel, num, index)
+
+                const isBaseCase = level.length === nums.length;
+                if (isBaseCase) {
+                    permutations.push(level);
+                    continue
+                }
+
+                levels.push(level)
+            }
         }
-
-        perms.forEach((perm) => {
-            res.push(perm);
-        });
-
-        nums.push(n);
     }
 
-    return res;
+    return permutations
+}
+
+const reArrangeSet = (previousLevel, num, index) => {
+    const [ before, after ] = [ previousLevel.slice(0, index), previousLevel.slice(index) ]
+
+    return [...before, num, ...after]
 }
