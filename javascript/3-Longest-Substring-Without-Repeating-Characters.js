007561b483017e08eaa99db03a706918e4b70c55
boussarilatif <boussarilatif@gmail.com>
2022-07-20 20:43:50 -0700

    feat: add solution for Longest Substring Without Repeating Characters

--- javascript/3-Longest-Substring-Without-Repeating-Characters.js
+++ javascript/3-Longest-Substring-Without-Repeating-Characters.js
@@ -20,3 +20,53 @@ var lengthOfLongestSubstring = function (str) {
   }
   return max;
 };
+
+
+/**
+ * @param {string} str
+ * @return {number}
+ */
+ var lengthOfLongestSubstring = function(str) {
+
+  let set = new Set()
+  let max = 0;
+  
+  let firstLetterTracker = 0
+  for(let i = 0; i < str.length; i++) {
+      const letter = str[i]
+      
+      while(set.has(letter)){
+        const firstLetter = str[firstLetterTracker]
+        set.delete(firstLetter)
+        firstLetterTracker++
+      }
+
+      set.add(letter)
+
+      max = Math.max(max,i - firstLetterTracker + 1)   
+  }
+  
+  return max;
+};
+
+
+/**
+ * @param {string} str
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(str) {
+  let windowStart = 0, maxLength = 0, charIndexMap = {};
+
+  for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
+    const rightChar = str[windowEnd];
+
+    if (rightChar in charIndexMap) {
+      windowStart = Math.max(windowStart, charIndexMap[rightChar] + 1);
+    }
+
+    charIndexMap[rightChar] = windowEnd;
+    maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+  }
+
+  return maxLength;
+}
