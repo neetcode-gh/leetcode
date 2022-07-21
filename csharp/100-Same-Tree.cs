4ef4b6e3386358bf0d0bfa227d384006252050c0
notauserx <notauserx@users.noreply.github.com>
2022-07-21 12:42:31 +0600

    fix formatting based on conventions used in the repo

--- csharp/100-Same-Tree.cs
+++ csharp/100-Same-Tree.cs
@@ -11,16 +11,11 @@
  *     }
  * }
  */
-public class Solution
-{
-
-    public bool IsSameTree(TreeNode p, TreeNode q)
-    {
+public class Solution {
+    public bool IsSameTree(TreeNode p, TreeNode q) {
         if (p == null || q == null)
-        {
             return p == q;
-        }
-
+        
         return
             p.val == q.val &&
             IsSameTree(p.left, q.left) &&
