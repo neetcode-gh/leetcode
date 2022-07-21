c2346fe3e957b4e210741110ac85459e70af06a0
notauserx <notauserx@users.noreply.github.com>
2022-07-21 11:27:12 +0600

    Create 100-Same-Tree.cs

---
+++ csharp/100-Same-Tree.cs
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+
+    public bool IsSameTree(TreeNode p, TreeNode q)
+    {
+        if (p == null || q == null)
+        {
+            return p == q;
+        }
+
+        return
+            p.val == q.val &&
+            IsSameTree(p.left, q.left) &&
+            IsSameTree(p.right, q.right);
+    }
+}
