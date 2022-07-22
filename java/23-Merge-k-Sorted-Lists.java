aa9fa7207e38d10f82b56c835cbf748b549b4a2f
KC10201 <70864069+KC10201@users.noreply.github.com>
2022-07-21 23:02:42 -0400

    Update 23-Merge-k-Sorted-Lists.java
    Add alternative solution utilizing merge sort rather than min heap.

--- java/23-Merge-k-Sorted-Lists.java
+++ java/23-Merge-k-Sorted-Lists.java
@@ -1,3 +1,7 @@
+//  Solution using Min Heap 
+//  Time Complexity:         O(n*log(k))
+//  Extra Space Complexity:  O(n)
+
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         
@@ -20,5 +24,52 @@ class Solution {
         }
         
         return dummy.next;
+    }
+}
+
+//  Solution using Iterative Merge Sort
+//  Time Complexity:         O(n*log(k))
+//  Extra Space Complexity:  O(1)
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        int size = lists.length;
+        int interval = 1;
+        
+        while(interval < size) {
+            for(int i = 0; i < size - interval; i += 2 * interval) {
+                lists[i] = merge(lists[i], lists[i + interval]);
+            }
+            
+            interval *= 2;
+        }
+        
+        return size > 0? lists[0] : null;
+    }
+    
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        
+        while(l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            
+            curr = curr.next;
+        }
+        
+        if (l1 != null) {
+            curr.next = l1;
+        } else {
+            curr.next = l2;
+        }
+        
+        return dummy.next;
+        
     }
 }
