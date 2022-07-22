13f5087e6379a937e5987264f11043879de87b57
Akhil Rajan <rajanakhil07@gmail.com>
2022-07-22 23:29:35 +0530

    c++ code on the webiste fails for ["zy","zx"] testcase
    my new c++ solution for alien_dictionary.cpp

--- cpp/neetcode_150/12_advanced_graphs/alien_dictionary.cpp
+++ cpp/neetcode_150/12_advanced_graphs/alien_dictionary.cpp
@@ -9,82 +9,83 @@
     Space: O(n)
 */
 
-// create graph or adjacency list
-        unordered_map<char, unordered_set<char>> graph; // set to avoid duplicacy
+class Solution {
+public:
+    string alienOrder(vector<string> &words) {
+        
+        unordered_map<char, unordered_set<char>> graph;
         unordered_map<char, int> indegree;
 
-        for(auto word: words){
-            for(auto c : word){
-                indegree[c] = 0;
+        // indegree make all char 0
+        for(auto word : words){
+            for(auto c : st){
+                indegree[c]=0;
             }
         }
 
         for(int i=0; i<words.size()-1; i++){
+            string curr = words[i];
+            string next = words[i+1];
             
             bool flag = false;
-            string s1 = words[i];
-            string s2 = words[i+1];
-
-            int len = min(s1.length(), s2.length());
-
+            int len = min(curr.length(), next.length());
             for(int j=0; j<len; j++){
+                char ch1 = curr[j];
+                char ch2 = next[j];
 
-                char ch1 = s1[j];
-                char ch2 = s2[j];
-
-                // if the char do not match
                 if(ch1 != ch2){
-
                     unordered_set<char> set;
+
                     if(graph.find(ch1) != graph.end()){
-                        
                         set = graph[ch1];
 
                         if(set.find(ch2) == set.end()){
                             set.insert(ch2);
-                            graph[ch1] = set;
                             indegree[ch2]++;
+                            graph[ch1] = set;
                         }
-        
                     }
                     else{
                         set.insert(ch2);
-                        graph[ch1] = set;
                         indegree[ch2]++;
+                        graph[ch1] = set;
                     }
 
                     flag = true;
                     break;
                 }
+                
             }
-            if(flag == false and (s1.length() > s2.length())) return "";
+
+            if(flag == false and (curr.length() > next.length())) return "";
         }
 
-        queue<char> pq;
+        priority_queue<char, vector<char>, greater<char>> q;
 
-        for(auto i : indegree){
-            if(i.second == 0) pq.push(i.first);
+        for(auto it : indegree){
+            if(it.second == 0){
+                //cout<<it.first<<endl;
+                q.push(it.first);
+            }
         }
 
+        int count=0;
         string ans = "";
-        int count = 0;
-        while(pq.size()>0){
 
-            auto rem = pq.front();
-            pq.pop();
+        while(q.size()>0){
+            auto rem = q.top();
+            q.pop();
 
             ans += rem;
             count++;
 
-            if(graph[rem].empty()) continue;
-
             if(graph.find(rem) != graph.end()){
                 unordered_set<char> nbrs = graph[rem];
 
-                for(char nbr : nbrs){
+                for(auto nbr : nbrs){
                     indegree[nbr]--;
                     if(indegree[nbr] == 0){
-                        pq.push(nbr);
+                        q.push(nbr);
                     }
                 }
             }
@@ -94,3 +95,5 @@
             return ans;
         }
         return "";
+    }
+};
