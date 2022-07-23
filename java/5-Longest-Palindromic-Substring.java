92aaa699581e7828e4686c4f5d31eb481f9af18f
KC10201 <70864069+KC10201@users.noreply.github.com>
2022-07-23 14:02:14 -0400

    Update 5-Longest-Palindromic-Substring.java

--- java/5-Longest-Palindromic-Substring.java
+++ java/5-Longest-Palindromic-Substring.java
@@ -90,19 +90,19 @@ class Solution3 {
         
         var isPalindrome = new boolean[len][len];
         
-        for(int i = len-1; i >= 0; i--) {
-            for(int j = i; j < len; j++) {
-                if(i == j) {
+        for (int i = len-1; i >= 0; i--) {
+            for (int j = i; j < len; j++) {
+                if (i == j) {
                     isPalindrome[i][j] = true;
-                } else if(s.charAt(i) == s.charAt(j)) {
-                    if(j - i == 1) {
+                } else if (s.charAt(i) == s.charAt(j)) {
+                    if (j - i == 1) {
                         isPalindrome[i][j] = true;
                     } else {
                         isPalindrome[i][j] = isPalindrome[i+1][j-1];
                     }
                 }
                 
-                if(isPalindrome[i][j] && j-i+1 > max) {
+                if (isPalindrome[i][j] && j-i+1 > max) {
                     max = j-i+1;
                     left = i;
                     right = j+1;
