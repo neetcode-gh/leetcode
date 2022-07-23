39e839831eabac8fb4da71101f41a8af1a914c26
MaratKhakim <marakhakim@gmail.com>
2022-07-23 22:15:42 +0200

    200 Number of Islands Union Find solution (#366)

--- java/200-Number-of-Islands.java
+++ java/200-Number-of-Islands.java
@@ -29,4 +29,80 @@ class Solution {
         dfs(grid, i - 1, j);
         dfs(grid, i, j - 1);
     }
+
+    public int numIslandsUnionFind(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        UnionFind uf = new UnionFind(grid);
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (grid[i][j] == '1') {
+                    for (int[] dir : dirs) {
+                        int x = dir[0] + i;
+                        int y = dir[1] + j;
+
+                        if (isSafe(grid, x, y)) {
+                            int id1 = i*n + j;
+                            int id2 = x*n + y;
+
+                            uf.union(id1, id2);
+                        }
+                    }
+                }
+
+        return uf.count();
+    }
+
+    private boolean isSafe(char[][] grid, int x, int y) {
+        int m = grid.length;
+        int n = grid[0].length;
+        return x >= 0 && x < m && y >= 0 && y < n && grid[x][y] != '0';
+    }
+
+    class UnionFind {
+        private int count;
+        private int[] parent, size;
+
+        public UnionFind(char[][] grid) {
+            int m = grid.length, n = grid[0].length;
+            parent = new int[m*n];
+
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < n; j++) {
+                    if (grid[i][j] == '1') {
+                        int id = i*n + j;
+                        parent[id] = id;
+                        count++;
+                    }
+                }
+        }
+
+        public int find(int p) {
+            while(p != parent[p]) {
+                parent[p] = parent[parent[p]];
+                p = parent[p];
+            }
+
+            return p;
+        }
+
+        public boolean union(int p, int q) {
+            int root1 = find(p);
+            int root2 = find(q);
+
+            if (root1 == root2)
+                return false;
+
+            parent[root1] = root2;
+            count--;
+
+            return true;
+        }
+
+        public int count() {
+            return count;
+        }
+    }
 }
