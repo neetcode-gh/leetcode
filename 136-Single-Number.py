8ee9c786457c5a326600b50196c9017fda99e735
Mitch Irvin <mitchellirvin@google.com>
2022-07-23 14:43:51 -0400

    removing root level python sols in favor of the python/ dir

--- 136-Single-Number.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        res = 0
-        for n in nums:
-            res = n ^ res
-        return res
--- 55-Jump-Game.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        goal = len(nums) - 1
-
-        for i in range(len(nums) - 2, -1, -1):
-            if i + nums[i] >= goal:
-                goal = i
-        return goal == 0
