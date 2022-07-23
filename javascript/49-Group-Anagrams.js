b1f182ab9a6e10dbdfea27c8ba95a6f06b9e77bf
Rutanshu Jhaveri <19225402+rutanshuj@users.noreply.github.com>
2022-07-23 18:58:16 -0400

    Update 49-Group-Anagrams.js

--- javascript/49-Group-Anagrams.js
+++ javascript/49-Group-Anagrams.js
@@ -5,36 +5,6 @@
 // This solution is faster than sorting each word.
 //////////////////////////////////////////////////////////////////////////////
 
-/** @const {!Object<string, number>} */
-const CODES = {
-    a: 0,
-    b: 1,
-    c: 2,
-    d: 3,
-    e: 4,
-    f: 5,
-    g: 6,
-    h: 7,
-    i: 8,
-    j: 9,
-    k: 10,
-    l: 11,
-    m: 12,
-    n: 13,
-    o: 14,
-    p: 15,
-    q: 16,
-    r: 17,
-    s: 18,
-    t: 19,
-    u: 20,
-    v: 21,
-    w: 22,
-    x: 23,
-    y: 24,
-    z: 25,
-};
-
 /**
  * @param {string[]} words
  * @return {string[][]}
@@ -63,7 +33,7 @@ function groupAnagrams(words) {
 function hashWord(word) {
     const hash = new Array(26).fill(0);
     for (const ch of word) {
-        ++hash[CODES[ch]];
+        ++hash[ch.charCodeAt(0) - 'a'.charCodeAt(0)];
     }
     return hash.toString();
 }
