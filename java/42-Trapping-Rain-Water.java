9e544ae00584ac618beba25c88813672b63eccb3
milad <miladranjbari@gmail.com>
2022-07-24 19:42:56 +0430

    Update 1143, 252, 42 (#365)
    * Add 1143 Longest Common Subsequence
    
    * Delete 1143 beacuse It has conflict
    
    * Add Itrative version of Longest Common Subsequence
    
    * Add 252 meeting rooms
    
    * Add another approach fot meeting rooms
    
    * Update 42 trapping rain water with O(1) space
    
    * Update 1143-Longest-Common-Subsequence.java
    
    * Fix meeting rooms
    
    Co-authored-by: Mitchell Irvin <16233245+mitchellirvin@users.noreply.github.com>

--- java/42-Trapping-Rain-Water.java
+++ java/42-Trapping-Rain-Water.java
@@ -21,3 +21,30 @@ class Solution {
         return c;
     }
 }
+
+//O(1) space
+class Solution {
+    public int trap(int[] heights) {
+
+        if (heights.length == 0) return 0;
+
+        int l = 0, r = heights.length - 1;
+        int leftMax = heights[l], rightMax = heights[r];
+        int res = 0;
+
+        while (l < r) {
+            if (leftMax < rightMax) {
+                l++;
+                leftMax = Math.max(leftMax, heights[l]);
+                res += leftMax - heights[l];
+            } else {
+                r--;
+                rightMax = Math.max(rightMax, heights[r]);
+                res += rightMax - heights[r];
+            }
+        }
+
+        return res;
+
+    }
+}
