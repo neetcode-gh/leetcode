9ee15d31e7fd524f2afa32b419ee860984dd13fe
Alec Wang <alswang18@gmail.com>
2022-07-23 18:42:01 -0700

    added new python solution for lc-74

--- python/74-Search-a-2D-Matrix.py
+++ python/74-Search-a-2D-Matrix.py
@@ -1,27 +1,19 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        ROWS, COLS = len(matrix), len(matrix[0])
+        ROWS = len(matrix)
 
-        top, bot = 0, ROWS - 1
-        while top <= bot:
-            row = (top + bot) // 2
-            if target > matrix[row][-1]:
-                top = row + 1
-            elif target < matrix[row][0]:
-                bot = row - 1
+        for i in range(ROWS):
+            if target > matrix[i][-1] or target < matrix[i][0]:
+                continue
             else:
-                break
-
-        if not (top <= bot):
-            return False
-
-        l, r = 0, COLS - 1
-        while l <= r:
-            m = (l + r) // 2
-            if target > matrix[row][m]:
-                l = m + 1
-            elif target < matrix[row][m]:
-                r = m - 1
-            else:
-                return True
+                arr = matrix[i]
+                l, r = 0, len(arr) - 1
+                while l <= r:
+                    m = (l + r) // 2
+                    if target > arr[m]:
+                        l = m + 1
+                    elif target < arr[m]:
+                        r = m - 1
+                    else:
+                        return True
         return False
