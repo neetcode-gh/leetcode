56fdebc72b189bcf7e7d0ab246561d1bedfe5d51
Dmitriy Buzulutskiy <fbdm@technodom.kz>
2022-07-25 09:32:18 +0600

    Create 136-Single-Number.swift

---
+++ swift/136-Single-Number.swift
@@ -0,0 +1,12 @@
+class Solution {
+    func singleNumber(_ nums: [Int]) -> Int {
+        var hm: [Int: Int]  = [:]
+        for i in nums {
+            hm[i,default: 0] += 1
+        }
+        for i in nums {
+            if hm[i] == 1 { return i }
+        }
+        return -1
+    }
+}
