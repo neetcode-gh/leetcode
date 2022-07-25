e31138cb17a32a8b6a717db72dfe47490b36f5cc
Jason Pullara <lordkat@lordkat.com>
2022-07-25 08:31:25 -0400

    Update 238-Product-of-Array-Except-Self.java
    Added solution set that doesn't accumulate extra memory, uses nums[] as prefix table.

--- java/238-Product-of-Array-Except-Self.java
+++ java/238-Product-of-Array-Except-Self.java
@@ -3,18 +3,35 @@
 //In first pass calculate the left product except self and in second calculate the right 
 
 class Solution {
+	public int[] productExceptSelf(int[] nums) {
+		int[] arr = new int[nums.length];
+		int right = 1, left = 1;
+		for (int i = 0; i < nums.length; i++) {
+			arr[i] = left;
+			left *= nums[i];
+		}
+		for (int i = nums.length - 1; i >= 0; i--) {
+			arr[i] *= right;
+			right *= nums[i];
+		}
+		return arr;
+	}
 
-    public int[] productExceptSelf(int[] nums) {
-        int[] arr = new int[nums.length];
-    	int right=1, left=1;
-    	for (int i = 0; i < nums.length; i++) {
-    		arr[i] = left;
-    		left *= nums[i];
-    	}
-    	for(int i = nums.length - 1; i >= 0; i--) {
-    		arr[i] *= right;
-    		right *= nums[i];
-    	}
-    	return arr;
-    }
+	// using nums[] as the prefix calculation table
+	public int[] productExceptSelfNumsAsPrefix(int[] nums) {
+		int[] output = new int[nums.length];
+		output[0] = 1; // default prefix is 1.
+
+		// prefix
+		for (int i = 0; i < nums.length - 1; i++)
+			output[i + 1] = output[i] * nums[i];
+
+		// postfix
+		// we start at nums.length - 2 because multiplying by 1 is silly.
+		for (int i = nums.length - 2; i >= 0; i--) {
+			output[i] = nums[i + 1] * output[i];
+			nums[i] = nums[i] * nums[i + 1];
+		}
+		return output;
+	}
 }
