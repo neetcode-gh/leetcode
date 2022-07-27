51beca96d0f2ff8f9c1265ae674231d49e5be976
Florian <florianlap2@gmail.com>
2022-07-27 10:59:22 +0200

    Delete 746-Min-Cost-Climbing-Stairs.py

--- 746-Min-Cost-Climbing-Stairs.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def minCostClimbingStairs(self, cost: List[int]) -> int:
-        for i in range(len(cost) - 3, -1, -1):
-            cost[i] += min(cost[i + 1], cost[i + 2])
-            
-        return min(cost[0], cost[1])
