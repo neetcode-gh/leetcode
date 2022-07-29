2e92d687dae1cb9be42f27c649e7badb456a2687
Yannick Pferr <yannick@pferr.de>
2022-07-29 18:19:16 +0200

    Add java solution for problem 1899 Merge Triplets to Form Target Triplet

---
+++ java/1899-Merge-Triplets-to-Form-Target-Triplet.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean mergeTriplets(int[][] triplets, int[] target) {
+        boolean[] greedy = new boolean[3];
+        loop:
+        for (int[] triplet : triplets) {
+            for (int i = 0; i < 3; i++)
+                if (triplet[i] > target[i])
+                    continue loop;
+
+            for (int i = 0; i < 3; i++)
+                if (triplet[i] == target[i])
+                    greedy[i] = true;
+        }
+
+        return greedy[0] && greedy[1] && greedy[2];
+    }
+}
