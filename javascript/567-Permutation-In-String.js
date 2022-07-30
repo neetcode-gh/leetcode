80d92b9a845a97168324f19919a417f53cba4bea
aakhtar3 <aakhtar3@users.noreply.github.com>
2022-07-30 16:02:29 -0400

    refactor: js - slidewindow (#454)

--- javascript/567-Permutation-In-String.js
+++ javascript/567-Permutation-In-String.js
@@ -1,3 +1,76 @@
+/**
+ * https://leetcode.com/problems/permutation-in-string/
+ * Time O(N + (M - N)) | Space O(1)
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = (s1, s2) => {
+    const isInvalid = s2.length < s1.length
+    if (isInvalid) return false;
+    
+    let [ left, right ] = [ 0, 0 ];
+    const [ s1FrequencyMap, s2FrequencyMap ] = getFrequencyMaps(s1);
+    
+    while (right < s2.length) {
+        addRightFrequency(s2, right, s2FrequencyMap);
+
+        const window = (right - left) + 1;
+        const isPermutation = (window === s1.length) && isSame(s1FrequencyMap, s2FrequencyMap);
+        if (isPermutation) return true;
+
+        const canSlide = s1.length <= window;
+        if (canSlide) {
+            subtractLeftFrequency(s2, left, s2FrequencyMap);
+            left++;
+        }
+        
+        right++;
+    }
+    
+    return false;
+}
+
+const getFrequencyMaps = (s1) => {
+    const [ s1FrequencyMap, s2FrequencyMap ] = new Array(2)
+        .fill().map(() => new Array(26).fill(0))
+
+    for (const char of s1) s1FrequencyMap[getCode(char)]++;
+
+    return [ s1FrequencyMap, s2FrequencyMap ]
+}
+
+
+const getCode = (char) => char.charCodeAt(0) - 'a'.charCodeAt(0);
+
+const addRightFrequency = (s, right, frequencyMap) => {
+    const char = s[right];
+    const index = getCode(char);
+
+    frequencyMap[index]++;
+    
+    return frequencyMap[index];
+}
+
+const subtractLeftFrequency = (s, left, frequencyMap) => {
+    const char = s[left];
+    const index = getCode(char);
+
+    frequencyMap[index]--;
+    
+    return frequencyMap[index];
+}
+
+const isSame = (a, b) => {
+    for (let i = 0; i < 26; i++) {
+        const isMatch = a[i] === b[i];
+        if (!isMatch) return false
+    }
+    
+    return true;
+}
+
+
 //////////////////////////////////////////////////////////////////////////////
 // Static Sliding Window
 // Time: Theta(l1 + l2) O(l1 + l2)  Space: Theta(1) O(1)
@@ -13,7 +86,7 @@
  * @param {string} s2
  * @return {boolean}
  */
-function checkInclusion(s1, s2) {
+ function checkInclusion(s1, s2) {
     if (s1.length > s2.length) {
         return false;
     }
