69cf2c4d08eb962ad5029729f711d1e29bcf920c
aakhtar3 <aakhtar3@users.noreply.github.com>
2022-07-30 16:01:05 -0400

    refactor: js - intervals (#449)

--- javascript/252-Meeting-Rooms.js
+++ javascript/252-Meeting-Rooms.js
@@ -1,14 +1,30 @@
 /**
- * @param {number[]} intervals
+ * https://leetcode.com/problems/meeting-rooms/
+ * Time O(N * logN) | Space O(1)
+ * @param {number[][]} intervals
  * @return {boolean}
  */
-var canAttendMeetings = function (intervals) {
-    intervals.sort((a, b) => a[0] - b[0]);
+ var canAttendMeetings = function(intervals) {
+    intervals.sort(([ aStart, aEnd ], [ bStart, bEnd ]) => aStart !== bStart
+        ? aStart - bStart
+        : aEnd - bEnd
+    );
 
-    for (let i = 0; i < intervals.length - 1; i++) {
-        if (intervals[i][1] > intervals[i + 1][0]) {
-            return false;
-        }
-    }
-    return true;
+    return canAttend(intervals)
 };
+
+const canAttend = (intervals) => {
+    let prev = intervals.shift();
+
+    for (const curr of intervals) {
+        const [ prevStart, prevEnd ] = prev;
+        const [ currStart, currEnd ] = curr;
+
+        const hasOverlap  = currStart < prevEnd;
+        if (hasOverlap) return false;
+
+        prev = curr;
+    }
+
+  return true;
+}
