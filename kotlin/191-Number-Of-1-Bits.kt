b24dc126f4a5af58818e18af5d4a995064df59cb
Sharan Salian <9111022+sharansalian@users.noreply.github.com>
2022-08-02 18:59:07 +0530

    Add Class Solution

--- kotlin/191-Number-Of-1-Bits.kt
+++ kotlin/191-Number-Of-1-Bits.kt
@@ -1,12 +1,16 @@
 package kotlin
 
-fun hammingWeight(n: Int): Int {
-    var n = n
-    var count = 0
-    for (i in 0 .. 31){
-        if(n and 1 == 1) count++
-        n = n shr 1
+class Solution {
+    // you need treat n as an unsigned value
+    fun hammingWeight(n:Int):Int {
+        
+        var n = n 
+        var count = 0
+        for(i in 0..31){
+            if( n and 1 == 1) count++
+            n = n shr 1
+        }
+        
+        return count
     }
-
-    return count
 }
