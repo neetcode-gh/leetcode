08e672a79a56f0723a006ae3d1f0b0dacbe8ce0d
SubhadeepDas <subhadeepdasasd@gmail.com>
2022-08-05 00:10:42 +0530

    Adding 155-Min-Stack.kt

--- kotlin/155-Min-Stack.kt
+++ kotlin/155-Min-Stack.kt
@@ -1,25 +1,27 @@
-class MinStack() {
+import kotlin.math.min
 
-    /** initialize your data structure here. */
-    var m = MutableList(0){0}
-    init{
-        this.m = m
-    }
+class MinStack() {
+    private class Node(var `val`: Int, var min: Int, var next: Node?)
+
+    private var head: Node? = null
 
     fun push(x: Int) {
-        this.m.add(x)
+        if(head==null) head = Node(x,x,null) else head = Node(x, min(x,head?.min!!),head)
     }
 
     fun pop() {
-        this.m.removeAt(this.m.size-1)
+        head = head?.next
+
     }
 
-    fun top(): Int {
-        return this.m[this.m.size-1]
+    fun top(): Int? {
+        return head?.`val`
+
     }
 
-    fun getMin(): Int {
-        return this.m.min()!!
+    fun getMin(): Int? {
+        return head?.min
+
     }
 
 }
