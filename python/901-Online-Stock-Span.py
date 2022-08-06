5d5abf7f311f54e0a4fdecc5a44b80d1c9742ad3
neetcode-gh <77742485+neetcode-gh@users.noreply.github.com>
2022-08-06 16:33:34 -0700

    Create 901-Online-Stock-Span.py

---
+++ python/901-Online-Stock-Span.py
@@ -0,0 +1,12 @@
+class StockSpanner:
+
+    def __init__(self):
+        self.stack = [] # pair: (price, span)
+
+    def next(self, price: int) -> int:
+        span = 1
+        while self.stack and self.stack[-1][0] <= price:
+            span += self.stack[-1][1]
+            self.stack.pop()
+        self.stack.append((price, span))
+        return span
