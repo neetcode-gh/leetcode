984eaa78940fefa597a6a548ab621f35decfc4d0
Zhiyu Yan <31463079+callmefish@users.noreply.github.com>
2022-08-10 17:08:58 -0400

    Update 128. Longest Consecutive Sequence.java
    Previous version cannot pass the test case [-1000000000,1,2,3,9,1000000000]

--- java/128. Longest Consecutive Sequence.java
+++ java/128. Longest Consecutive Sequence.java
@@ -1,34 +1,20 @@
 class Solution {
 
     public int longestConsecutive(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        int max = -1;
-        int min = 1000000000;
-
-        // Find min and max from array & add each to set
-        for (int i : nums) {
-            set.add(i);
-            max = Math.max(max, i);
-            min = Math.min(min, i);
-        }
-
-        int c = 0; // count streak
-        int res = 0; // longest streak
-        for (int i = min; i <= max; ++i) {
-            // Check if set contains ith value; increment count & remove from set
-            if (set.contains(i)) {
-                c++;
-                set.remove(i);
-            } else {
-                // If not found set count to 0
-                c = 0;
+        Set<Integer> memory = new HashSet<Integer>();
+        for(int num: nums) memory.add(num);
+        int ans = 0;
+        for (int num: nums){
+            int cnt = 0;
+            if (!memory.contains(num-1)){
+                cnt++;
+                int tmp = num+1;
+                while (memory.contains(tmp++)){
+                    cnt++;
+                }
             }
-            // Find longest streak at every step in case we break out from loop
-            res = Math.max(res, c);
-
-            // If set size is less than current longest streak break as we wont get any longer streak
-            if (set.size() <= res && c == 0) break;
+            ans = Math.max(ans, cnt);
         }
-        return res;
+        return ans;
     }
 }
