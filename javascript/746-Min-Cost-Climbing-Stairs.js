d745a6cd8355314f0753ba3b7ecc66ffc31b5816
Tyler Shanks <86684183+tylershanks@users.noreply.github.com>
2022-08-20 15:36:56 -0400

    Adding js solution for 746-Min-Cost-Climbing-Stairs.js

--- javascript/746-Min-Cost-Climbing-Stairs.js
+++ javascript/746-Min-Cost-Climbing-Stairs.js
@@ -1,9 +1,11 @@
-function minCostClimbingStairs(cost) {
-    cost.push(0);
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
 
-    for (let i = cost.length - 3; i > -1; i--) {
-        cost[i] += Math.min(cost[i + 1], cost[i + 2]);
+let minCostClimbingStairs = (cost) => {    
+    for (let i = 2; i < cost.length; i++){
+        cost[i] = Math.min(cost[i-1] + cost[i], cost[i-2] + cost[i])
     }
-
-    return Math.min(cost[0], cost[1]);
+    return(Math.min(cost[cost.length-1], cost[cost.length-2]))
 }
