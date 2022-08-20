e204a2c2e557cda4bfa88495d487a580ed86c2ae
Pontus Magnusson <pontus.magnusson@soderbergpartners.se>
2022-08-21 00:44:50 +0200

    Add csharp solution for 66 Plus One

---
+++ csharp/66-Plus-One.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int[] PlusOne(int[] digits) {
+        string n = string.Join("", digits);
+        n = (BigInteger.Parse(n) + 1).ToString();
+        int[] result = new int[n.Length];
+        for ( int i = 0; i< n.Length; i++)
+        {
+            result[i] = n[i] - '0';
+        }
+        return result;
+    }
+}
