54ceaadfd3910ac20ee4a785373aed16098ada7c
Sam Bakkach <bakkach.abdessamad@gmail.com>
2022-08-21 15:03:01 +0100

    merge heaps solution inside old one's file

--- javascript/295-Find-Median-from-Data-Stream.js
+++ javascript/295-Find-Median-from-Data-Stream.js
@@ -1,5 +1,45 @@
 
-var MedianFinder = function() {
+// Solution using heaps
+const MedianFinder = function () {
+    this.small = new MaxPriorityQueue();
+    this.large = new MinPriorityQueue();
+};
+
+/**
+ * @param {number} num
+ * @return {void}
+ */
+MedianFinder.prototype.addNum = function (num) {
+    this.small.enqueue(num);
+    // Ensure every elem in small is <= every num in large
+    if (!this.small.isEmpty() && !this.large.isEmpty()
+    && this.small.front().element > this.large.front().element) {
+        this.large.enqueue(this.small.dequeue().element);
+    }
+    // If size diff is > 1 => we should balance the two heaps
+    if (this.small.size() > this.large.size() + 1) {
+        this.large.enqueue(this.small.dequeue().element);
+    } else if (this.large.size() > this.small.size() + 1) {
+        this.small.enqueue(this.large.dequeue().element);
+    }
+
+};
+
+/**
+ * @return {number}
+ */
+MedianFinder.prototype.findMedian = function () {
+    if (this.small.size() === this.large.size()) {
+        return (this.small.front().element + this.large.front().element) / 2;
+    }
+
+    return this.small.size() > this.large.size() ? this.small.front().element : this.large.front().element;
+};
+
+
+// Other solution
+
+var MedianFinder2 = function() {
     this.array = [];
 };
 
@@ -7,7 +47,7 @@ var MedianFinder = function() {
  * @param {number} num
  * @return {void}
  */
-MedianFinder.prototype.addNum = function(num) {
+MedianFinder2.prototype.addNum = function(num) {
     var low = 0 ; 
     var high = this.array.length - 1;
     
@@ -27,7 +67,7 @@ MedianFinder.prototype.addNum = function(num) {
 /**
  * @return {number}
  */
-MedianFinder.prototype.findMedian = function() {
+MedianFinder2.prototype.findMedian = function() {
     if (this.array.length % 2 === 0) {
         var mid = this.array.length / 2;
         return (this.array[mid] + this.array[mid - 1]) / 2;
@@ -38,8 +78,8 @@ MedianFinder.prototype.findMedian = function() {
 };
 
 /** 
- * Your MedianFinder object will be instantiated and called as such:
- * var obj = new MedianFinder()
+ * Your MedianFinder2 object will be instantiated and called as such:
+ * var obj = new MedianFinder2()
  * obj.addNum(num)
  * var param_2 = obj.findMedian()
  */
