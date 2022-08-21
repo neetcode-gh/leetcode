08f677a05c608c8963c6c6e19821c4fb2523712e
Rutanshu Jhaveri <19225402+rutanshuj@users.noreply.github.com>
2022-08-20 20:53:55 -0400

    Update 121-Best-Time-to-Buy-and-Sell-Stock.js

--- javascript/121-Best-Time-to-Buy-and-Sell-Stock.js
+++ javascript/121-Best-Time-to-Buy-and-Sell-Stock.js
@@ -19,3 +19,27 @@ var maxProfit = function(prices) {
 
     return max;
 }
+
+
+/**
+ * Another approach without using sliding window
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+ * Time O(N) | Space O(1)
+ * @param {number} prices
+ * @return {number}
+*/
+
+var maxProfit = function(prices) {
+    let min = prices[0];
+    let max = min;
+    let value = 0;
+    for(let i = 0; i<prices.length; i++){
+        if(i != prices.length-1 && prices[i] <= min){
+            max = min = prices[i];
+        } else if(prices[i] > max){
+            max = prices[i];
+        }
+        value = (max-min>value) ? max-min : value;
+    }
+    return value;
+};
