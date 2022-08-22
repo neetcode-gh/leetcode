10bfb1576f81e468811676764eef8c6e3b801475
Vitali-Matteo <111738770+Vitali-Matteo@users.noreply.github.com>
2022-08-22 13:42:08 +0200

    Create 63_Unique_Paths_II.py

---
+++ python/63_Unique_Paths_II.py
@@ -0,0 +1,44 @@
+#Dynamic Programming Solution with Memoization
+
+class Solution:
+    
+    def uniquePathsWithObstacles(self, obstacleGrid) -> int:  
+        
+        self.Memo = {}
+        
+        return self.Unique(obstacleGrid)
+    
+    def Unique(self, obstacleGrid, m = 0, n = 0) -> int:
+        
+        if (m, n) in self.Memo:
+            
+            return self.Memo[(m, n)]
+        
+        if obstacleGrid[0][0] == 1:
+            
+            return 0
+        
+        if m == len(obstacleGrid) - 1 and n == len(obstacleGrid[0]) - 1:
+            
+            if obstacleGrid[m][n] != 1:
+
+                return 1
+
+            else:
+
+                return 0
+
+        Tot = 0
+        
+        if m + 1 < len(obstacleGrid) and obstacleGrid[m + 1][n] != 1:
+            
+            Tot = Tot + self.Unique(obstacleGrid, m + 1, n)
+            
+        if n + 1 < len(obstacleGrid[0]) and obstacleGrid[m][n + 1] != 1:
+            
+            Tot = Tot + self.Unique(obstacleGrid, m, n + 1)
+            
+        self.Memo[(m, n)] = Tot
+
+        return Tot
+    
