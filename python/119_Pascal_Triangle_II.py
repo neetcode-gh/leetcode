47ea81c0c80710a632f53e8e45624253eae3dcb2
Matteo <111738770+Vitali-Matteo@users.noreply.github.com>
2022-08-22 14:44:03 +0200

    Create 119_Pascal_Triangle_II.py

---
+++ python/119_Pascal_Triangle_II.py
@@ -0,0 +1,28 @@
+class Solution:
+    
+    Memo = {}
+    
+    def getRow(self, rowIndex: int) -> List[int]:
+        
+        if rowIndex in self.Memo:
+            
+            return self.Memo[rowIndex]
+
+        if rowIndex == 0:
+
+            return [1]
+
+        ListPrec = self.getRow(rowIndex - 1)
+
+        Result = [1]
+
+        for i in range(0, len(ListPrec) - 1):
+
+            Result.append(ListPrec[i] + ListPrec[i + 1])
+
+        Result.append(1)
+        
+        self.Memo[rowIndex] = Result
+
+        return Result
+        
