77d48d206e3dd17824efe83b588c95833301af8b
Dushyant <devops@bydushyant.com>
2022-08-23 15:51:36 +0530

    Solving Valid Anagram
    - Using javascript map object, this will be simpler and similar solution as Python

--- javascript/242-Valid-Anagram.js
+++ javascript/242-Valid-Anagram.js
@@ -2,29 +2,24 @@
  * @param {string} s
  * @param {string} t
  * @return {boolean}
+ * 
+ * 
+ * complexity
+ * Time  O(2n)
+ * Space O(2n)
+ * 
+ * Runtime: 146 ms, faster than 29.80% of JavaScript online submissions for Valid Anagram.
+ * Memory Usage: 50 MB, less than 5.51% of JavaScript online submissions for Valid Anagram.
  */
-var isAnagram = function (s, t) {
-    if (s.length !== t.length) {
-        return false;
+ var isAnagram = function(s, t) {
+    if(s.length !== t.length) return false;
+    const mapS = {}, mapT = {};
+    for(const i in s){
+        mapS[s[i]] ? mapS[s[i]] += 1 : mapS[s[i]]= 1;
+        mapT[t[i]] ? mapT[t[i]] += 1 : mapT[t[i]]= 1;
     }
-    let sMap = {};
-    let tMap = {};
-    for (let i = 0; i < s.length; i++) {
-        if (sMap.hasOwnProperty(s[i])) {
-            sMap[s[i]]++;
-        } else {
-            sMap[s[i]] = 1;
-        }
-        if (tMap.hasOwnProperty(t[i])) {
-            tMap[t[i]]++;
-        } else {
-            tMap[t[i]] = 1;
-        }
-    }
-    for (let k in sMap) {
-        if (sMap[k] !== tMap[k]) {
-            return false;
-        }
+    for(const i in mapS){
+        if (mapS[i] !== mapT[i]) return false;    
     }
     return true;
 };
