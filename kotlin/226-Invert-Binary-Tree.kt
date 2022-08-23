7f181565859f9bfcb80847fdd9be13bb5c841521
marat <marakhakim@gmail.com>
2022-08-23 20:48:11 +0200

    Kotlin: 226. Invert Binary Tree

---
+++ kotlin/226-Invert-Binary-Tree.kt
@@ -0,0 +1,22 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun invertTree(root: TreeNode?): TreeNode? {
+        if (root == null)
+            return root
+
+        val left = invertTree(root.left)
+        root.left = invertTree(root.right)
+        root.right = left
+
+        return root
+    }
+}
