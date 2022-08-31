daf8bfe463f244784245c7473b6c4a0050a6ffaa
Sourasish Das <43234998+Scorpiussmith13@users.noreply.github.com>
2022-08-31 11:31:24 +0530

    Updated 152-Maximum-Product-Subarray.cpp
    Added a previously missing conditional statement about possibility of 0 coming in the array (Line no 24). Also added comments for better clarity.

--- cpp/152-Maximum-Product-Subarray.cpp
+++ cpp/152-Maximum-Product-Subarray.cpp
@@ -12,19 +12,25 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int currMax = nums[0];
-        int currMin = nums[0];
-        int result = nums[0];
+      
+        int currMax = nums[0],currMin=nums[0],ans=nums[0]; //initializing with 1 causes bugs since line 19 stores max between ans and currMAx; Example: arr ={-2,0,-1} At the 0th index if ans = 1 then max(currMax,ans) = max(-2,1) is 1 which is wrong.
+      
         
-        for (int i = 1; i < nums.size(); i++) {
-            int temp = currMax;
+        for(int i = 1; i<nums.size();i++)
+          
+        {
+            int temp = currMax; // since currMax will change value in this iteration. Imp to store the previous value
+          
+            if(nums[i] == 0) currMax = currMin = 1;
+          
+            currMax = max(max(currMax*nums[i],currMin*nums[i]),nums[i]); //curr Max caclculates the maximum product achived right now
+          
+            currMin = min(min(currMin*nums[i],temp*nums[i]),nums[i]);// curr Min is also imp because a negative number multiplied by another negative may result in a large positve number. Therefore IMP to check max between currMax and currMin.
             
-            currMax = max(max(currMax * nums[i], currMin * nums[i]), nums[i]);
-            currMin = min(min(currMin * nums[i], temp * nums[i]), nums[i]);
-            
-            result = max(result, currMax);
+            ans = max(currMax,ans); //global result or maxUntilNow
         }
-        
-        return result;
+  return ans;
     }
+
+  
 };
