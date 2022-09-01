4f27f3e5957d7bd1220f1f44681b6994d0c0ba03
Khan-17101136 <fardinshuvro96@gmail.com>
2022-09-02 04:33:59 +0600

    Merger-Two-Sorted-Lists-Cleaner-Recursive

--- java/21-Merge-Two-Sorted-Lists.java
+++ java/21-Merge-Two-Sorted-Lists.java
@@ -44,20 +44,15 @@ class Solution {
 class Solution {
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode head = new ListNode(0);
-
-        if (list1 == null && list2 == null) return null;
-        if (list1 == null) return list2;
-        if (list2 == null) return list1;
-
-        if (list1.val > list2.val) {
-            head = list2;
-            list2 = list2.next;
-        } else {
-            head = list1;
-            list1 = list1.next;
+        if(list1 == null)return list2;
+        if(list2 == null)return list1;
+        
+        if(list1.val < list2.val){
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        }else{
+            list2.next = mergeTwoLists(list2.next, list1);
+            return list2;
         }
-        head.next = mergeTwoLists(list1, list2);
-        return head;
     }
 }
