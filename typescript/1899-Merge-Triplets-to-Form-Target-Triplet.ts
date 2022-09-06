f9e95be90a3a58d2b40f15214ed268ade6e327e9
Yaseen Khan <78000116+Ykhan799@users.noreply.github.com>
2022-09-06 16:46:06 -0700

    Create: 1899-Merge-Triplets-to-Form-Target-Triplet.ts

---
+++ typescript/1899-Merge-Triplets-to-Form-Target-Triplet.ts
@@ -0,0 +1,17 @@
+function mergeTriplets(triplets: number[][], target: number[]): boolean {
+    let good = new Set();
+    
+    for (let t in triplets) {
+        let triplet = triplets[t];
+        if (triplet[0] > target[0] || triplet[1] > target[1] || triplet[2] > target[2]) {
+            continue;
+        }
+        
+        for (let i = 0; i < triplet.length; i++) {
+            if (triplet[i] === target[i]) {
+                good.add(i);
+            }
+        }
+    }
+    return good.size === 3;
+};
