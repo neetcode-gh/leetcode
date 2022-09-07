07753d08bf79d34c84e339bb4e44859d93d89eb8
Yaseen Khan <78000116+Ykhan799@users.noreply.github.com>
2022-09-07 14:19:38 -0700

    Create: 45-Jump-Game-II.ts

---
+++ typescript/45-Jump-Game-II.ts
@@ -0,0 +1,16 @@
+function jump(nums: number[]): number {
+    let left = 0;
+    let right = 0;
+    let res = 0;
+    
+    while (right < nums.length - 1) {
+        let maxJump = 0;
+        for (let i = left; i <= right; i++) {
+            maxJump = Math.max(maxJump, i + nums[i]);
+        }
+        left = right + 1;
+        right = maxJump;
+        res += 1;
+    }
+    return res;
+};
