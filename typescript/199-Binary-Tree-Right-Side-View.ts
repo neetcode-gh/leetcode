ac3cef1f39c333b2a17af4bae371b7db9c507406
Yaseen Khan <78000116+Ykhan799@users.noreply.github.com>
2022-09-07 14:27:30 -0700

    Create: 199-Binary-Tree-Right-Side-View.ts

---
+++ typescript/199-Binary-Tree-Right-Side-View.ts
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function rightSideView(root: TreeNode | null): number[] {
+    let result = [];
+    let queue = [];
+    
+    if (root === null) {
+        return [];
+    }
+    
+    queue.push(root);
+    
+    while (queue.length > 0) {
+        let length = queue.length;
+        for (let i = 0; i < length; i++) {
+            let node = queue.shift();
+            
+            if (i === length - 1) {
+                result.push(node.val);
+            }
+            if (node.left !== null) {
+                queue.push(node.left);
+            }
+            if (node.right !== null) {
+                queue.push(node.right);
+            }
+        }
+    }
+    return result;
+};
