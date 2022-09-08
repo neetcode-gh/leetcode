e6a0eb764e847ba11c0fbca9405ddfd094eeb9e1
Yaseen Khan <78000116+Ykhan799@users.noreply.github.com>
2022-09-08 14:03:23 -0700

    Create: 678-Valid-Parenthesis-String.ts

---
+++ typescript/678-Valid-Parenthesis-String.ts
@@ -0,0 +1,29 @@
+function checkValidString(s: string): boolean {
+    let leftMin = 0;
+    let leftMax = 0;
+    
+    for (let c of s) {
+        if (c === '(') {
+            leftMin++;
+            leftMax++;
+        }
+        else if (c === ')') {
+            leftMin--;
+            leftMax--;
+        }
+        else {
+            leftMin--;
+            leftMax++;
+        }
+        
+        if (leftMax < 0) {
+            return false;
+        }
+        
+        if (leftMin < 0) {
+            leftMin = 0;
+        }
+    }
+    
+    return leftMin === 0;
+};
