ef9719987ee630a6b8f582292984b3756e1ce3c6
aa0 <71089234+Ahmad-A0@users.noreply.github.com>
2022-09-11 15:21:59 +0100

    Update 269-Alien-Dictionary.py

--- python/269-Alien-Dictionary.py
+++ python/269-Alien-Dictionary.py
@@ -1,37 +1,39 @@
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
-        adj = {char: set() for word in words for char in word}
+        adj = { c:set() for w in words for c in w }
 
         for i in range(len(words) - 1):
             w1, w2 = words[i], words[i + 1]
             minLen = min(len(w1), len(w2))
+
             if len(w1) > len(w2) and w1[:minLen] == w2[:minLen]:
                 return ""
+            
             for j in range(minLen):
-                if w1[j] != w2[j]:
-                    print(w1[j], w2[j])
-                    adj[w1[j]].add(w2[j])
+                w1[j] != w2[j]:
+                    adj[w1[j]].add(w2[j]]
                     break
 
-        visited = {}  # {char: bool} False visited, True current path
-        res = []
+            visit = {} # False=visited, True=current path
+            res = []
 
-        def dfs(char):
-            if char in visited:
-                return visited[char]
+            def dfs(c):
+                if c in visit:
+                    return visit[c]
+                
+                visit[c] = True
 
-            visited[char] = True
+                for nei in adj[c]:
+                    if dfs(nei):
+                        return True
 
-            for neighChar in adj[char]:
-                if dfs(neighChar):
-                    return True
+                visit[c] = False
+                res.append(c)
 
-            visited[char] = False
-            res.append(char)
+            for c in adj:
+                if dfs(c):
+                    return ""
 
-        for char in adj:
-            if dfs(char):
-                return ""
+            return "".join(res)
 
-        res.reverse()
-        return "".join(res)
+                
