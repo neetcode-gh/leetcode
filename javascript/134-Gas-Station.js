5ff1156596afc96b36169290f694d4afc72e7793
aakhtar3 <aakhtar3@users.noreply.github.com>
2022-09-12 22:42:02 -0400

    refactor: js - greedy

--- javascript/134-Gas-Station.js
+++ javascript/134-Gas-Station.js
@@ -10,9 +10,7 @@ var canCompleteCircuit = function (gas, cost) {
     let res = 0;
 
     //Checks if theres enough gas to complete a cycle
-    if (gas.reduce((a, b) => a + b) - cost.reduce((a, b) => a + b) < 0) {
-        return -1;
-    }
+    if (gas.reduce((a, b) => a + b) - cost.reduce((a, b) => a + b) < 0) return -1;
 
     // Finds the first appearence of a positive netDistance, if the cycle can't
     // be completed (netDistance < 0), starts cycle again @ the next positive netDistance value.
@@ -24,5 +22,27 @@ var canCompleteCircuit = function (gas, cost) {
             res = i + 1;
         }
     }
+    
     return res;
 };
+
+/**
+ * @param {number[]} gas
+ * @param {number[]} cost
+ * @return {number}
+ */
+ var canCompleteCircuit = function(gas, cost) {
+    let [ totalTank, currTank, startingStation ] = [ 0, 0, 0 ]
+
+    for (let i = 0; i < gas.length; i++) {
+        totalTank += gas[i] - cost[i];
+        currTank += gas[i] - cost[i];
+        
+        const isEmpty = currTank < 0;
+        if (isEmpty) { startingStation = (i + 1); currTank = 0; }
+    }
+    
+    return 0 <= totalTank
+        ? startingStation 
+        : -1;
+}
--- javascript/1899-Merge-Triplets-to-Form-Target-Triplet.js
+++ javascript/1899-Merge-Triplets-to-Form-Target-Triplet.js
@@ -25,3 +25,46 @@ var mergeTriplets = function (triplets, target) {
 
     return good.size === 3;
 };
+
+/**
+ * https://leetcode.com/problems/merge-triplets-to-form-target-triplet/
+ * Time O(N) | Space O(1)
+ * @param {number[][]} triplets
+ * @param {number[]} target
+ * @return {boolean}
+ */
+ var mergeTriplets = function(triplets, target, res = new Array(3).fill(0)) { 
+    for (const [ a, b, c ] of triplets) {          /* Time O(N) */
+        const [ _a, _b, _c ] = target;
+
+        const isTargetGreater = (a <= _a) && (b <= _b) && (c <= _c);
+        if (!isTargetGreater) continue;
+
+        const [ __a, __b, __c ] = res;
+        res = [ Math.max(__a, a), Math.max(__b, b), Math.max(__c, c) ];
+    }
+        
+    return res.every((val, i) => val === target[i])/* Time O(N) */
+};
+
+/**
+ * https://leetcode.com/problems/merge-triplets-to-form-target-triplet/
+ * Time O(N) | Space O(1)
+ * @param {number[][]} triplets
+ * @param {number[]} target
+ * @return {boolean}
+ */
+var mergeTriplets = function(triplets, target, res = new Array(3).fill(false)) { 
+    for (const [ a, b, c ] of triplets) {/* Time O(N) */
+        const [ _a, _b, _c ] = target;
+        
+        const isTargetGreater = (a <= _a) && (b <= _b) && (c <= _c);
+        if (!isTargetGreater) continue;
+        
+        res[0] |= (a === _a);
+        res[1] |= (b === _b);
+        res[2] |= (c === _c);
+    }
+    
+    return res[0] && res[1] && res[2];
+}
