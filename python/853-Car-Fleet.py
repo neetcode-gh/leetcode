0d8fcacc6d3379897c1626f4f2e1f01dfaec06c8
Busi Reddy <busisatya@gmail.com>
2022-09-15 19:35:26 -0400

    Updated 853.CarFleet.py
    Better space complexity than the one posted.

--- python/853-Car-Fleet.py
+++ python/853-Car-Fleet.py
@@ -1,10 +1,13 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        pair = [(p, s) for p, s in zip(position, speed)]
-        pair.sort(reverse=True)
-        stack = []
-        for p, s in pair:  # Reverse Sorted Order
-            stack.append((target - p) / s)
-            if len(stack) >= 2 and stack[-1] <= stack[-2]:
-                stack.pop()
-        return len(stack)
+        pos_vel = sorted(zip(position,speed), reverse=True)
+        max_seen = float("-inf")
+        ans = 0
+        for elem in pos_vel:
+            vel = elem[1]
+            pos = elem[0]
+            time = (target-pos)/vel
+            if max_seen < time:#I reach after everyone on my right, so i make one fleet
+                ans += 1
+                max_seen = time
+        return ans
