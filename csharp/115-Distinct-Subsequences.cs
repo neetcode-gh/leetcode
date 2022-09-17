99c26a204c7eb57df68aa360fef1457c850a554d
aa0 <71089234+Ahmad-A0@users.noreply.github.com>
2022-09-17 21:38:31 +0100

    Update 115-Distinct-Subsequences.cs

--- csharp/115-Distinct-Subsequences.cs
+++ csharp/115-Distinct-Subsequences.cs
@@ -16,21 +16,8 @@ public class Solution {
             for (int j = 1; j <= nT; j++) {
                 var tIndex = j - 1;
                 if (s[sIndex] == t[tIndex]) {
-                    /*
-                        rabb
-                         rab
-                         
-                        1. use the last b: dp[i-1,j-1]
-                        2. do not use the last b: dp[i-1,j];
-                    */
                     dp[i, j] = dp[i - 1, j - 1] + dp[i - 1, j];
                 } else {
-                    /*
-                        ra
-                         r
-                        
-                        cannot use the last char because they are not the same.
-                    */
                     dp[i, j] = dp[i - 1, j];
                 }
             }
