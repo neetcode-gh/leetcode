23b6d27a6f75b7031a80263546d8d2f42a150b68
aakhtar3 <aakhtar3@users.noreply.github.com>
2022-09-18 20:10:26 -0400

    refactor: js - heap (#686)
    Co-authored-by: Mitchell Irvin <16233245+mitchellirvin@users.noreply.github.com>

--- javascript/621-Task-Scheduler.js
+++ javascript/621-Task-Scheduler.js
@@ -1,30 +1,107 @@
 /**
+ * https://leetcode.com/problems/task-scheduler/
+ * Time O(N * log(N)) | Space O(N)
  * @param {character[]} tasks
  * @param {number} n
  * @return {number}
  */
-var leastInterval = function (tasks, n) {
-    const charMap = new Map();
-    let maxCharCount = 0;
-    let maxChar = tasks[0];
+var leastInterval = function(tasks, n) {
+  const frequencyMap = getFrequencyMap(tasks)
+  const maxHeap = getMaxHeap(frequencyMap)
 
-    for (let char of tasks) {
-        charMap.set(char, (charMap.get(char) || 0) + 1);
-        if (charMap.get(char) > maxCharCount) {
-            maxCharCount = charMap.get(char);
-            maxChar = char;
-        }
+  return getMinimumCpuIntervals(maxHeap, n)
+}
+
+var getFrequencyMap = (tasks, frequencyMap = new Array(26).fill(0)) => {
+    for (const task of tasks) {
+        const index = task.charCodeAt(0) - 'A'.charCodeAt(0);
+
+        frequencyMap[index]++;
     }
 
-    let idleCount = (maxCharCount - 1) * n;
+    return frequencyMap;
+}
 
-    charMap.forEach((count, char) => {
-        // 'return' inside forEach() serve as 'continue'
-        if (char === maxChar) return;
-        if (count === maxCharCount) idleCount -= count - 1;
-        else idleCount -= count;
-    });
+const getMaxHeap = (frequencyMap, maxHeap = new MaxPriorityQueue()) => {
+    for (const frequency of frequencyMap) {
+        const hasFrequency = 0 < frequency;
+        if (hasFrequency) maxHeap.enqueue(frequency)
+    }
 
-    if (idleCount <= 0) return tasks.length;
-    return tasks.length + idleCount;
-};
+    return maxHeap
+}
+
+const getMinimumCpuIntervals = (maxHeap, n, cpuIntervals = [ 0 ]) => {
+    while (!maxHeap.isEmpty()) {
+        const { iterations, coolingPeriodQueue } = execute(n, maxHeap, cpuIntervals)
+
+        reQueueCoolingPeriod(coolingPeriodQueue, maxHeap)
+
+        if (!maxHeap.isEmpty()) cpuIntervals[0] += iterations
+    }
+
+    return cpuIntervals[0]
+}
+
+const execute = (n, maxHeap, cpuIntervals, iterations = (n + 1), coolingPeriodQueue = new Queue()) => {
+    while ((0 < iterations) && !maxHeap.isEmpty()) {
+        const frequency = maxHeap.dequeue().element;
+
+        const hasFrequency = 0 < (frequency - 1);
+        if (hasFrequency) coolingPeriodQueue.enqueue(frequency - 1);
+
+        cpuIntervals[0]++;
+        iterations--;
+    }
+
+    return { iterations, coolingPeriodQueue };
+}
+
+const reQueueCoolingPeriod = (coolingPeriodQueue, maxHeap) => {
+    while (!coolingPeriodQueue.isEmpty()) {
+        maxHeap.enqueue(coolingPeriodQueue.dequeue())
+    }
+}
+
+/**
+ * https://leetcode.com/problems/task-scheduler/
+ * Time O(N) | Space O(1)
+ * @param {character[]} tasks
+ * @param {number} n
+ * @return {number}
+ */
+ var leastInterval = function(tasks, n) {
+    const frequencyMap = getFrequencyMap(tasks);
+    const maxFrequency = getMaxFrequency(frequencyMap);
+    const mostFrequentTask = getMostFrequentTask(frequencyMap, maxFrequency);
+    const interval = ((maxFrequency - 1) * (n + 1)) + mostFrequentTask;
+
+    return Math.max(tasks.length, interval);
+}
+
+var getFrequencyMap = (tasks, frequencyMap = new Array(26).fill(0)) => {
+    for (const task of tasks) {
+        const index = task.charCodeAt(0) - 'A'.charCodeAt(0);
+
+        frequencyMap[index]++;
+    }
+
+    return frequencyMap;
+}
+
+const getMaxFrequency = (frequencyMap, maxFrequency = 0) => {
+    for (const frequency of frequencyMap) {
+        maxFrequency = Math.max(maxFrequency, frequency);
+    }
+
+    return maxFrequency;
+}
+
+const getMostFrequentTask = (frequencyMap, maxFrequency, mostFrequentTask = 0) => {
+    for (const frequency of frequencyMap) {
+        const isSame = frequency === maxFrequency;
+        if (isSame) mostFrequentTask++;
+    }
+
+    return mostFrequentTask;
+}
