8377ec32d293e7a4f960e791c628d654584f1156
aakhtar3 <aakhtar3@users.noreply.github.com>
2022-09-20 09:09:53 -0400

    refactor: js - 1dp

--- javascript/152-Maximum-Product-Subarray.js
+++ javascript/152-Maximum-Product-Subarray.js
@@ -1,19 +1,59 @@
 /**
+ * Brute Force - Linear Search
+ * Time O(N^2) | Space O(1)
+ * https://leetcode.com/problems/maximum-product-subarray/
  * @param {number[]} nums
  * @return {number}
  */
-var maxProduct = function (nums) {
-    let result = nums[0];
-    let prevMax = nums[0];
-    let prevMin = nums[0];
-    for (let i = 1; i < nums.length; i++) {
-        currMax = Math.max(nums[i], prevMax * nums[i], prevMin * nums[i]);
-        currMin = Math.min(nums[i], prevMax * nums[i], prevMin * nums[i]);
+ var maxProduct = (nums) => {
+    const isEmpty = nums.length === 0;
+    if (isEmpty) return 0;
 
-        prevMax = currMax;
-        prevMin = currMin;
+    return linearSearch(nums);/* Time O(N * N) */
+}
 
-        result = Math.max(currMax, result);
+const linearSearch = (nums, max = nums[0]) => {
+    for (let index = 0; index < nums.length; index++) {/* Time O(N) */
+        max = getMax(nums, index, max);                    /* Time O(N) */
     }
-    return result;
+
+    return max;
+}
+
+const getMax = (nums, index, max, product = 1) => {
+    for (let num = index; num < nums.length; num++) {/* Time O(N) */
+        product *= nums[num];
+        max = Math.max(max, product);
+    }
+
+    return max;
+}
+
+/**
+ * Greedy - product
+ * Time O(N) | Space O(1)
+ * https://leetcode.com/problems/maximum-product-subarray/
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = (nums) => {
+    const isEmpty = nums.length === 0;
+    if (isEmpty) return 0;
+
+    return greedySearch(nums);/* Time O(N) */
 };
+
+const greedySearch = (nums) => {
+    let min = max = product = nums[0];
+
+    for (let num = 1; num < nums.length; num++) {/* Time O(N) */
+        const [ minProduct, maxProduct ] = [ (min * nums[num]), (max * nums[num]) ];
+
+        min = Math.min(maxProduct, minProduct, nums[num]);
+        max = Math.max(maxProduct, minProduct, nums[num]);
+
+        product = Math.max(product, max);
+    }
+
+    return product;
+}
--- javascript/91-Decode-Ways.js
+++ javascript/91-Decode-Ways.js
@@ -1,20 +1,122 @@
-function numDecodings(s) {
-    let dp = {
-        [s.length]: 1,
-    };
+/**
+ * DP - Top Down
+ * Hash Map - Memoization
+ * Time O(N) | Space O(N)
+ * https://leetcode.com/problems/decode-ways/
+ * @param {string} s
+ * @return {number}
+ */
+var numDecodings = (str, index = 0, memo = new Map()) => {
+    const isBaseCase1 = !str.length || (str[index] === '0');
+    if (isBaseCase1) return 0;
 
-    for (let i = s.length - 1; i > -1; i--) {
-        if (s[i] == '0') {
-            dp[i] = 0;
-        } else {
-            dp[i] = dp[i + 1];
-        }
-        if (
-            i + 1 < s.length &&
-            (s[i] == '1' || (s[i] == '2' && '0123456'.includes(s[i + 1])))
-        ) {
-            dp[i] += dp[i + 2];
-        }
+    const isisBaseCase2 = index === str.length;
+    if (isisBaseCase2) return 1;
+
+    if (memo.has(index)) return memo.get(index);
+
+    return dfs(str, index, memo);
+};
+
+const dfs = (str, index, memo) => {
+    let count = numDecodings(str, (index + 1), memo);
+
+    if (isTwoDigit(str, index)) {
+        count += numDecodings(str, (index + 2), memo);
     }
-    return dp[0];
+
+    memo.set(index, count);
+
+    return count;
+}
+
+var isTwoDigit = (str, index) => {
+    const twoDigit = Number(str.slice(index, (index + 2)));
+
+    return (10 <= twoDigit) && (twoDigit <= 26);
+}
+
+/**
+ * DP - Bottom Up
+ * Array - Tabulation
+ * Time O(N) | Space O(N)
+ * https://leetcode.com/problems/decode-ways/
+ * @param {string} s
+ * @return {number}
+ */
+var numDecodings = (s) => {
+    const isBaseCase = !s.length || s[0] === '0'
+    if (isBaseCase) return 0;
+
+    const tabu = getTabu(s);
+
+    decode(s, tabu);
+
+    return tabu[s.length];
+}
+
+const getTabu = (s) => {
+    const tabu = new Array(s.length + 1).fill(0);
+
+    tabu[0] = 1;
+    tabu[1] = (s[1] === '0')
+        ? 0
+        : 1;
+
+    return tabu;
+}
+
+var decode = (s, tabu) => {
+    for (let curr = 2; curr < tabu.length; curr++) {
+        const [ prev, prevPrev ] = [ (curr - 1), (curr - 2) ];
+        const isEqual = s[prev] === '0';
+        if (!isEqual) tabu[curr] += tabu[prev];
+
+        if (isTwoDigit(s, curr)) tabu[curr] += tabu[prevPrev];
+    }
+}
+
+var isTwoDigit = (s, index) => {
+    const twoDigit = Number(s.slice((index - 2), index));
+
+    return 10 <= twoDigit && twoDigit <= 26;
+}
+
+/**
+ * 2 Pointer - previous + previousPrevious
+ * Time O(N) | Space O(1)
+ * https://leetcode.com/problems/decode-ways/
+ * @param {string} s
+ * @return {number}
+ */
+var numDecodings = (s) => {
+    const isBaseCase = !s.length || s[0] === '0';
+    if (isBaseCase) return 0;
+
+    return decode(s);
+}
+
+var decode = (s) => {
+    let [ prev, prevPrev ] = [ 1, 1 ];
+
+    for (let curr = 1; curr < s.length; curr++) {
+        const temp = prev;
+
+        const isEqual = s[curr] === '0';
+        if (isEqual) prev = 0;
+
+        if (isTwoDigit(s, curr)) prev += prevPrev;
+
+        prevPrev = temp;
+    }
+
+    return prev;
+}
+
+var isTwoDigit = (s, i) => {
+    const [ prevChar, curChar ] = [ (s[i - 1]), s[i] ];
+    const is10 = prevChar === '1';
+    const is20 = (prevChar === '2' && curChar <= '6');
+
+    return is10 || is20;
 }
