de530f6c8c1fb4b2cbee06febf9e8d3e9fa74320
Sakshum Gadyal <100090997+sxsmg@users.noreply.github.com>
2022-09-24 00:40:40 +0530

    Update 215-Kth-Largest-Element-in-an-Array.py
    Heap Solution for reference

--- python/215-Kth-Largest-Element-in-an-Array.py
+++ python/215-Kth-Largest-Element-in-an-Array.py
@@ -44,3 +44,15 @@ class Solution2:
                 break
 
         return nums[k]
+ 
+#Heap Solution (For Reference)
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        nums = [-n for n in nums]
+        heapq.heapify(nums)
+        
+        while(k):
+            ans = heapq.heappop(nums)
+            k -= 1
+        return -1 * ans
