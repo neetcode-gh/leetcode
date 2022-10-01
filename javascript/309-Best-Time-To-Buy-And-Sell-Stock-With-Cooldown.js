303c12f08f2931208645c30ca9bdc30d96f27f50
aakhtar3 <aakhtar3@users.noreply.github.com>
2022-10-01 12:50:10 -0400

    refactor: js | 2dp

--- javascript/309-Best-Time-To-Buy-And-Sell-Stock-With-Cooldown.js
+++ javascript/309-Best-Time-To-Buy-And-Sell-Stock-With-Cooldown.js
@@ -1,24 +1,63 @@
 /**
+ * Greedy - State Machine
+ * Time O(N) | Space O(1)
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function (prices) {
-    let dp = {}; // key=[i, buying] val=max_profit
+ var maxProfit = (prices) => {
+    let [ sold, held, reset ] = [ (-Infinity), (-Infinity), 0 ];
 
-    const dfs = (i, buying) => {
-        if (i >= prices.length) return 0;
-        if (dp[[i, buying]]) return dp[[i, buying]];
+    [ sold, reset ] = search(prices, sold, held, reset);/* Time O(N) */
 
-        const cooldown = dfs(i + 1, buying);
-        if (buying) {
-            const buy = dfs(i + 1, !buying) - prices[i];
-            dp[[i, buying]] = Math.max(buy, cooldown);
-        } else {
-            sell = dfs(i + 2, !buying) + prices[i];
-            dp[[i, buying]] = Math.max(sell, cooldown);
-        }
-        return dp[[i, buying]];
-    };
+    return Math.max(sold, reset);
+}
 
-    return dfs(0, true);
-};
+var search = (prices, sold, held, reset) => {
+    for (const price of prices) {/* Time O(N) */
+        const preSold = sold;
+
+        sold = (held + price);
+        held = Math.max(held, (reset - price));
+        reset = Math.max(reset, preSold);
+    }
+
+    return [ sold, reset ];
+}
+
+/**
+ * DP - Bottom Up
+ * Array - Tabulation
+ * Time O(N^2) | Space O(N)
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = (prices) => {
+    const tabu = initTabu(prices);/* Space O(N) */
+
+    search(prices, tabu);/* Time O(N * N) */
+
+    return tabu[0];
+}
+
+var initTabu = (prices) => new Array(prices.length + 2).fill(0);
+
+var search = (prices, tabu) => {
+    for (let i = (prices.length - 1); (0 <= i); i--) {/* Time O(N) */
+        const prev = buyAndSell(prices, i, tabu);         /* Time O(N) */
+        const next = tabu[i + 1];
+
+        tabu[i] = Math.max(prev, next);                       /* Space O(N) */
+    }
+}
+
+const buyAndSell = (prices, i, tabu, max = 0) => {
+    for (let sell = (i + 1); (sell < prices.length); sell++) {/* Time O(N) */
+        const profit = ((prices[sell] - prices[i]) + tabu[(sell + 2)]);
+
+        max = Math.max(max, profit);
+    }
+
+    return max;
+}
--- javascript/329-Longest-Increasing-Path-in-a-Matrix.js
+++ javascript/329-Longest-Increasing-Path-in-a-Matrix.js
@@ -1,41 +1,176 @@
 /**
+ * Brute Force - DFS
+ * Time O(2^(N + M)) | Space O(N * M)
+ * https://leetcode.com/problems/longest-increasing-path-in-a-matrix/
  * @param {number[][]} matrix
  * @return {number}
  */
-var longestIncreasingPath = function (matrix) {
-    const ROWS = matrix.length;
-    const COLS = matrix[0].length;
-    const dp = {}; // r|c -> LIP
+ var longestIncreasingPath = (matrix, maxPath = 0) => {
+    const [ rows, cols ] = [ matrix.length, matrix[0].length ];
 
-    function dfs(r, c, prevVal) {
-        if (
-            r < 0 ||
-            r === ROWS ||
-            c < 0 ||
-            c === COLS ||
-            matrix[r][c] <= prevVal
-        ) {
-            return 0;
-        }
+    for (let row = 0; (row < rows); row++) {/* Time O(N) */
+        for (let col = 0; (col < cols); col++) {/* Time O(M) */
+            const path = dfs(matrix, row, rows, col, cols);/* Time O(2^(N + M)) | Space O(HEIGHT) */
 
-        if (dp.hasOwnProperty(`${r}|${c}`)) {
-            return dp[`${r}|${c}`];
-        }
-
-        let res = 1;
-        res = Math.max(res, 1 + dfs(r + 1, c, matrix[r][c]));
-        res = Math.max(res, 1 + dfs(r - 1, c, matrix[r][c]));
-        res = Math.max(res, 1 + dfs(r, c + 1, matrix[r][c]));
-        res = Math.max(res, 1 + dfs(r, c - 1, matrix[r][c]));
-        dp[`${r}|${c}`] = res;
-        return res;
-    }
-
-    for (var r = 0; r < ROWS; r++) {
-        for (var c = 0; c < COLS; c++) {
-            dfs(r, c, -1);
+            maxPath = Math.max(maxPath, path);
         }
     }
 
-    return Math.max(...Object.values(dp));
+    return maxPath;
+}
+
+var dfs = (matrix, row, rows, col, cols, ans = 0) => {
+    for (const [ _row, _col ] of getNeighbors(row, rows, col, cols)) {/* Time O(4) */
+        const path = dfs(matrix, _row, rows, _col, cols);             /* Time O(2^(N + M)) | Space O(HEIGHT) */
+
+        ans = Math.max(ans, path);
+    }
+
+    ans += 1;
+    return ans;
+}
+
+var getNeighbors = (row, rows, col, cols) => [ [ 0, 1 ], [ 0, -1 ], [ 1, 0 ], [ -1, 0 ] ]
+    .map(([ _row, _col ]) => [ (row + _row), (col + _col) ])
+    .filter(([ _row, _col ]) => (0 <= _row) && (_row < rows) && (0 <= _col) && (_col < cols));
+
+/**
+ * DP - Top Down
+ * Matrix - Memoization
+ * Time O(N * M) | Space O(N * M)
+ * https://leetcode.com/problems/longest-increasing-path-in-a-matrix/
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+ var longestIncreasingPath = (matrix, maxPath = 0, memo = initMemo(matrix)) => {
+    const [ rows, cols ] = [ matrix.length, matrix[0].length ];
+
+    for (let row = 0; row < rows; row++) {/* Time O(N) */
+        for (let col = 0; col < cols; col++) {/* Time O(M) */
+            const path =                  /* Time O(N * M) | Space O((N * M) + HEIGHT) */
+                search(matrix, row, rows, col, cols, memo);
+
+            maxPath = Math.max(maxPath, path);
+        }
+    }
+
+    return maxPath;
 };
+
+var initMemo = (matrix) => new Array(matrix.length).fill()/* Time O(N) | Space O(N)*/
+    .map(() => new Array(matrix[0].length).fill(0));          /* Time O(M) | Space O(M)*/
+
+const search = (matrix, row, rows, col, cols, memo) => {
+    const hasSeen = (memo[row][col] !== 0)
+    if (hasSeen) return memo[row][col];
+
+    return dfs(matrix, row, rows, col, cols, memo);/* Time O(N * M) | Space O((N * M) + HEIGHT) */
+}
+
+var dfs = (matrix, row, rows, col, cols, memo) => {
+    for (const [ _row, _col ] of getNeighbors(row, rows, col, cols)) {/* Time O(4) */
+        const [ parent, node ] = [ matrix[row][col], matrix[_row][_col] ];
+
+        const isLess = (node <= parent);
+        if (isLess) continue;
+
+        const path = search(matrix, _row, rows, _col, cols, memo);    /* Time O(N * M) | Space O(HEIGHT) */
+
+        memo[row][col] = Math.max(memo[row][col], path);
+    }
+
+    memo[row][col] += 1;                                              /*               | Space O(N * M) */
+    return memo[row][col];
+}
+
+var getNeighbors = (row, rows, col, cols) => [ [ 0, 1 ], [ 0, -1 ], [ 1, 0 ], [ -1, 0 ] ]
+    .map(([ _row, _col ]) => [ (row + _row), (col + _col) ])
+    .filter(([ _row, _col ]) => (0 <= _row) && (_row < rows) && (0 <= _col) && (_col < cols));
+
+/**
+ * Topological Sort
+ * Matrix - Graph
+ * Matrix - In-Degree
+ * Queue - BFS
+ * Time O(N * M) | Space O(N * M)
+ * https://leetcode.com/problems/longest-increasing-path-in-a-matrix/
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+var longestIncreasingPath = (matrix) => {
+    const { graph, indegree, sources } =  /* Time O(N * M) | Space O(N * M) */
+        buildGraph(matrix);
+
+    findSources(graph, indegree, sources);/* Time O(N * M) | Space O(N * M) */
+
+    return bfs(graph, indegree, sources); /* Time O((N * M) + WIDTH) | Space O((N * M) + WIDTH) */
+}
+
+const initGraph = (rows, cols) => ({
+    graph: new Array((rows + 2)).fill()       /* Time O(N) | Space O(N) */
+        .map(() => new Array((cols + 2)).fill(0)),/* Time O(M) | Space O(M) */
+    indegree: new Array((rows + 2)).fill()    /* Time O(N) | Space O(N) */
+        .map(() => new Array((cols + 2)).fill(0)),/* Time O(M) | Space O(M) */
+    sources: new Queue()
+})
+
+var buildGraph = (matrix) => {
+    const [ rows, cols ] = [ matrix.length, matrix[0].length ];
+    const { graph, indegree, sources } =          /* Time O(N * M) | Space O(N * M) */
+        initGraph(rows, cols);
+
+    for (let row = 1; (row < (rows + 1)); row++) {/* Time O(N) */
+        graph[row] = [ 0, ...matrix[row - 1], 0 ];    /*           | Space O(N * M) */
+    }
+
+    for (let row = 1; (row <= rows); row++) {     /* Time O(N) */
+        for (let col = 1; (col <= cols); col++) {     /* Time O(M) */
+            for (const [ _row, _col ] of getNeighbors(row, col)) {/* Time O(4) */
+                const isSink = (graph[row][col] < graph[_row][_col]);
+                if (isSink) indegree[row][col] += 1;      /*       | Space O(N * M) */
+            }
+        }
+    }
+
+    return { graph, indegree, sources };
+}
+
+var getNeighbors = (row, col) => [ [ 0, 1 ], [ 0, -1 ], [ 1, 0 ], [ -1, 0 ] ]
+    .map(([ _row, _col ]) => [ (row + _row), (col + _col) ]);
+
+var findSources = (graph, indegree, sources) => {
+    const [ rows, cols ] = [ graph.length, graph[0].length ];
+
+    for (let row = 1; (row < (rows - 1)); ++row) {/* Time O(N) */
+        for (let col = 1; (col < (cols - 1)); ++col) {/* Time O(M) */
+            const isSource = (indegree[row][col] === 0);
+            if (isSource) sources.enqueue([ row, col ]);  /* Space O(N * M) */
+        }
+    }
+}
+
+const bfs = (graph, indegree, sources, path = 0) => {
+    while (!sources.isEmpty()) {                                     /* Time(N * M) */
+        for (let level = (sources.size() - 1); 0 <= level; level--) {/* Time(WIDTH) */
+            checkNeighbors(graph, indegree, sources)                     /* Space((N * M) + WIDTH) */
+        }
+
+        path += 1;
+    }
+
+    return path;
+}
+
+const checkNeighbors = (graph, indegree, sources) => {
+    const [ row, col ] = sources.dequeue();
+
+    for (const [ _row, _col ] of getNeighbors(row, col)) {
+        const canDisconnect = (graph[_row][_col] < graph[row][col]);
+        if (!canDisconnect) continue;
+
+        indegree[_row][_col] -= 1;                    /* Space O(N * M) */
+
+        const isSource = (indegree[_row][_col] === 0);
+        if (isSource) sources.enqueue([ _row, _col ]);/* Space O(WIDTH) */
+    }
+}
