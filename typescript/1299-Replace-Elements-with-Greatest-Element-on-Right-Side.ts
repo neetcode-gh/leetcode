ce60e80591992d90fa3802acb38ff6a57670e328
x33r0 <mahmoud_magdy_m@outlook.com>
2022-10-02 00:03:22 +0200

    [ADD] Add ts solution for 1299-Replace-Elements-with-Greatest-Element-on-Right-Side

---
+++ typescript/1299-Replace-Elements-with-Greatest-Element-on-Right-Side.ts
@@ -0,0 +1,13 @@
+// ======================= [Iter solu] =======================
+function replaceElements(arr: number[]): number[] {
+    let currMax = -1;
+    
+    for(let i = arr.length -1 ; i >= 0; i--) {
+        let newMax = Math.max(currMax, arr[i]);         
+        arr[i] = currMax;
+        
+        currMax = newMax;
+    }
+    
+    return arr;
+};
