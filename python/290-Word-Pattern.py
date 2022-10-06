8948d679740b91374aac93bed055cb18079f29cd
Uday garg <65303126+UdayGarg@users.noreply.github.com>
2022-10-06 18:18:25 +0530

    Create: 290-Word-Pattern.py

---
+++ python/290-Word-Pattern.py
@@ -0,0 +1,16 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split(" ")
+        if len(pattern) != len(words):
+            return False
+        charToWord = {}
+        wordToChar = {}
+        
+        for c, w in zip(pattern, words):
+            if c in charToWord and charToWord[c] != w:
+                return False
+            if w in wordToChar and wordToChar[w] != c:
+                return False
+            charToWord[c] = w
+            wordToChar[w] = c
+        return True
