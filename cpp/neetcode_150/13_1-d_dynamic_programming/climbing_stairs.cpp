6243d9c582722fe758298594248c8a7425bf827b
Aniruddha Krishna Jha <56834666+aniruddhakj@users.noreply.github.com>
2022-10-07 11:41:27 +0530

    Updating 70.climbing_stairs.cpp
    Updating the solution with a more easily understandable code according to the video solution

--- cpp/neetcode_150/13_1-d_dynamic_programming/climbing_stairs.cpp
+++ cpp/neetcode_150/13_1-d_dynamic_programming/climbing_stairs.cpp
@@ -2,7 +2,7 @@
     Climbing stairs, either 1 or 2 steps, distinct ways to reach top
     Ex. n = 2 -> 2 (1 + 1, 2), n = 3 -> 3 (1 + 1 + 1, 1 + 2, 2 + 1)
 
-    Recursion w/ memoization -> DP, why DP? Optimal substructure
+    Bottom-up DP
     Recurrence relation: dp[i] = dp[i - 1] + dp[i - 2]
     Reach ith step in 2 ways: 1) 1 step from i-1, 2) 2 steps from i-2
 
@@ -12,25 +12,15 @@
 
 class Solution {
 public:
+    //Fibonacci series : 'one' stores ways from [n-2]
+    //'two' stores ways from [n-1] 
     int climbStairs(int n) {
-        if (n == 1) {
-            return 1;
+        int one = 1, two = 1;
+        for(int i = 0; i < n - 1; ++i){
+            int temp = one;
+            one += two;
+            two = temp;
         }
-        if (n == 2) {
-            return 2;
-        }
-        
-        int first = 1;
-        int second = 2;
-        
-        int result = 0;
-        
-        for (int i = 2; i < n; i++) {
-            result = first + second;
-            first = second;
-            second = result;
-        }
-        
-        return result;
+        return one;
     }
 };
