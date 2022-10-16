139bbc17d1e378e712d85ce2ffee328ae676cb97
Aadil <77232799+aadil42@users.noreply.github.com>
2022-10-16 16:11:12 +0530

    Create 560-subarray-sum-equals-k.js
    Solved "subarray-sum-equals-k" for JS folks.

---
+++ javascript/560-subarray-sum-equals-k.js
@@ -0,0 +1,19 @@
+var subarraySum = function(nums, k) {
+    
+   
+ 
+const prefixMap = {};
+let totalSubArray = 0;
+let ongoingSum = 0;
+
+prefixMap[0] = 1;
+for(let i = 0; i < nums.length; i++) {
+    ongoingSum += nums[i];
+    if(prefixMap[ongoingSum - k]){
+        totalSubArray += prefixMap[ongoingSum - k];
+    }
+    prefixMap[ongoingSum] = (prefixMap[ongoingSum] ? prefixMap[ongoingSum] + 1: 1);
+}
+
+return totalSubArray;
+};
