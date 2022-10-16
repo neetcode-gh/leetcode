882f4bd3e7f41dc9d8a28027ecf0846c501bfd9b
Aadil <77232799+aadil42@users.noreply.github.com>
2022-10-16 16:18:48 +0530

    Create 0209-minimum-size-subarray-sum.js
    Solved "minimum-size-subarray-sum" for JS folks.

---
+++ javascript/0209-minimum-size-subarray-sum.js
@@ -0,0 +1,21 @@
+var minSubArrayLen = function(target, nums) {
+
+    let total = 0;
+    let min_length = Number.MAX_SAFE_INTEGER;
+    let left_pointer = 0;
+    for(let i = 0; i < nums.length; i++) {
+        
+        total += nums[i];
+        while(total >= target) {
+            min_length = Math.min(min_length, i + 1 - left_pointer);
+            total -= nums[left_pointer];
+            left_pointer = left_pointer+1;
+        }
+    }
+    
+   if(min_length == Number.MAX_SAFE_INTEGER) {
+       return 0;
+   } else {
+       return min_length;
+   }
+};
