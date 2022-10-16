07ba3fa6432a91b22275e96df7f73d8f105087a2
Aadil <77232799+aadil42@users.noreply.github.com>
2022-10-16 17:47:31 +0530

    Create 0112-path-sum.js
    solved "path-sum" for JS folks.

---
+++ javascript/0112-path-sum.js
@@ -0,0 +1,18 @@
+var hasPathSum = function(root, targetSum) {
+
+    const ans = [];
+    function goDFS(node, curruntSum) {
+        
+    if(!node) return;
+    
+        if(!node.left && !node.right) {
+            ans.push(node.val + curruntSum);
+        }
+        
+        goDFS(node.left, curruntSum + node.val);
+        goDFS(node.right, curruntSum + node.val);
+    }
+    goDFS(root, 0);
+    
+    return ans.includes(targetSum);
+};
