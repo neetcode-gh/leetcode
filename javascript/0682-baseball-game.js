740b373fe56a251ff14773de61b5da9f0fee1ba3
Aadil <77232799+aadil42@users.noreply.github.com>
2022-10-16 16:27:43 +0530

    Create 0682-baseball-game.js
    solved "baseball-game" for JS folks.

---
+++ javascript/0682-baseball-game.js
@@ -0,0 +1,29 @@
+var calPoints = function(ops) {
+    
+       const outputArr = [];
+    
+    ops.forEach((op) => {
+        
+         switch(op) {
+             case 'D':
+                 outputArr.push(+outputArr[outputArr.length - 1] * 2);
+                 break;
+             case "C":
+                 outputArr.pop();
+                 break;
+             case "+":
+                outputArr.push(+outputArr[outputArr.length - 1] + +outputArr[outputArr.length - 2]);
+                 break;
+             default:
+                 outputArr.push(+op);
+         }    
+    });
+
+    console.log(outputArr);
+    
+ const result = outputArr.reduce((pre, currunt) => {
+        return currunt + pre;
+    },0);
+    
+    return result;
+};
