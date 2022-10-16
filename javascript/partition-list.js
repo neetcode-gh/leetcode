9c08324a413381e52e8775cdbbc141b565ec3955
Aadil <77232799+aadil42@users.noreply.github.com>
2022-10-16 17:18:11 +0530

    Create partition-list.js
    solved "partition-list" solved for JS folks.

---
+++ javascript/partition-list.js
@@ -0,0 +1,24 @@
+var partition = function(head, x) {
+
+ if(!head) return null;
+    
+ let lesserHead = new ListNode();
+ let greaterHead = new ListNode();
+ let lesserHeadRef = lesserHead;
+ let greaterHeadRef = greaterHead;
+    while(head) {
+        if(head.val < x) {
+            lesserHead.next = head;
+            lesserHead = lesserHead.next;
+        } else {
+            greaterHead.next = head;
+            greaterHead = greaterHead.next;
+        }
+        head = head.next;
+    }
+    greaterHead.next = null;
+    lesserHead.next = greaterHeadRef.next;
+    
+    return lesserHeadRef.next;
+    
+};
