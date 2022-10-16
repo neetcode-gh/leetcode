21cb2e34d13f9744dc1be7bd69b0496b67c59490
Aadil <77232799+aadil42@users.noreply.github.com>
2022-10-16 16:49:27 +0530

    Create 0071-simplify-path.js
    solved "simplify-path" for JS folks.

---
+++ javascript/0071-simplify-path.js
@@ -0,0 +1,24 @@
+var simplifyPath = function(path) {
+       let currunt = '';
+    let myStack = [];
+    path = '/' + path + '/';
+    for(let i = 0; i < path.length; i++) {
+    
+        if(path[i] === '/') {
+            if(currunt == '..') {
+                if(myStack.length) {
+                    myStack.pop();
+                }
+            } else if(currunt !== '' && currunt !== '.') {
+                myStack.push(currunt);
+            }
+            currunt = '';
+        } else {
+            currunt += path[i];
+        }
+    }
+
+    myStack = myStack.join('/');
+    myStack = '/' + myStack;
+    return myStack;
+};
