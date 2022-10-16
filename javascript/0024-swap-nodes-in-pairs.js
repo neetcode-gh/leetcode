e7dada5cff4c565cdfb72389be3cb99b51d76029
Aadil <77232799+aadil42@users.noreply.github.com>
2022-10-16 17:15:44 +0530

    Create 0024-swap-nodes-in-pairs.js
    solved "swap-nodes-in-pairs" for JS folks.

---
+++ javascript/0024-swap-nodes-in-pairs.js
@@ -0,0 +1,21 @@
+var swapPairs = function(head) {
+    
+    if(!head) return null;
+    const dummy = new ListNode(null, head);
+    let pre = dummy;
+    let current = head;
+   
+    while(current && current.next) {
+        const temp = current.next.next;
+        const temp1 = current.next;
+        
+        temp1.next = current;
+        current.next = temp;
+        pre.next = temp1; // why do that find out
+        
+        pre = current;
+        current = temp;
+    }
+    
+    return dummy.next;
+};
