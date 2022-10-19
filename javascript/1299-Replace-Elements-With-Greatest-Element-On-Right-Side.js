7a2a1dcc45850e912907c7914bef97b58221c293
aakhtar3 <aakhtar3@users.noreply.github.com>
2022-10-19 07:35:37 -0400

    Update 1299-Replace-Elements-With-Greatest-Element-On-Right-Side.js

--- javascript/1299-Replace-Elements-With-Greatest-Element-On-Right-Side.js
+++ javascript/1299-Replace-Elements-With-Greatest-Element-On-Right-Side.js
@@ -1,17 +1,35 @@
 /**
- * Time O(N) | Space O(1)
- * 
+ * Reverse - Space O(1)
+ * Time O(N) | Space O(N)
+ * https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
  * @param {number[]} arr
  * @return {number[]}
  */
-function replaceElements(arr) {
-    let max = -1;
-
-    for (let i = arr.length - 1; i >= 0; i--) {
-        const temp = arr[i];
-        arr[i] = max;
-        max = Math.max(max, temp);
+var replaceElements = (arr, max = -1, ans = [ -1 ]) => {    
+    arr = arr.reverse();                          /* Time O(N) */
+    
+    for (let i = 0; (i < (arr.length - 1)); i++) {/* Time O(N) */
+        max = Math.max(max, arr[i]);
+        ans[(i + 1)] = max;                           /* Space O(N) */
     }
-
-    return arr;
+    
+    return ans.reverse();                         /* Time O(N) */
 };
+
+/**
+ * Backward - In-Place
+ * Time O(N) | Space O(1)
+ * https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var replaceElements = (arr, max = -1) => {  
+     for (let i = (arr.length - 1); (0 <= i); i--) {/* Time O(N) */
+         const num = arr[i];
+
+         arr[i] = max;
+         max = Math.max(max, num);
+     }
+
+     return arr;
+ };
