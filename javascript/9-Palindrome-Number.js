47f3a34b680bc4063ba82e58aa13aef6f69fe57d
a-rinaldi <andrea.rinaldi54@gmail.com>
2022-10-19 23:03:37 +0200

    added integer reverse using modulo Solution

--- javascript/9-Palindrome-Number.js
+++ javascript/9-Palindrome-Number.js
@@ -19,3 +19,26 @@ var isPalindrome = function (x) {
 
 // Runtime: 302 ms, faster than 40.50% of JavaScript online submissions for Palindrome Number.
 // Memory Usage: 51.8 MB, less than 8.36% of JavaScript online submissions for Palindrome Number.
+
+/**
+ * Reversing Integer With Modulo
+ * Time O(N) | Space O(1)
+ * https://leetcode.com/problems/palindrome-number/
+ * @param {number} x
+ * @return {boolean}
+ */
+ var isPalindrome = function(x) {
+    if (x < 0) return false;
+
+    const inputX = x;
+    let revX = 0;
+
+    while (x > 0) {
+        revX += x % 10;
+        x = Math.floor(x / 10);
+
+        if (x > 0) revX *= 10
+    }
+
+    return revX === inputX;
+};
