21cb2d62798a5a41722cbe1117317dc73bf20e78
zim0369 <zim@onionmail.org>
2022-10-26 02:12:06 +0530

    Create 191-Number-of-1-Bits.rs

--- rust/198-House-Robber.rs
+++ rust/198-House-Robber.rs
@@ -1,18 +1,14 @@
-// time complexity: O(n)
-// space complexity: O(1)
-
 impl Solution {
-    pub fn rob(mut nums: Vec<i32>) -> i32 {
-        if nums.len() == 1{
-            return nums[0];
-        }
-        
-        nums[1] = nums[0].max(nums[1]);
-        
-        for i in 2..nums.len(){
-            nums[i] = nums[i-1].max(nums[i - 2] + nums[i]);
-        }
-        
-        *nums.last().unwrap()
+    pub fn rob(nums: Vec<i32>) -> i32 {
+        nums.into_iter()
+            .fold((0, 0), |loot, money| (loot.1, loot.1.max(loot.0 + money)))
+            .1
     }
 }
+
+
+
+ - 
+  
+
+
