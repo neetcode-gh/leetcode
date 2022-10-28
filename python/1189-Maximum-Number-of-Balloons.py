e25ec9686742017ed621cfeeb594c9726e51fc76
sezanhaque <sezansarker@gmail.com>
2022-10-28 11:24:42 +0600

    Revert "Create 1189-Maximum-Number-of-Balloons.py"
    This reverts commit 2dbe5fe397e2b52c66e08f323abf84d25efc8473.

--- python/1189-Maximum-Number-of-Balloons.py
+++
@@ -1,7 +0,0 @@
-from collections import Counter
-
-
-class Solution:
-    def maxNumberOfBalloons(self, text: str) -> int:
-        count = Counter(text)
-        return min(count["b"], count["a"], count["l"] >> 1, count["o"] >> 1, count["n"])
