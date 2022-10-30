21da44987e4b7e339dd20edafa4d17aef48d9f61
Nehal Ali <42276697+nehalalifayed@users.noreply.github.com>
2022-10-30 17:15:05 +0200

    Create 463-island-perimeter.cpp

---
+++ cpp/463-island-perimeter.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+  // Time complexity is O(M*N)
+  // Space complexity is O(1)
+  
+    int islandPerimeter(vector<vector<int>>& grid) {
+      int m = grid.size();
+      int n = grid[0].size();
+      
+      int prem = 0;
+      
+      for(int i = 0 ; i < m ; i++)
+      {
+        for(int j = 0 ; j < n ; j++)
+        {
+          if(grid[i][j] == 1) {
+            prem += 4;
+          
+            if(j > 0 && grid[i][j-1] == 1) prem -=2;
+            if(i >0 && grid[i-1][j] == 1) prem -=2;
+
+          }
+        }
+      }
+      
+      return prem;
+    }
+};
