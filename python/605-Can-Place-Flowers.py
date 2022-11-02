12e7601b0a199089c9dc20c2cc9629acc0185059
sezanhaque <sezansarker@gmail.com>
2022-11-02 17:54:32 +0600

    change solution according to neetcode video

--- python/605-Can-Place-Flowers.py
+++ python/605-Can-Place-Flowers.py
@@ -1,16 +1,21 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        flowerbed.append(0)
-        count = 1
-        for f in flowerbed:
-            if f == 0:
-                count += 1
-            else:
-                count = 0
-            if count == 3:
-                n -= 1
-                count = 1
-            if n == 0:
-                return True
-        return False
+       f = [0] + flowerbed + [0]
+       
+       for i in range(1, len(f) - 1):  # skip first & last
+           if f[i - 1] == 0 and f[i] == 0 and f[i + 1] == 0:
+               f[i] = 1
+               n -= 1
+       return n <= 0
 
+       empty = 0 if flowerbed[0] else 1
+       
+       for f in flowerbed:
+           if f:
+               n -= int((empty - 1) / 2)  # int division, round toward zero
+               empty = 0
+           else:
+               empty += 1
+
+       n -= (empty) // 2
+       return n <= 0
