db52261f9b814c229f2abdb5c77d3b8f14092481
sezanhaque <sezansarker@gmail.com>
2022-11-02 17:37:39 +0600

    change solution according to neetcode video

--- python/205-Isomorphic-Strings.py
+++ python/205-Isomorphic-Strings.py
@@ -1,3 +1,11 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        return len(set(s)) == len(set(zip(s, t))) == len(set(t))
+        mapST, mapTS = {}, {}
+
+        for c1, c2 in zip(s, t):
+            if (c1 in mapST and mapST[c1] != c2) or (c2 in mapTS and mapTS[c2] != c1):
+                return False
+            mapST[c1] = c2
+            mapTS[c2] = c1
+
+        return True
