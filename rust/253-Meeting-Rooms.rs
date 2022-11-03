c41219769ad8fd0ffcec8eebba3b95a863f664ba
Invader Zim <85027668+zim0369@users.noreply.github.com>
2022-11-03 13:23:05 +0530

    Update 253-Meeting-Rooms.rs

--- rust/253-Meeting-Rooms.rs
+++ rust/253-Meeting-Rooms.rs
@@ -1,3 +1,4 @@
+// Interval is a struct with fields `start:i32` & `end:i32`
 pub fn can_attend_meetings(intervals: Vec<Interval>) -> bool {
     let mut intervals = intervals;
     intervals.sort_unstable_by(|a, b| a.start.cmp(&b.start));
