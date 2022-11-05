967b11645ef89385642dac81247bd03b76edd3c7
Aadil <77232799+aadil42@users.noreply.github.com>
2022-11-06 05:15:25 +0530

    Solved valid-sudoku in JS.
    Solved Valid sudoku in JS

---
+++ javascript/36-valid-sudoku.js
@@ -0,0 +1,30 @@
+
+// the time complexity is constant as we only going to have a 9*9 matrix.
+// problem link:  https://leetcode.com/problems/valid-sudoku
+
+var isValidSudoku = function(board) {
+    
+
+    let rows = [];
+    let columns = [];
+    let boxes = [];
+    for(let i = 0; i < board.length; i++) {
+        rows.push(new Set());
+        columns.push(new Set());
+        boxes.push(new Set());
+    }
+
+    for(let i = 0; i < board.length; i++) {
+        for(let j = 0; j < board[0].length; j++) {
+            const boxIndex = 3 * Math.floor(i/3) + Math.floor(j/3);
+            let curruntCell = board[i][j];
+            if(rows[i].has(curruntCell) || columns[j].has(curruntCell) || boxes[boxIndex].has(curruntCell)) return false;
+            if(curruntCell == '.') continue;
+            rows[i].add(curruntCell);
+            columns[j].add(curruntCell);
+            boxes[boxIndex].add(curruntCell);
+        }
+    }
+
+    return true;
+};
