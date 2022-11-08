163f2e6689bd2361ec193d51a1315cede17ecd3e
Aadil <77232799+aadil42@users.noreply.github.com>
2022-11-08 16:40:28 +0530

    Create 203-remove-linked-list-elements.js
    Solved remove linked linked list in JS.

---
+++ javascript/203-remove-linked-list-elements.js
@@ -0,0 +1,27 @@
+// problem link https://leetcode.com/problems/remove-linked-list-elements/
+// time complexity  O(n).
+
+var removeElements = function(head, val) {
+    // let currunt = head;
+    let pre = new ListNode(null, head);
+    let dummy = pre;
+    while(head) {
+        if(head.val == val) {
+            while(head && head.val == val) {
+                head = head.next;
+            }            
+            pre.next = head;
+            pre = head;
+            if(head) {
+            temp = head;    
+            head = head.next;
+            temp = null;
+            }
+            continue;
+        }
+        pre = pre.next;
+        head = head.next;
+    }
+    
+    return dummy.next;
+};
