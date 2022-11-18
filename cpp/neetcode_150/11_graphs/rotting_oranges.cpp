6eee163e1355c216a9038e763c32401eb8167693
Alessandro Cabassa <85694967+elcabalero@users.noreply.github.com>
2022-11-18 22:09:23 +0100

    Update rotting_oranges.cpp

--- cpp/neetcode_150/11_graphs/rotting_oranges.cpp
+++ cpp/neetcode_150/11_graphs/rotting_oranges.cpp
@@ -7,7 +7,7 @@
     Time: O(m x n)
     Space: O(m x n)
 */
-
+/*
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
@@ -71,4 +71,49 @@ public:
     }
 private:
     vector<vector<int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+};
+*/
+
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int time = 0;
+        int fresh = 0;
+
+        queue<pair<int,int>> q;
+
+        vector<pair<int,int>> directions = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+
+        for (int i = 0; i < n; ++i){
+            for (int j = 0; j < m; ++j){
+                if (grid[i][j] == 1)
+                    ++fresh;
+                if (grid[i][j] == 2)
+                    q.push({i,j});
+            }
+        }
+
+        while (!q.empty() and fresh > 0){
+            int size = q.size();
+            while (size--){
+                int i = q.front().first;
+                int j = q.front().second;
+                q.pop();
+                for (auto& [x,y] : directions){
+                    int n_i = i + x;
+                    int n_j = j + y;
+                    if (n_i < 0 || n_i >= n || n_j < 0 || n_j >= m || grid[n_i][n_j] != 1)
+                        continue;
+                    grid[n_i][n_j] = 2;
+                    q.push({n_i,n_j});
+                    --fresh;
+                }
+            }
+            ++time;
+        }
+
+        return fresh ? -1 : time;
+    }
 };
