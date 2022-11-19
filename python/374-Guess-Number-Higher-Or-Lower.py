227c1b9be9b23fe84b9bb1c2df7c9c56a0e39219
aa0 <71089234+Ahmad-A0@users.noreply.github.com>
2022-11-19 22:38:40 +0000

    Update and rename pythonCreate: 374-Guess-Number-Higher-or-Lower.py to python/374-Guess-Number-Higher-Or-Lower.py

--- python/374-Guess-Number-Higher-Or-Lower.py
+++ python/374-Guess-Number-Higher-Or-Lower.py
@@ -1,10 +1,12 @@
 class Solution:
     def guessNumber(self, n: int) -> int:
         # return a num btw 1,..,n
+        
         low = 1
         high = n
+        
         while True:
-            mid = low + (high - low)//2
+            mid = low + (high - low) // 2
             myGuess = guess(mid)
             if myGuess == 1:
                 low = mid + 1
