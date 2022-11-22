f4c1d0cc0dcaf6c18661cb04cb3295983bc9c049
コリン <111611023+KORINZ@users.noreply.github.com>
2022-11-23 04:51:42 +0900

    Add recursive approach to 704-Binary-Search.py

--- python/704-Binary-Search.py
+++ python/704-Binary-Search.py
@@ -11,3 +11,18 @@ class Solution:
             else:
                 return m
         return -1
+
+        # could also use a recursive approach
+        def recursive_binary_search(start, end):
+            mid_index = start + ((end - start) // 2)
+
+            if start > end:
+                return -1
+            elif target == nums[mid_index]:
+                return mid_index
+            elif target < nums[mid_index]:
+                return recursive_binary_search(start, mid_index - 1)
+            elif target > nums[mid_index]:
+                return recursive_binary_search(mid_index + 1, end)
+
+        return recursive_binary_search(start=0, end=len(nums) - 1)
