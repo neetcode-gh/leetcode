626e6f79b3690791854a8a891873746f78aa7962
vicyyn <55297234+vicyyn@users.noreply.github.com>
2022-11-22 11:01:05 +0100

    Reduce Runtime for 295. Find Median from Data Stream
    Current code:
    - push to small
    - if max of small is greater then pop it and add it to large
    
    Proposed Solution:
    - check if num is bigger then min of large (large is not empty)
    - if so add it to large 
    - else add it to min
    
    This added a 30% increase in Runtime. (Beats)

--- python/295-Find-Median-from-Data-Stream.py
+++ python/295-Find-Median-from-Data-Stream.py
@@ -8,11 +8,10 @@ class MedianFinder:
         self.small, self.large = [], []  # maxHeap, minHeap (python default)
 
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.small, -1 * num)
-
-        if self.small and self.large and (-1 * self.small[0]) > self.large[0]:
-            val = -1 * heapq.heappop(self.small)
-            heapq.heappush(self.large, val)
+        if self.large and num > self.large[0]:
+            heapq.heappush(self.large, num)
+        else:
+            heapq.heappush(self.small, -1 * num
 
         if len(self.small) > len(self.large) + 1:
             val = -1 * heapq.heappop(self.small)
