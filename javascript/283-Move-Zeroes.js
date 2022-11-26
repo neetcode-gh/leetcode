d6e59eb1993d62bfead41f42c647e5051d52bf89
aakhtar3 <aakhtar3@users.noreply.github.com>
2022-11-26 07:02:41 -0500

    Update 283-Move-Zeroes.js

--- javascript/283-Move-Zeroes.js
+++ javascript/283-Move-Zeroes.js
@@ -1,16 +1,20 @@
 /**
+ * 2 Pointer
+ * Time O(N) | Space O(1)
+ * https://leetcode.com/problems/move-zeroes/
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
- var moveZeroes = function(nums) {
-    let pos = 0;
-    
-    for(let i in nums) {
-        if(nums[i] !== 0) {
-            let temp = nums[i];
-            nums[i] = nums[pos];
-            nums[pos] = temp;
-            pos++;
+var moveZeroes = (nums) => {
+    let [ left, right ] = [ 0, 0 ];
+
+    while (right < nums.length) {
+        const canSwap = (nums[right] !== 0)
+        if (canSwap) {
+            [nums[left], nums[right]] = [nums[right], nums[left]];
+            left++;
         }
+
+        right++;
     }
 };
