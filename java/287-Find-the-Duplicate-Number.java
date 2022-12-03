f29d90f6f8534eed510e242dd0a5ec5f7c918b45
vanamala shivaji ganesh <67234017+vanamala-19@users.noreply.github.com>
2022-12-04 02:24:12 +0530

    Update 287-Find-the-Duplicate-Number.java

--- java/287-Find-the-Duplicate-Number.java
+++ java/287-Find-the-Duplicate-Number.java
@@ -23,4 +23,31 @@ class Solution {
         }
         return slow;
     }
+    
+//     we can use do while in java
+    public int findDuplicate(int[] nums) {
+		int slow = 0,fast=0;
+		/*
+		if we assume the array in linkedList we can say the accept first index every index a digit which 
+		is possibily the index of another number (1-n) in n+1 
+		 and mathematically if c is distance btw start of loop and concerence of slow and fast
+		 						p is the distance btwn the 0th index and start of loop and
+		 						x is the distance btwn the concurent index and start of loop
+		 						we get 2*slow = fast
+		 						=> 2(p+c-x) = p+2c-x
+		 						=> p-x = 0
+		 						=> p == x
+		 */
+		do {
+			slow = nums[slow];
+			fast = nums[nums[fast]];
+		}while(slow != fast);
+		
+		int slow2 = 0;
+		do {
+			slow = nums[slow];
+			slow2 = nums[slow2];
+		}while(slow != slow2);
+        return slow;
+    }
 }
