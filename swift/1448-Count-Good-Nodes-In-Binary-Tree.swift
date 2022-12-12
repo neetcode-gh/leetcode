d3f2242e247a99d0e62672250472a325c5048059
Samuel Hinchliffe <88140540+Samuel-Hinchliffe@users.noreply.github.com>
2022-12-12 16:38:46 +0000

    112-Path-Sum JS & TS

--- swift/1448-Count-Good-Nodes-In-Binary-Tree.swift
+++ swift/1448-Count-Good-Nodes-In-Binary-Tree.swift
@@ -1,19 +1,51 @@
-//
-//  1448-Count-Good-Nodes-In-Binary-Tree.swift
-//  Question Link: https://leetcode.com/problems/count-good-nodes-in-binary-tree/
-//
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
 class Solution {
-    func goodNodes(_ root: TreeNode?) -> Int {
-        guard let root = root else { return 0 }
-        return helper(root, Int.min)
-    }
+    var globalCount: Int = 0
     
-    func helper(_ root: TreeNode?, _ lastVal: Int) -> Int {
-        guard let root = root else { return 0 }
-        let i = root.val >= lastVal ? 1 : 0
-        let left = helper(root.left, max(lastVal, root.val))
-        let right = helper(root.right, max(lastVal, root.val))
-        return i + left + right
+    func goodNodes(_ root: TreeNode?) -> Int {
+        self.globalCount = 0
+        visit(root, maxSoFar: nil)
+        return self.globalCount
     }
 }
+
+private extension Solution {
+    // DFS/Recursive traversal
+    func visit(_ node: TreeNode?, maxSoFar: Int?) -> Void {
+        // Base case 1. Node is nil
+        guard let node = node else { return }
+        
+        // Base case 2. Initial value doesn't exist (in case of root)
+        let maxVal: Int
+        if let maxSoFar = maxSoFar  {
+            maxVal = max(maxSoFar, node.val)
+        } else { 
+            maxVal = node.val
+        }
+        
+        // update global count
+        if !(maxVal > node.val) {
+            self.globalCount += 1
+        }
+        
+        // visit children
+        visit(node.left,  maxSoFar: maxVal)
+        visit(node.right, maxSoFar: maxVal)
+    }
+}
+
+
