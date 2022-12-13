c492c1b0bbfec7da58fde21a53b9f343d7701500
Kabir Dhillon <74223402+kabirdhillon7@users.noreply.github.com>
2022-12-12 18:39:16 -0800

    Create 344-Reverse-String.swift

---
+++ swift/344-Reverse-String.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func reverseString(_ s: inout [Character]) {
+        var left: Int = 0
+        var right: Int = s.count - 1
+
+        while left < right {
+            let temp: Character = s[left]
+            s[left] = s[right]
+            s[right] = temp
+
+            left += 1
+            right -= 1
+        }
+    }
+}
