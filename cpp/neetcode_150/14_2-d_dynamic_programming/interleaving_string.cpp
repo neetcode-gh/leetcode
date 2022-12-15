8c52ca9919dddd3d1a093a0fb1c84b672842be14
Alessandro Cabassa <85694967+elcabalero@users.noreply.github.com>
2022-12-15 18:58:16 +0100

    Update interleaving_string.cpp

--- cpp/neetcode_150/14_2-d_dynamic_programming/interleaving_string.cpp
+++ cpp/neetcode_150/14_2-d_dynamic_programming/interleaving_string.cpp
@@ -8,7 +8,7 @@
     Time: O(m x n)
     Space: O(m x n)
 */
-
+/*
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
@@ -37,5 +37,30 @@ private:
         
         dp[{i, j}] = false;
         return dp[{i, j}];
+    }
+};
+*/
+// Top-down solution using a DP matrix
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        int n1 = s1.size();
+        int n2 = s2.size();
+        if (n1 + n2 != s3.size())
+            return false;
+        
+        vector<vector<bool>> DP(n1+1, vector<bool>(n2+1, false));
+        DP[0][0] = true;
+
+        for (int i = 0; i <= n1; ++i){
+            for (int j = 0; j <= n2; ++j){
+                if (i > 0 && DP[i-1][j] && s1[i-1] == s3[i+j-1])
+                    DP[i][j] = true;
+                if (j > 0 && DP[i][j-1] && s2[j-1] == s3[i+j-1])
+                    DP[i][j] = true;
+            }
+        }
+
+        return DP[n1][n2];
     }
 };
