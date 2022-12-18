6e5ff8bef828a9117b894981ae28656a90d41477
turmaxx <88465473+turmaxx@users.noreply.github.com>
2022-12-18 11:20:36 -0500

    Shortest-Subarry-With-Sum-At-Least-K.py

--- python/862-Shortest-Subarray-With-Sum-At-Least-K.py
+++ python/862-Shortest-Subarray-With-Sum-At-Least-K.py
@@ -1,16 +1,18 @@
-import collections
-
 class Solution:
-    def shortestSubarray(self, nums, k) -> int:
-        d = collections.deque([[0, 0]])
-        res, cur = float('inf'), 0
+    def shortestSubarray(self, nums: List[int], k: int) -> int:
+        size = len(nums)
+        pre = [0]
+        for i in nums:
+            pre.append(pre[-1] + i)
 
-        for i, a in enumerate(nums):
-            cur += a
-            while d and cur - d[0][1] >= k:
-                res = min(res, i + 1 - d.popleft()[0])
-            while d and cur <= d[-1][1]:
-                d.pop()
-            d.append([i + 1, cur])
-
-        return res if res < float('inf') else -1
+        ans = size + 1
+        monoq = collections.deque()
+        for i, val in enumerate(pre):
+            while monoq and val <= pre[monoq[-1]]:
+                monoq.pop()
+            while monoq and val - pre[monoq[0]] >= k:
+                ans = min(ans, i - monoq.popleft())
+            
+            monoq.append(i)
+        
+        return ans if ans < size + 1 else -1   
