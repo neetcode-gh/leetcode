e0e882ec67bf3b84c2d40894a9cade11ca5a09ed
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2022-12-18 14:09:37 +0530

    I have added one more solution
    Using ASCII values

--- java/20-Valid-Parentheses.java
+++ java/20-Valid-Parentheses.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 
     public boolean isValid(String s) {
         if (s.length() % 2 != 0) return false;
@@ -21,4 +21,37 @@ class Solution {
         }
         return stack.isEmpty();
     }
+}
+
+// ASCII 
+// '(' = 40  AND ')' = 41        DIFF= 1 <=2
+// '[' = 91  AND ']' = 93        DIFF= 2 <=2
+// '{' = 123  AND '}' = 125      DIFF= 2 <=2
+// Absolute difference for non matching brackets will be more than 2
+// We are only taking one open and one close bracket in diff, there would not be both open or close
+// eg.   ( and ]  -> Abs(diff) = 53 > 2
+//       [ and }  -> Abs(diff) = 34 > 2
+
+class Solution2 {
+    public boolean isValid(String str) {
+        Stack<Character> st = new Stack<>();
+        
+        for(int i=0; i<str.length(); i++){
+            char c = str.charAt(i);
+            
+            if(c=='(' || c=='{' || c=='[' ){
+                st.push(c);   // only opening brackets will be pushed
+            }else{
+                // c -> closing bracket
+                // st.peek() -> opening bracket
+                if(!st.empty() && Math.abs(c-st.peek())<=2) 
+                    st.pop();
+                else
+                    return false;
+            }
+         }
+           
+        
+        return st.empty();
+    }       
 }
