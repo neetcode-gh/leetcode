93d00a0c074a07ca6b8c4c0074fdd9ed784fdd17
Azizbek <55094322+azizbekx@users.noreply.github.com>
2022-12-22 12:48:05 +0500

    Updated 347-Top-K_Frequent Elements
    This solution is close to python solution which is written in video. This solution is more clear to understand after watching solution video

--- java/347-Top-K-Frequent-Elements.java
+++ java/347-Top-K-Frequent-Elements.java
@@ -89,3 +89,34 @@ class Solution2 {
         array[i2] = temp;
     }
 }
+class Solution3 {
+     /**
+     * Time Complexity: O(n)
+     * Space Complexity: O(n)
+     */
+     public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+        List<Integer> bucket[] = new ArrayList[nums.length + 1];                
+        
+        for (int num : nums)
+            count.merge(num, 1, Integer::sum);
+        
+        for (int key : count.keySet()){
+            int freq = count.get(key);
+            if (bucket[freq] == null)
+                bucket[freq] = new ArrayList<>();
+            bucket[freq].add(key);
+        }
+        
+        int index = 0;
+        int[] res = new int[k];
+        for (int i = nums.length; i >= 0; i--)
+            if (bucket[i] != null)
+                for (int val : bucket[i]){
+                    res[index++] = val;
+                    if(index == k)
+                        return res;
+                }
+        return res;
+    }
+       
