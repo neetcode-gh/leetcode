4ac2297c8c7413496ab73ea8aba67e516a98d5a6
Aadil <77232799+aadil42@users.noreply.github.com>
2022-12-24 01:03:38 +0530

    Create 1984-Minimum-Difference-Between-Highest-and-Lowest-of-K-Scores.js
    Solved Minimum Difference Between Highest and Lowest of K Scores in JS.

---
+++ javascript/1984-Minimum-Difference-Between-Highest-and-Lowest-of-K-Scores.js
@@ -0,0 +1,29 @@
+/**
+ * Logarithmic time. 
+ * Time O(N + N*long(N)) | Space O(1)
+ * https://leetcode.com/problems/minimum-difference-between-highest-and-lowest-of-k-scores
+ * 
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minimumDifference = function(nums, k) {
+
+    if (k === 1) return 0;
+
+    nums = nums.sort((a, b) => {
+        return a - b;
+    });
+
+    let i = 0;
+    let j = k - 1;
+    let minDiffrence = Infinity;
+
+    while (j < nums.length) {
+        minDiffrence = Math.min(Math.abs(nums[i] - nums[j]), minDiffrence);
+        j++;
+        i++;
+    }
+
+    return minDiffrence;
+};
