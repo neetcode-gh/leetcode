337e694b97298bbf6d634031b0e0a4b5ea6d05f2
Aadil <77232799+aadil42@users.noreply.github.com>
2022-12-26 01:30:21 +0530

    Update 26-Remove-Duplicates-from-Sorted-Array.js
    Updated the code as suggested and. the splice method is removed.

--- javascript/26-Remove-Duplicates-from-Sorted-Array.js
+++ javascript/26-Remove-Duplicates-from-Sorted-Array.js
@@ -5,21 +5,20 @@
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function(nums) {
+var removeDuplicates = (nums) => {
+    let [left, right] = [0, 0];
 
-    let left = 0;
-    let right = 1;
-    let delCount = 0;
+    while (right < nums.length) {
+        const [leftVal, rightVal] = [nums[left], nums[right]];
 
-    while (right <= nums.length) {
-        if (nums[left] === nums[right]) {
-            right++;
-            delCount++;
-        } else {
-            nums.splice(left + 1, delCount);
-            delCount = 0;
+        const isEqual = (rightVal === leftVal);
+        if (!isEqual) {
             left++;
-            right = left + 1;
+            nums[left] = rightVal;
         }
+
+        right++;
     }
+
+    return (left + 1);
 };
