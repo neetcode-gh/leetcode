4c9fd50d1cf37c29502a444c1c056bba2ec276b8
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2022-12-25 19:34:24 +0400

    Update 104-Maximum-Depth-of-Binary-Tree.java

--- java/104-Maximum-Depth-of-Binary-Tree.java
+++ java/104-Maximum-Depth-of-Binary-Tree.java
@@ -1,7 +1,27 @@
+// Recursive DFS
 class Solution {
-
     public int maxDepth(TreeNode root) {
         if (root == null) return 0;
         return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
+
+// BFS
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int levels = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.poll();
+                if (current.left != null) queue.add(current.left);
+                if (current.right != null) queue.add(current.right);
+            }
+            levels++;
+        }
+        return levels;
     }
 }
