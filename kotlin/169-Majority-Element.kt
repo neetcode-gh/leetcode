c8998e12d873ee894cb52aeee770fb154584c7d8
unknown <aviamirswe@gmail.com>
2022-12-27 16:50:26 +0100

    Create 169-Majority-Element.kt

---
+++ kotlin/169-Majority-Element.kt
@@ -0,0 +1,17 @@
+class Solution {
+    //Boyer–Moore string-search algorithm
+    fun majorityElement(nums: IntArray): Int {
+        var res = 0; var count = 0
+        for(i in nums.indices){
+            val found = nums[i]
+            if(count == 0){
+                res = found
+                count++
+            }else if(found == res)
+                count++
+            else
+                count--
+        }   
+        return res
+    }
+}
