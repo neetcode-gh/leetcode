78ee7dcbe848627207e3a6db47b8510c3e11cdd5
GarretTomlin <garrettomlin808@gmail.com>
2022-12-28 12:45:59 -0500

    go: added 0058 Length of last word

---
+++ 0058-Length-of-Last-Word.go
@@ -0,0 +1,31 @@
+func lengthOfLastWord(s string) int {
+    len := 0
+    for i := range s {
+        if s[i] != ' ' {
+            if s[i-1] == ' ' {
+                len = 1
+            } else {
+                len += 1
+            }
+        }
+    }
+    return len
+}
+
+// another approach. starting out from the last so we don't have to go all the way to the end.
+
+func lengthOfLastWord(s string) int {
+    firstCharOccurance := false
+    lastWordLen := 0
+
+    for i := len(s) - 1; i > -1; i-- {
+        if s[i] != ' ' {
+            firstCharOccurance = true
+            lastWordLen++
+        }
+        if firstCharOccurance && s[i] == ' ' {
+            break
+        }
+    }
+    return lastWordLen
+}
