2228dea183efe7053a859dba002538ae60462701
Tahsin Tunan <tahsintunan@gmail.com>
2022-12-29 01:14:34 +0600

    Create 9. Palindrome Number
    Signed-off-by: Tahsin Tunan <tahsintunan@gmail.com>

---
+++ go/0009-palindrome-number.go
@@ -0,0 +1,10 @@
+func isPalindrome(x int) bool {
+	s := strconv.Itoa(x)
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		if r[i] != r[j] {
+			return false
+		}
+	}
+	return true
+}
