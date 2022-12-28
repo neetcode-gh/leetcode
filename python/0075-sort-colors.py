41ef3953b41ef3b254562935ebb19cfd9426530c
zvdy <cristian.030902@gmail.com>
2022-12-28 22:53:30 +0100

    python/0075-sort-colors.py

--- python/0075-sort-colors.py
+++ python/0075-sort-colors.py
@@ -2,7 +2,7 @@ class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
-        """
+        """ 
         l = 0
         r = len(nums) - 1
         i = 0
