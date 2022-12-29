a9e2670eca522a0dae7cb1180eb5178bce4fff17
AP-Repositories <106278457+AP-Repositories@users.noreply.github.com>
2022-12-29 14:07:27 -0600

    Create 0203-remove-linked-list-elements.java

---
+++ java/0203-remove-linked-list-elements.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode prev = dummy, curr = head;
+        while(curr != null) {
+            ListNode nxt = curr.next;
+            
+            if(curr.val == val)
+                prev.next = nxt;
+            else
+                prev = curr;
+            
+            curr = nxt;
+        }
+        return dummy.next;
+    }
+}
