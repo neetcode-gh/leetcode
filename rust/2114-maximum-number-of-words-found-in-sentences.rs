e857729bda795c08c689444d6b55a58a1548fde9
heronet <sirat4757@gmail.com>
2022-12-29 16:38:42 +0600

    1480 2114 2469 2235

---
+++ rust/2114-maximum-number-of-words-found-in-sentences.rs
@@ -0,0 +1,13 @@
+struct Solution;
+impl Solution {
+    pub fn most_words_found(sentences: Vec<String>) -> i32 {
+       let mut most = 0;
+       for sentence in sentences {
+           let size = sentence.split_whitespace().count();
+           if size > most {
+               most = size;
+           }
+       }
+       return most as i32;
+    }
+}
