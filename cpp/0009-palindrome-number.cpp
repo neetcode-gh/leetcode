7e27c99fd645028f33fef4a7a118bf29dc0f287c
AP-Repositories <106278457+AP-Repositories@users.noreply.github.com>
2022-12-30 14:14:07 -0600

    Create 0009-palindrome-number.cpp

---
+++ cpp/0009-palindrome-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0) return false;
+        
+        long div = 1;
+        while(x >= 10 * div)
+            div *= 10;
+        
+        while(x) {
+            int right = x % 10;
+            int left = x / div;
+            
+            if(left != right) return false;
+            
+            x = (x % div) / 10;
+            div = div / 100;
+        }
+        return true;
+    }
+};
