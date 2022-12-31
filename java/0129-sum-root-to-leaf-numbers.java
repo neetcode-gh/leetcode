0ce05fb2a128bf84c6f78577d1e1ddeaf08ca93a
Jonathan Diaz <johndiaz693@gmail.com>
2022-12-31 09:58:20 -0800

    Create 0129-Sum-Root-To-Leaf-Numbers.java

--- java/0129-sum-root-to-leaf-numbers.java
+++ java/0129-sum-root-to-leaf-numbers.java
@@ -13,6 +13,46 @@
  *     }
  * }
  */
+// solution from the video
+class Solution {
+    /*
+        Time complexity: O(V) where V is the number of vertices
+        Space complexity: O(h) where h is the height of the tree
+    */
+
+    public int sumNumbers(TreeNode root) {
+        // track the sum that we want to add
+        int solution = 0;
+
+        // execute a dfs to find the leaf nodes
+        solution = findLeafNodes(root, solution);
+
+        // return the solution
+        return solution;
+    }
+
+    // dfs method
+    public int findLeafNodes(TreeNode node, int currentPath){
+        // base case, if no node then return 0
+        if(node==null){
+            return 0;
+        }
+
+        // add the current node value to the currentPath (move decimal to right by 1 and add)
+        currentPath = (currentPath * 10) + node.val;
+
+        // if we are at a non-null node, check if it is a leaf
+        if(node.left==null && node.right==null){
+            // return the solution
+            return currentPath;
+        }
+
+        // check find the leaf nodes on the left and right
+        return findLeafNodes(node.left, currentPath) + findLeafNodes(node.right, currentPath);
+    }
+}
+
+// solution using strings
 class Solution {
     /*
         Time complexity: O(V) where V is the number of vertices
