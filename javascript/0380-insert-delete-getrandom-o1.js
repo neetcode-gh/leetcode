9114e03975602883a95bbc05513fa35297457e2f
Andy Wong <andynullwong@gmail.com>
2022-12-31 12:22:08 -0500

    Update 0380-insert-delete-getrandom-o1.js

--- javascript/0380-insert-delete-getrandom-o1.js
+++ javascript/0380-insert-delete-getrandom-o1.js
@@ -1,38 +1,37 @@
-var RandomizedSet = function() {
+var RandomizedSet = function () {
     this.set = new Set();
 };
 
-/** 
+/**
  * @param {number} val
  * @return {boolean}
  */
-RandomizedSet.prototype.insert = function(val) {
-    if (!this.set.has(val)) {
+RandomizedSet.prototype.insert = function (val) {
+    const res = !this.set.has(val);
+    if (res) {
         this.set.add(val);
-        return true;
-    } else {
-        return false;
     }
+    return res;
 };
 
-/** 
+/**
  * @param {number} val
  * @return {boolean}
  */
-RandomizedSet.prototype.remove = function(val) {
+RandomizedSet.prototype.remove = function (val) {
     return this.set.delete(val);
 };
 
 /**
  * @return {number}
  */
-RandomizedSet.prototype.getRandom = function() {
+RandomizedSet.prototype.getRandom = function () {
     const keys = Array.from(this.set.keys());
     const seed = Math.floor(Math.random() * keys.length);
     return keys[seed];
 };
 
-/** 
+/**
  * Your RandomizedSet object will be instantiated and called as such:
  * var obj = new RandomizedSet()
  * var param_1 = obj.insert(val)
