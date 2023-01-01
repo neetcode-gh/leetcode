f70c23559d487613487e097b0ede807b719cc392
Hiten-Tandon <66129059+Hiten-Tandon@users.noreply.github.com>
2023-01-01 22:44:57 +0530

    Updated solution to a correct one
    Updated solution of problem 98 Validate BST to a correct one

--- rust/0098-validate-binary-search-tree.rs
+++ rust/0098-validate-binary-search-tree.rs
@@ -3,21 +3,17 @@ use std::rc::Rc;
 
 impl Solution {
     pub fn is_valid_bst(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
-        Self::inorder(root).1
+        Self::inorder(root).windows(2).all(|window| window[0] < window[1])
     }
 
-    fn inorder(node : Option<Rc<RefCell<TreeNode>>>) -> (Option<i32>, bool) {
+    fn inorder(node : Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
         if let Some(node) = node {
-            let val = node.borrow().val;
-            let (left_val, left_state) = Self::inorder(node.borrow_mut().left.take());
-            let (right_val, right_state) = Self::inorder(node.borrow_mut().right.take());
-            
-            (Some(val), left_state && 
-            right_state && 
-            if let Some(left_val) = left_val {left_val < val} else {true} && 
-            if let Some(right_val) = right_val {val < right_val} else {true})
+            let mut res = Self::inorder(node.borrow_mut().left.take());
+            res.push(node.borrow().val);
+            res.extend(&Self::inorder(node.borrow_mut().right.take()));
+            res
         } else {
-            (None, true)
+            Vec::with_capacity(10_000)
         }
     }
 }
