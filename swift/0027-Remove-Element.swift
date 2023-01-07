ace28dbdc2ced63a8e43f1205072de95098b63b4
Kabir Dhillon <74223402+kabirdhillon7@users.noreply.github.com>
2023-01-06 19:40:11 -0800

    Update 0027-Remove-Element.swift

--- swift/0027-Remove-Element.swift
+++ swift/0027-Remove-Element.swift
@@ -1,20 +1,12 @@
 class Solution {
     func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
-        // keep track of the count, init to 0
         var count: Int = 0
 
-        // (for) go through nums
-            // if nums[i] !- val
-                // set nums[count] = nums[i]
-                // increment count by 1
-        for i in nums {
-            if i != val {
-                nums[count] = i
-                count += 1
-            }
+        for i in nums where i != val {
+            nums[count] = i
+            count += 1
         }
         
-        // return count
         return count
     }
 }
