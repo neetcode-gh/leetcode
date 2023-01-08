de27d94d094a71bed4a18f8ebd2261851120a882
Ankush kumar <91266368+Ankush11903@users.noreply.github.com>
2023-01-08 19:12:18 +0530

    Update and rename 0122 - Best Time to Buy And Sell Stock II to 0122 - Best Time to Buy And Sell Stock II.cpp

--- cpp/0122 - Best Time to Buy And Sell Stock II.cpp
+++ cpp/0122 - Best Time to Buy And Sell Stock II.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
+    int maxProfit(vector<int>& prices) { 
         int ans=0;
         for(int i=0;i<prices.size()-1;i++){
             if(prices[i]<prices[i+1]){
