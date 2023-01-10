afb94e93bdd101f0390638c1aa8a65eb4765e8e4
Alessandro Cabassa <85694967+elcabalero@users.noreply.github.com>
2023-01-10 22:49:18 +0100

    Update copy-list-with-random-pointer.cpp as in the video
    Problem: 0138-copy-list-with-random-pointer.cpp
    Language: C++
    Submission: https://leetcode.com/problems/copy-list-with-random-pointer/submissions/875731269/

--- cpp/0138-copy-list-with-random-pointer.cpp
+++ cpp/0138-copy-list-with-random-pointer.cpp
@@ -55,7 +55,7 @@ public:
 //         return visited[node];
 //     }
 // };
-
+/*
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
@@ -97,5 +97,27 @@ public:
         }
         
         return oldHead;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        unordered_map<Node*, Node*> nodes;
+        Node* h = head;
+        
+        while (h){
+            nodes[h] = new Node(h->val);
+            h = h->next;
+        }
+        h = head;
+        while (h){
+            Node* newNode = nodes[h];
+            newNode->next = nodes[h->next];
+            newNode->random = nodes[h->random];
+            h = h->next;
+        }
+        return nodes[head];
     }
 };
