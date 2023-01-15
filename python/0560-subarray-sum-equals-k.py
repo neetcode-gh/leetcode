42da52b52ce5880a6d121d46de9f4618bc36c1d4
AakashPawanGPS <aakashpawangps@gmail.com>
2023-01-16 01:00:29 +0530

    0560-subarray-sum-equals-k.py added and Updated README

---
+++ python/0560-subarray-sum-equals-k.py
@@ -0,0 +1,12 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count = 0
+        sum = 0
+        dic = {}
+        dic[0] = 1
+        for i in range(len(nums)):
+            sum += nums[i]
+            if sum-k in dic:
+                count += dic[sum-k]
+            dic[sum] = dic.get(sum, 0)+1
+        return count
