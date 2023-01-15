7f77a59697b50f851959c8b5c07c84dacef482cd
AakashPawanGPS <aakashpawangps@gmail.com>
2023-01-16 01:11:41 +0530

    0560-subarray-sum-equals-k.py added and Updated README

--- python/0560-subarray-sum-equals-k.py
+++ python/0560-subarray-sum-equals-k.py
@@ -10,3 +10,9 @@ class Solution:
                 count += dic[sum-k]
             dic[sum] = dic.get(sum, 0)+1
         return count
+
+# Time Complexity :
+#     O(N) -> Where N is the size of the array and we are iterating over the array once
+
+# Space Complexity:
+#     O(N) -> Creating a hashmap/dictionary
