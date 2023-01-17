166d265efff823a47f208492d741c2949d904306
Mahmoud Hamed <63455983+MHamiid@users.noreply.github.com>
2023-01-17 16:03:26 +0200

    Update 1968-array-with-elements-not-equal-to-average-of-neighbors.cpp

--- cpp/1968-array-with-elements-not-equal-to-average-of-neighbors.cpp
+++ cpp/1968-array-with-elements-not-equal-to-average-of-neighbors.cpp
@@ -6,7 +6,10 @@ public:
             int a=nums[i-1];
             int b=nums[i];
             int c=nums[i+1];
-            if(a>b && b>c || a<b && b<c) swap(nums[i],nums[i+1]);
+            if(a>b && b>c || a<b && b<c)
+            {
+                swap(nums[i],nums[i+1]);
+            }
         }
         return nums;
     }
