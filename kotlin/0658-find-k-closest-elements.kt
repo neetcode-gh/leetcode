b562f754e9b8b5716c87efe68ab1f6238780c5ef
a93a <aviamirswe@gmail.com>
2023-02-03 13:33:17 +0100

    Update 0658-find-k-closest-elements.kt (Optimization)
    Added solution using binary search instead of linear. Both solutions are in the file.

--- kotlin/0658-find-k-closest-elements.kt
+++ kotlin/0658-find-k-closest-elements.kt
@@ -1,4 +1,27 @@
 /*
+* Optimized solution with binary search for the k-window instead of linear search. Time O(LogN-K) and Space O(1)
+*/
+class Solution {
+    fun findClosestElements(arr: IntArray, k: Int, x: Int): List<Int> {
+
+        var left = 0
+        var right = arr.size - k
+
+        while (left < right) {
+            val mid = (left + right) / 2
+            if (x - arr[mid] > arr[mid + k] - x)
+                left = mid + 1
+            else
+                right = mid
+        }
+
+        val res = ArrayList<Int>()
+        for(i in left until right+k) res.add(arr[i])
+        return res
+    }
+}
+
+/*
 * Linear search with window resizing. Time: O(N-K) and Space O(1) 
 */
 class Solution {
