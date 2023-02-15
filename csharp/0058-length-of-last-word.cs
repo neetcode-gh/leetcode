e1a7010997d053ad7e2a8f2e9d25164523aec1dc
yubaozhang <zhangyubao99@gmail.com>
2023-02-14 22:20:56 -0800

    delete csharp project

--- csharp/0058-length-of-last-word.cs
+++
@@ -1,8 +0,0 @@
-public class Solution {
-    public int LengthOfLastWord(string s) {
-        s = s.TrimEnd();
-        string[] arr = s.Split(' ');
-        return arr[arr.Length - 1].Length;
-    }
-}
-
--- csharp/0929-unique-email-addresses.cs
+++
@@ -1,9 +0,0 @@
-public class Solution {
-    public int NumUniqueEmails(string[] emails) 
-	=> emails.Select(x => x.Split('@')).
-              Select(split => split[0].
-              Split('+')[0].
-              Replace(".", "") + "@" + split[1]).
-              Distinct().
-              Count();
-}
