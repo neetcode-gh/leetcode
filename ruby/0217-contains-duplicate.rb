932b02396c93bc49897cd955914d762588f8d27b
yubaozhang <zhangyubao99@gmail.com>
2023-02-14 22:21:11 -0800

    delete ruby project

--- ruby/0217-contains-duplicate.rb
+++
@@ -1,9 +0,0 @@
-def contains_duplicate(nums)
-  hash = {}
-  nums.each do |num|
-    return true if hash.key? num
-
-    hash[num] = true
-  end
-  false
-end
--- ruby/0226-invert-binary-tree.rb
+++
@@ -1,9 +0,0 @@
-def invert_tree(root)
-  return root if root.nil?
-  return root if root.left.nil? && root.right.nil?
-
-  root.left, root.right = root.right, root.left
-  invert_tree(root.left)
-  invert_tree(root.right)
-  root
-end
