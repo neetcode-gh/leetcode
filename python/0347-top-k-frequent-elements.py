1c02e3d38c83898558a5424a49168ba8d81c0e6e
Tatsuran <tatsuran@gmail.com>
2023-02-17 04:37:33 +0200

    Update 0347-top-k-frequent-elements.py
    Changed freq to a cleaner variant
    Changed unpacking freq to res from two nested for loops O(n^2) to O(n)

--- python/0347-top-k-frequent-elements.py
+++ python/0347-top-k-frequent-elements.py
@@ -1,7 +1,7 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = {}
-        freq = [[] for i in range(len(nums) + 1)]
+        freq =  [[] for _ in nums] + [[]]
 
         for n in nums:
             count[n] = 1 + count.get(n, 0)
@@ -9,10 +9,9 @@ class Solution:
             freq[c].append(n)
 
         res = []
-        for i in range(len(freq) - 1, 0, -1):
-            for n in freq[i]:
-                res.append(n)
-                if len(res) == k:
-                    return res
+        for n in freq[::-1]:
+            res += n
+            if len(res) == k:
+                return res
 
         # O(n)
