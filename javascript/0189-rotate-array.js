8de25ca9b2317443221424f1fb23adb8e6bb9bf6
ADITHYA KRISHNA <90098332+AdithyaKrishna169@users.noreply.github.com>
2023-03-08 10:15:17 +0530

    Create 0189-rotate-array.js

---
+++ javascript/0189-rotate-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+    const n = nums.length;
+    k = k % n;
+    reverse(nums, 0, n - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, n - 1);
+};
+
+function reverse(nums, start, end) {
+    while (start < end) {
+        [nums[start], nums[end]] = [nums[end], nums[start]];
+        start++;
+        end--;
+    }
+}
