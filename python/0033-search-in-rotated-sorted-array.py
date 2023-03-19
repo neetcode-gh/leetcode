7ffffaeb5e8ff63ba7df22f1a4e8a2c083c52f0b
sefa can  genc <30600216+s3f4-g@users.noreply.github.com>
2023-03-19 23:48:59 +0300

    Update 0033-search-in-rotated-sorted-array.py
    i edited the "or" cases in if statements  i think this is better for understanding of concept

--- python/0033-search-in-rotated-sorted-array.py
+++ python/0033-search-in-rotated-sorted-array.py
@@ -1,22 +1,27 @@
-class Solution:
+class Solution: 
     def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums) - 1
 
-        while l <= r:
-            mid = (l + r) // 2
-            if target == nums[mid]:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+        
+            mid = (left + right) // 2
+        
+            if nums[mid] == target:
                 return mid
-
+                
             # left sorted portion
-            if nums[l] <= nums[mid]:
-                if target > nums[mid] or target < nums[l]:
-                    l = mid + 1
+            if nums[left] <= nums[mid]:
+            
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
                 else:
-                    r = mid - 1
+                    left = mid + 1
+            
             # right sorted portion
-            else:
-                if target < nums[mid] or target > nums[r]:
-                    r = mid - 1
+            else:     
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
                 else:
-                    l = mid + 1
+                    right = mid - 1
         return -1
