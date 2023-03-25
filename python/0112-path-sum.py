6001f964a1ed3bc54dc92be744e8ed8ebe0b1360
Shivansh Srivastava <ss5945@columbia.edu>
2023-03-25 14:41:30 -0400

    Added solution for #112: Path Sum in Python

---
+++ python/0112-path-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasPathSum(self, root, sum):
+        if not root:
+            return False
+
+        sum -= root.val
+        if not root.left and not root.right:
+            return sum == 0
+        return self.hasPathSum(root.left, sum) or self.hasPathSum(root.right, sum)
