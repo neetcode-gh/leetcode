0e7228288dde10569432e24b7cae51d3061b4d8a
Ali <aviamirswe@gmail.com>
2023-05-04 20:29:29 +0200

    Update 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.kt

--- kotlin/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.kt
+++ kotlin/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.kt
@@ -1,7 +1,47 @@
-package kotlin
+/*
+* DFS Solution
+*/
+class Solution {
+    fun minReorder(n: Int, connections: Array<IntArray>): Int {
 
-import java.util.LinkedList
+        val neighbors = ArrayList<ArrayList<Int>>().apply {
+            for (i in 0 until n)
+                this.add(ArrayList<Int>())
+            for ((u,v) in connections) {
+                this.get(u).apply { this.add(v) }
+                this.get(v).apply { this.add(u) }
+            }
+        }
 
+        val edges = HashSet<String>().apply {
+            for ((u,v) in connections)
+                this.add("$u:$v")
+        }
+
+        var changes = 0
+        val visited = BooleanArray(n)
+
+        fun dfs(n: Int) {
+            for (nei in neighbors[n]) {
+                if (visited[nei] == true)
+                    continue
+                if ("$nei:$n" !in edges)
+                    changes++
+                visited[nei] = true
+                dfs(nei)
+            }
+        }
+
+        visited[0] = true
+        dfs(0)
+
+        return changes
+    }
+}
+
+/*
+* BFS Solution
+*/
 class Solution {
     fun minReorder(n: Int, connections: Array<IntArray>): Int {
         // node -> (adjacentNode,isArtificialEdge)
