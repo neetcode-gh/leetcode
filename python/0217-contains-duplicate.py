21af3b806a4b3e73ca64ff10caee6f4e6f8a3a3d
Khizar Amin <khizaramin95@gmail.com>
2023-05-09 20:51:46 +0500

    Update 0217-contains-duplicate.py
    converting the list directly to set will remove the need to add elements to it individually. if the length of the set is smaller than the length of the input list, then that would mean there were duplicates in the list that have been removed.

--- python/0217-contains-duplicate.py
+++ python/0217-contains-duplicate.py
@@ -1,9 +1,3 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        hashset = set()
-
-        for n in nums:
-            if n in hashset:
-                return True
-            hashset.add(n)
-        return False
+        return not(len(set(nums)) == len(nums))
