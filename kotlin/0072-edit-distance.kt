3edbdb91fe6209f8f33e8b1a6a27452cef01d98e
Ali <aviamirswe@gmail.com>
2023-05-15 22:49:46 +0200

    Update 0072-edit-distance.kt

--- kotlin/0072-edit-distance.kt
+++ kotlin/0072-edit-distance.kt
@@ -1,3 +1,6 @@
+/*
+* DP Time O(m*n) and space O(m*n)
+*/
 class Solution {
     fun minDistance(word1: String, word2: String): Int {      
 
@@ -23,7 +26,37 @@ class Solution {
                 }
             }
         }
-
+     
         return cache[0][0]
+    }
+}
+
+/*
+* DP Time O(m*n) and optimized space O(n)
+*/
+class Solution {
+    fun minDistance(word1: String, word2: String): Int { 
+        val m = word1.length
+        val n = word2.length    
+        var prev = IntArray(n + 1) { it }
+
+        for (i in 1..m) {
+            val new = IntArray(n + 1)
+            new[0] = i
+            for (j in 1..n) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    new[j] = prev[j - 1]
+                } else {
+                    new[j] = 1 + minOf(
+                        prev[j], 
+                        prev[j - 1], 
+                        new[j - 1]
+                    )
+                }
+            }
+            prev = new
+        }
+     
+        return prev[n]
     }
 }
