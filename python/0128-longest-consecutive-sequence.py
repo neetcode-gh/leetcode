b0c959e3a1e4c6e5d3182238058e5873bdea28eb
ShashankZobb <shashanknke1112@gmail.com>
2023-05-28 19:57:02 +0530

    fixed small issue

--- python/0128-longest-consecutive-sequence.py
+++ python/0128-longest-consecutive-sequence.py
@@ -1,9 +1,9 @@
 class Solution:
-    def longestConsecutive(self, nums: List[int]) -> int:
+    def longestConsecutive(self, nums: list[int]) -> int:
         numSet = set(nums)
         longest = 0
 
-        for n in nums:
+        for n in numSet:
             # check if its the start of a sequence
             if (n - 1) not in numSet:
                 length = 1
