3db953510270184a3dc85a9180ca1f0ee72abda1
Bhavyaa Trivedi <bhavyaatrivedi2002@gmail.com>
2023-05-28 21:19:48 +0530

    removed the reused code and did the fix

--- cpp/0021-merge-two-sorted-lists.cpp
+++ cpp/0021-merge-two-sorted-lists.cpp
@@ -21,26 +21,9 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if (list1 == NULL && list2 == NULL) {
-            return NULL;
-        }
-        if (list1 == NULL) {
-            return list2;
-        }
-        if (list2 == NULL) {
-            return list1;
-        }
-        
-        ListNode* head = NULL;
-        if (list1->val <= list2->val) {
-            head = list1;
-            list1 = list1->next;
-        } else {
-            head = list2;
-            list2 = list2->next;
-        }
-        ListNode* curr = head;
-        
+        ListNode* dummy = new ListNode();
+        ListNode* curr = dummy;
+
         while (list1 != NULL && list2 != NULL) {
             if (list1->val <= list2->val) {
                 curr->next = list1;
@@ -51,13 +34,13 @@ public:
             }
             curr = curr->next;
         }
-        
+
         if (list1 == NULL) {
             curr->next = list2;
         } else {
             curr->next = list1;
         }
-        
-        return head;
+
+        return dummy->next;
     }
 };
