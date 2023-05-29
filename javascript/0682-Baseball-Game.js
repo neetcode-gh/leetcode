c4ba73bce93d90cb3c1bc1529e61212c48c9e0e9
Aarti Rai <aartijbm@gmail.com>
2023-05-29 15:07:09 +0530

    0392-Is Subsequence
    Solution in Ruby

--- javascript/0682-Baseball-Game.js
+++ javascript/0682-Baseball-Game.js
@@ -2,28 +2,18 @@
  * @param {string[]} operations
  * @return {number}
  */
-var calPoints = function(operations) {
-    let runningSum = 0;
+var calPoints = function (operations) {
     const stack = [];
-    for(const o of operations) {
-        if(o === 'C') {
-            runningSum -= stack.pop();
-            continue;
+    for(const op of operations){
+        if(op==="+"){
+            stack.push(stack[stack.length - 1] + stack[stack.length - 2]);
+        }else if(op==="C"){
+            stack.pop()
+        }else if(op==="D"){
+            stack.push(stack[stack.length - 1] * 2);
+        }else{
+            stack.push(parseInt(op))
         }
-        if(o === 'D') {
-            const val = stack[stack.length - 1] * 2;
-            stack.push(val);
-            runningSum += val;
-            continue;
-        }
-        if(o === '+') {
-            const val = stack[stack.length - 1] + stack[stack.length - 2];
-            stack.push(val);
-            runningSum += val;
-            continue;
-        }
-        stack.push(+o);
-        runningSum += +o;
     }
-    return runningSum;
+    return stack.reduce((prev,curr)=>prev+curr,0)
 };
