26e11a71857bfa408f3a7c401f5ced466c60617d
Bhavyaa Trivedi <bhavyaatrivedi2002@gmail.com>
2023-05-29 08:29:02 +0530

    added edge  case

--- cpp/0021-merge-two-sorted-lists.cpp
+++ cpp/0021-merge-two-sorted-lists.cpp
@@ -1,13 +1,3 @@
-/*
-    Given heads of 2 sorted linked lists, merge into 1 sorted list
-    Ex. list1 = [1,2,4], list2 = [1,3,4] -> [1,1,2,3,4,4]
-
-    Create curr pointer, iterate thru, choose next to be lower one
-
-    Time: O(m + n)
-    Space: O(1)
-*/
-
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -21,10 +11,18 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (list1 == NULL && list2 == NULL) {
+            return NULL;
+        } 
+        if (list1 == NULL) {
+            return list2;
+        }
+        if (list2 == NULL) {
+            return list1;
+        }
         ListNode* dummy = new ListNode();
         ListNode* curr = dummy;
-
-        while (list1 != NULL && list2 != NULL) {
+        while (list1!=NULL && list2!=NULL) {
             if (list1->val <= list2->val) {
                 curr->next = list1;
                 list1 = list1->next;
@@ -34,13 +32,12 @@ public:
             }
             curr = curr->next;
         }
-
         if (list1 == NULL) {
             curr->next = list2;
-        } else {
+        }   
+        if (list2 == NULL) {
             curr->next = list1;
-        }
-
+        }        
         return dummy->next;
     }
 };
