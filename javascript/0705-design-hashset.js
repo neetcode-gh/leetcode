b22c4c002068fe3096ba06c5081312681fdb84d0
Aadil <77232799+aadil42@users.noreply.github.com>
2023-06-06 21:19:39 +0530

    Create 0705-design-hashset.js
    Solved design-hashset
    
    File(s) Added: 0705-design-hashset.js
    Language(s) Used: JavaScript
    Submission URL: https://leetcode.com/problems/design-hashset/submissions/647414890/

---
+++ javascript/0705-design-hashset.js
@@ -0,0 +1,40 @@
+
+//https://leetcode.com/problems/design-hashset
+var MyHashSet = function() {
+    this.set = [];
+};
+
+/** 
+ * Time O(1) | Space O(1)
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.add = function(key) {
+    this.set[key] = key;
+};
+
+/** 
+ * Time O(1) | Space O(1)
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.remove = function(key) {
+    this.set[key] = undefined;
+};
+
+/** 
+ * Time O(1) | Space O(1)
+ * @param {number} key
+ * @return {boolean}
+ */
+MyHashSet.prototype.contains = function(key) {
+    return this.set[key] !== undefined;
+};
+
+/** 
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
