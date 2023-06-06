c8f9c648ee4dfae9b7e1f9a7e053500adfac4be9
Briggs Miller <acidbriggs@gmail.com>
2023-06-05 21:49:39 -0400

    Update 0125-valid-palindrome.js
    * Clarified some variable and internal function names
    * Removed if/else conditions
    * Introduced new variables to increase clarity

--- javascript/0125-valid-palindrome.js
+++ javascript/0125-valid-palindrome.js
@@ -64,22 +64,24 @@ var isPalindrome = function(s) {
  * @return {boolean}
  */
 var isPalindrome = function (s) {
-  const isAlpha = c => (c.toLowerCase() >= 'a' && c.toLowerCase() <= 'z') || c >= '0' && c <= '9'
+  const isAlphaNumeric = c => (c.toLowerCase() >= 'a' && c.toLowerCase() <= 'z') || c >= '0' && c <= '9'
 
-  let i = 0;
-  let j = s.length - 1;
+  let left = 0;
+  let right = s.length - 1;
+  let skipLeft, skipRight, endsEqual = false;
+  
+  while (left < right) {
+    skipLeft = !isAlphaNumeric(s.charAt(left))
+    if (skipLeft) { left++; continue; }
 
-  while (i < j) {
-    if (!isAlpha(s.charAt(i))) {
-      i++
-    } else if (!isAlpha(s.charAt(j))) {
-      j--
-    } else if (s.charAt(i).toLowerCase() !== s.charAt(j).toLowerCase()) {
-      return false
-    } else {
-      i++
-      j--
-    }
+    skipRight = !isAlphaNumeric(s.charAt(right))
+    if (skipRight) { right--; continue; }
+
+    endsEqual = s.charAt(left).toLowerCase() === s.charAt(right).toLowerCase()
+    if (!endsEqual) return false
+
+    left++
+    right--
   }
   return true
 };
