589d759c009f5e4787032a686b7992a89f63df27
Chaitanya Bhardwaj <chaitanyabhardwaj89@gmail.com>
2023-06-07 23:42:43 +0530

    Solution with better time complexity O(m+n)

--- java/0496-next-greater-element-i.java
+++ java/0496-next-greater-element-i.java
@@ -1,26 +1,15 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        
-        int[] res = new int[nums1.length];
-        int counter=0;
-        
-        for(int i: nums1){
-            res[counter++]=ans(i, nums2);
+        var map = new HashMap<Integer,Integer>();
+        var stack = new ArrayDeque<Integer>();
+        stack.push(nums2[0]);
+        for(var i = 1; i < nums2.length; i++) {
+            while(!stack.isEmpty() && stack.peek() < nums2[i]) map.put(stack.pop(), nums2[i]);
+            stack.push(nums2[i]);
         }
-        
-        return res;
-        
-    }
-    
-    private int ans(int i, int[] nums){
-        for(int n=0; n<nums.length; n++){
-            if(nums[n]==i){
-                for(int j=n+1; j<nums.length; j++){
-                    if(nums[j]>i)
-                        return nums[j];
-                }
-            }
-        }
-        return -1;
+        var i = 0;
+        for(int n : nums1)
+            nums1[i++] = map.getOrDefault(n, -1);
+        return nums1;
     }
 }
