c9690d2905354795bd57adae595f9427e7740930
Aadil <77232799+aadil42@users.noreply.github.com>
2023-06-18 20:47:10 +0530

    Update 1396-design-underground-system.js
    Updated the code by using Classes in JS.

--- javascript/1396-design-underground-system.js
+++ javascript/1396-design-underground-system.js
@@ -1,60 +1,54 @@
-var UndergroundSystem = function() {
+// https://leetcode.com/problems/design-underground-system/
+class UndergroundSystem {
+  constructor() {
     this.stationSystem = {};
     this.averageTime = {};
-};
+  }
 
-/** 
- * https://leetcode.com/problems/design-underground-system
- * Time O(1) | Space O(1)
- * @param {number} id 
- * @param {string} stationName 
- * @param {number} t
- * @return {void}
- */
-UndergroundSystem.prototype.checkIn = function(id, stationName, t) {
+  /**
+   * Time O(1) | Space O(1)
+   * Records the check-in time and station for a user.
+   * @param {number} id - User ID
+   * @param {string} stationName - Check-in station name
+   * @param {number} t - Check-in time
+   * @return {void}
+   */
+  checkIn(id, stationName, t) {
     this.stationSystem[id] = [stationName, '', t, ''];
-};
+  }
 
-/** Time O(1) | Space O(1)
- * @param {number} id 
- * @param {string} stationName 
- * @param {number} t
- * @return {void}
- */
-UndergroundSystem.prototype.checkOut = function(id, stationName, t) {
+  /**
+   * Time O(1) | Space O(1)
+   * Records the check-out time and station for a user, and calculates the average time.
+   * @param {number} id - User ID
+   * @param {string} stationName - Check-out station name
+   * @param {number} t - Check-out time
+   * @return {void}
+   */
+  checkOut(id, stationName, t) {
     const user = this.stationSystem[id];
-    //1 is the position where we store end station
-    // 3 is the position where we store end time
     user[1] = stationName;
-    user[3] = t; 
-    console.log(user);
+    user[3] = t;
     const stationHash = `${user[0]}-${user[1]}`;
-    // console.log(stationHash, this.stationHash);
-    if(this.averageTime[stationHash]) {
-        this.averageTime[stationHash][0] += 1;
-        this.averageTime[stationHash][1] += user[3] - user[2];
+    if (this.averageTime[stationHash]) {
+      this.averageTime[stationHash][0] += 1;
+      this.averageTime[stationHash][1] += user[3] - user[2];
     } else {
-        this.averageTime[stationHash] = [];
-        this.averageTime[stationHash][0] = 1;
-        this.averageTime[stationHash][1] = user[3] - user[2];
+      this.averageTime[stationHash] = [];
+      this.averageTime[stationHash][0] = 1;
+      this.averageTime[stationHash][1] = user[3] - user[2];
     }
-};
+  }
 
-/** Time O(1) | Space O(1)
- * @param {string} startStation 
- * @param {string} endStation
- * @return {number}
- */
-UndergroundSystem.prototype.getAverageTime = function(startStation, endStation) {
-    // console.log(this.averageTime);
+  /**
+   * Time O(1) | Space O(1)
+   * Returns the average time taken to travel between two stations.
+   * @param {string} startStation - Start station name
+   * @param {string} endStation - End station name
+   * @return {number} - Average time in hours
+   */
+  getAverageTime(startStation, endStation) {
     const [rounds, totalHours] = this.averageTime[`${startStation}-${endStation}`];
     return totalHours / rounds;
-};
-
-/** 
- * Your UndergroundSystem object will be instantiated and called as such:
- * var obj = new UndergroundSystem()
- * obj.checkIn(id,stationName,t)
- * obj.checkOut(id,stationName,t)
- * var param_3 = obj.getAverageTime(startStation,endStation)
- */
+  }
+}
