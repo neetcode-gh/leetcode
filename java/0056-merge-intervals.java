98f25025abc9cd7872824d6fa8dc921445116e37
Nytrix-09 <75318042+Nytrix-09@users.noreply.github.com>
2023-06-18 18:52:29 -0400

    Update 0056-merge-intervals.java

--- java/0056-merge-intervals.java
+++ java/0056-merge-intervals.java
@@ -5,8 +5,7 @@ class Solution {
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         ans.add(intervals[0]);
         for (int i = 1; i < intervals.length; i++) {
-            int prevStart = ans.get(ans.size() - 1)[0];
-            //comparing the values of prevEnd and curEnd
+            //comparing the values of prevEnd and curStart
             int curStart = intervals[i][0];
             if (curStart <= ans.get(ans.size() - 1)[1]) {
                 //do the merging
