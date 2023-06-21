0ae5426991164b6e9f655515122dc8fc1edcedcb
Aadil <77232799+aadil42@users.noreply.github.com>
2023-06-21 20:17:50 +0530

    Create 0071-simplify-path.js
    Solved simplify-path in JS.

---
+++ javascript/0071-simplify-path.js
@@ -0,0 +1,33 @@
+/**
+ * Stack
+ * https://leetcode.com/problems/simplify-path/
+ * 
+ * Time O(n) | Space O(n)
+ * @param {string} path
+ * @return {string}
+ */
+var simplifyPath = function(path) {
+    let currunt = '';
+    let myStack = [];
+    path = '/' + path + '/';
+    for(let i = 0; i < path.length; i++) {
+        
+        console.log(myStack);
+        if(path[i] === '/') {
+            if(currunt == '..') {
+                if(myStack.length) {
+                    myStack.pop();
+                }
+            } else if(currunt !== '' && currunt !== '.') {
+                myStack.push(currunt);
+            }
+            currunt = '';
+        } else {
+            currunt += path[i];
+        }
+    }
+
+    myStack = myStack.join('/');
+    myStack = '/' + myStack;
+    return myStack;
+};
