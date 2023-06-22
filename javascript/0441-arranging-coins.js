3e071f7a64a08f223f8b80c73995da2494211a37
Aadil <77232799+aadil42@users.noreply.github.com>
2023-06-22 18:55:49 +0530

    Create 0441-arranging-coins.js
    Solved 0441-arranging-coins.js in JS.

---
+++ javascript/0441-arranging-coins.js
@@ -0,0 +1,62 @@
+/**
+ * https://leetcode.com/problems/arranging-coins/
+ * Linear time
+ * Time O(n) | Space O(1)
+ * @param {number} n
+ * @return {number}
+ */
+var arrangeCoins = function(n) {
+    
+    let steps = 1;
+    let canBuild = 0;
+
+    while(n >= steps) {
+        n = n - steps;
+        canBuild++;
+        steps++;
+    }
+
+    return canBuild || 1;
+};
+
+/**
+ * Binary Search
+ * 
+ * Time O(n*log(n)) | Space O 
+ * @param {number} n
+ * @return {number}
+ */
+var arrangeCoins = function(n) {
+    
+    let left = 1;
+    let right = n;
+    let result = 0;
+  
+    while(left <= right) {
+  
+        const mid = Math.floor((right+left)/2);
+        const total = (1 + mid) * (mid/2);
+        if(n < total) {
+            right = mid -1;
+        } else {
+            left = mid+1;
+            result = Math.max(result, mid);
+        }
+    }
+  
+    return result;
+  };
+  
+/** 
+ * Math 
+ * Time O(1) | Space O(1)
+ * @param {number} n
+ * @return {number}
+ */
+var arrangeCoins = function(n) {
+    
+    let result1 = Math.floor((-1 + Math.sqrt(1+(8*n)))/2);
+    let result2 = Math.floor((-1 - Math.sqrt(1+(8*n)))/2);
+   
+    return Math.max(result1, result2);
+};
