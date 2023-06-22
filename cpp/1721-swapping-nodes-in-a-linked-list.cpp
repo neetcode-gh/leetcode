2d15d21165dcfe758274dd5387ecc9b52199c8f8
Sunil Mourya <sunilmourya.cse.nitp@gmail.com>
2023-06-22 22:53:27 +0530

    Create 1721-swapping-nodes-in-a-linked-list.cpp

---
+++ cpp/1721-swapping-nodes-in-a-linked-list.cpp
@@ -0,0 +1,23 @@
+#include "ListNode.h"
+#include "stdc++.h"
+
+using namespace std;
+
+class Solution {
+   public:
+    ListNode* swapNodes(ListNode* head, int k) {
+        ListNode *fast{head}, *slow{head}, *kthFront = NULL;
+        while (--k) {
+            fast = fast->next;
+        }
+
+        kthFront = fast;
+
+        while (fast->next) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        swap(slow->val, kthFront->val);
+        return head;
+    }
+};
