5d41929b907a312544a0d00ee90382d75c98dd4b
Colston Bod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-06-24 22:04:05 +0800

    Update 0139-word-break.java
    Made the code more concise and removed unnecessary for loop.

--- java/0139-word-break.java
+++ java/0139-word-break.java
@@ -1,28 +1,20 @@
 class Solution {
-
     public boolean wordBreak(String s, List<String> wordDict) {
         boolean[] dp = new boolean[s.length() + 1];
-
-
-        for (int i = 0; i < dp.length; i++) {
-            dp[i] = false;
-        }
-
         dp[s.length()] = true;
 
         for (int i = s.length() - 1; i >= 0; i--) {
-            for (String word : wordDict) {
-                if (
-                    ((i + word.length()) <= s.length()) &&
-                    (s.substring(i, i + word.length()).startsWith(word))
-                ) {
-                    dp[i] = dp[i + word.length()];
-                }
-                if (dp[i]) {
-                    break;
+            for (String w : wordDict) {
+                if (s.substring(i).startsWith(w)) {
+                    dp[i] = dp[i + w.length()];  
+
+                    if (dp[i]) {
+                        break;
+                    }
                 }
             }
         }
+
         return dp[0];
     }
 }
