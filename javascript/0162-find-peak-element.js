c2156021b68b65835e975784a473afafa5c908f5
TheHong <chcheung.office@gmail.com>
2023-06-26 20:43:03 -0400

    Create 0162-find-peak-element.js

---
+++ javascript/0162-find-peak-element.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+    let [l, r] = [0, nums.length - 1];
+    let mid = null;
+    while (l <= r){
+        mid = (l + r) >> 1;
+        if (mid < nums.length - 1 && nums[mid] < nums[mid+1]){
+            l = mid + 1;
+        }
+        else if (mid > 0 && nums[mid] < nums[mid-1]) {
+            r = mid - 1;
+        } 
+        else {
+            break;
+        }
+    }
+    return mid;
+};
