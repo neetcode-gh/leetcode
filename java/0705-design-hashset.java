871bcba0b2dbfab38145b5288db605d11892d288
Alisher Togayev <38793933+Alisherka7@users.noreply.github.com>
2023-06-28 02:33:03 +0900

    Update 0705-design-hashset.java

--- java/0705-design-hashset.java
+++ java/0705-design-hashset.java
@@ -1,19 +1,53 @@
-class MyHashSet {
-    boolean[] set;
+class ListNode{
+    int key;
+    ListNode next;
+    ListNode(int key){
+        this.key = key;
+        next = null;
+    }
+}
 
+class MyHashSet {
+    ListNode[] set;
     public MyHashSet() {
-        set = new boolean[1000001];
+        int n = (int)Math.pow(10, 4);
+        set = new ListNode[n];
+
+        for (int i = 0; i < n; i++) {
+            set[i] = new ListNode(0);
+        }
     }
 
     public void add(int key) {
-        set[key] = true;
+        ListNode cur = set[key % set.length];
+        while(cur.next != null){
+            if(cur.next.key == key){
+                return;
+            }
+            cur = cur.next;
+        }
+        cur.next = new ListNode(key);
     }
 
     public void remove(int key) {
-        set[key] = false;
+        ListNode cur = set[key % set.length];
+        while(cur.next != null){
+            if(cur.next.key == key){
+                cur.next = cur.next.next;
+                return;
+            }
+            cur = cur.next;
+        }
     }
 
     public boolean contains(int key) {
-        return set[key];
+        ListNode cur = set[key % set.length];
+        while(cur.next != null){
+            if(cur.next.key == key){
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
     }
 }
