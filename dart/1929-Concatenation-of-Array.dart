8ee6e4e9ad2f0e36e6625efe0333ceb1f4a88b4b
Satyam Mishra <84375929+codewithsatyam@users.noreply.github.com>
2023-06-28 10:43:06 +0530

    Create 1929-Concatenation-of-Array.dart

---
+++ dart/1929-Concatenation-of-Array.dart
@@ -0,0 +1,13 @@
+//Time Complexity: O(n)
+//Space Complexity: O(n)
+
+class Solution {
+ List<int> getConcatenation(List<int> nums) {
+  List<int> ans = List<int>.filled(2 * nums.length, 0);
+  for (int i = 0; i < nums.length; i++) {
+    ans[i] = nums[i];
+    ans[i + nums.length] = nums[i];
+  }
+  return ans;
+}
+}
