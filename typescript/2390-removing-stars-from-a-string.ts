18bf3f4553abdd27af4af3929e6936e7fcd5caf1
William Leung <leungwk25@gmail.com>
2023-06-30 13:55:22 -0700

    Create 2390-removing-stars-from-a-string.ts

---
+++ typescript/2390-removing-stars-from-a-string.ts
@@ -0,0 +1,14 @@
+function removeStars(s: string): string {
+    if(!s.length) return '';
+
+    const result = [];
+
+    for(let char of s){
+        if(char == '*') result.pop()
+        else result.push(char)
+    }
+    return result.join('')
+};
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
