9e99e3e4224e871ed209dbef17533ad29fa2f5a7
Aadil <77232799+aadil42@users.noreply.github.com>
2023-07-01 18:32:12 +0530

    Update 0705-design-hashset.js
    made the code class based.

--- javascript/0705-design-hashset.js
+++ javascript/0705-design-hashset.js
@@ -1,35 +1,36 @@
+// https://leetcode.com/problems/design-hashset
+class MyHashSet {
+    constructor() {
+        this.set = [];
+    }
 
-//https://leetcode.com/problems/design-hashset
-var MyHashSet = function() {
-    this.set = [];
-};
+    /** 
+     * Time O(1) | Space O(1)
+     * @param {number} key
+     * @return {void}
+     */
+    add(key) {
+        this.set[key] = key;
+    }
 
-/** 
- * Time O(1) | Space O(1)
- * @param {number} key
- * @return {void}
- */
-MyHashSet.prototype.add = function(key) {
-    this.set[key] = key;
-};
+    /** 
+     * Time O(1) | Space O(1)
+     * @param {number} key
+     * @return {void}
+     */
+    remove(key) {
+        this.set[key] = undefined;
+    }
 
-/** 
- * Time O(1) | Space O(1)
- * @param {number} key
- * @return {void}
- */
-MyHashSet.prototype.remove = function(key) {
-    this.set[key] = undefined;
-};
-
-/** 
- * Time O(1) | Space O(1)
- * @param {number} key
- * @return {boolean}
- */
-MyHashSet.prototype.contains = function(key) {
-    return this.set[key] !== undefined;
-};
+    /** 
+     * Time O(1) | Space O(1)
+     * @param {number} key
+     * @return {boolean}
+     */
+    contains(key) {
+        return this.set[key] !== undefined;
+    }
+}
 
 /** 
  * Your MyHashSet object will be instantiated and called as such:
