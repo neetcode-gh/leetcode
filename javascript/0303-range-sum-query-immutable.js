1b4919ff23164f5e9d0e553e52414cd0a1ae4496
Aadil <77232799+aadil42@users.noreply.github.com>
2023-07-01 19:11:51 +0530

    Update 0303-range-sum-query-immutable.js
    Converted to class based.

--- javascript/0303-range-sum-query-immutable.js
+++ javascript/0303-range-sum-query-immutable.js
@@ -1,25 +1,26 @@
 /**
+ * https://leetcode.com/problems/range-sum-query-immutable/
  * @param {number[]} nums
  */
-var NumArray = function(nums) {
-    this.arr = nums;
-};
-
-/** 
- * https://leetcode.com/problems/range-sum-query-immutable/description/
- * Time O(n) | Space O(1)
- * @param {number} left 
- * @param {number} right
- * @return {number}
- */
-NumArray.prototype.sumRange = function(left, right) {
-    
-    let total = 0;
-    for(let i = left; i < right + 1; i++) {
-        total += this.arr[i];
+class NumArray {
+    constructor(nums) {
+        this.arr = nums;
     }
-    return total
-};
+
+    /** 
+     * Time O(n) | Space O(1)
+     * @param {number} left 
+     * @param {number} right
+     * @return {number}
+     */
+    sumRange(left, right) {
+        let total = 0;
+        for (let i = left; i < right + 1; i++) {
+            total += this.arr[i];
+        }
+        return total;
+    }
+}
 
 /** 
  * Your NumArray object will be instantiated and called as such:
