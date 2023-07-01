6919818c11b8e564758e60239f8095ec1c739874
Aadil <77232799+aadil42@users.noreply.github.com>
2023-07-01 19:23:43 +0530

    Update 1603-design-parking-system.js
    Converted to Class based.

--- javascript/1603-design-parking-system.js
+++ javascript/1603-design-parking-system.js
@@ -1,34 +1,38 @@
 /**
+ * https://leetcode.com/problems/design-parking-system/
+ * @class ParkingSystem
  * @param {number} big
  * @param {number} medium
  * @param {number} small
  */
-var ParkingSystem = function(big, medium, small) {
+class ParkingSystem {
+  constructor(big, medium, small) {
     this.bigRemaining = big;
     this.mediumRemaining = medium;
     this.smallRemaining = small;
-};
+  }
 
-/** 
- * Time O(1) | Space O(1)
- * @param {number} carType
- * @return {boolean}
- */
-ParkingSystem.prototype.addCar = function(carType) {
-    if(carType === 1 && this.bigRemaining > 0) {
-        this.bigRemaining -= 1;
-        return true;
+  /** 
+   * Time O(1) | Space O(1)
+   * @param {number} carType
+   * @return {boolean}
+   */
+  addCar(carType) {
+    if (carType === 1 && this.bigRemaining > 0) {
+      this.bigRemaining -= 1;
+      return true;
     }
-    if(carType === 2 && this.mediumRemaining > 0) {
-        this.mediumRemaining -= 1;
-        return true;
+    if (carType === 2 && this.mediumRemaining > 0) {
+      this.mediumRemaining -= 1;
+      return true;
     }
-    if(carType === 3 && this.smallRemaining > 0) {
-        this.smallRemaining -= 1;
-        return true;
+    if (carType === 3 && this.smallRemaining > 0) {
+      this.smallRemaining -= 1;
+      return true;
     }
     return false;
-};
+  }
+}
 
 /** 
  * Your ParkingSystem object will be instantiated and called as such:
