1676dc62d0ad6fa7f01266f7236861596dfb2140
Aadil <77232799+aadil42@users.noreply.github.com>
2023-07-02 20:06:56 +0530

    Update 0523-continuous-subarray-sum.js
    Trimmed the code.

--- javascript/0523-continuous-subarray-sum.js
+++ javascript/0523-continuous-subarray-sum.js
@@ -9,16 +9,16 @@
 var checkSubarraySum = function(arr, k) {
     let sum = 0;
     const remainderMap = new Map([ [0, -1] ]);
-    
+    // remainderMap.set(0,-1);
+  
     for(let i = 0; i < arr.length;  i++) {
         sum += arr[i];
-        if(remainderMap.has(sum%k)) {
-            if(i - remainderMap.get(sum%k) > 1) {
-                return true;
-            } 
-        } else {
-                remainderMap.set(sum%k,i);
-          }
+        if(remainderMap.has(sum%k) && i - remainderMap.get(sum%k) > 1) {
+            return true;
+        } 
+        if(!remainderMap.has(sum%k)) {
+            remainderMap.set(sum%k,i);
+        } 
     }
    
     return false;
