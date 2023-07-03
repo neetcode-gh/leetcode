aee0ac61bd7833535710f10d1a1dedcbc38d4d5c
Aadil <77232799+aadil42@users.noreply.github.com>
2023-07-03 17:53:51 +0530

    Update 0028-find-the-index-of-the-first-occurrence-in-a-string.js
    Avoiding else cases with continue where possible.

--- javascript/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ javascript/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -7,47 +7,49 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    
-    const pattern = Array(needle.length).fill(0);
+  const pattern = Array(needle.length).fill(0);
 
-    let i = 1;
-    let j = 0;
-  
-    while(i < needle.length) {
-        if(needle[i] === needle[j]) {
-            pattern[i] = j + 1;
-            i++;
-            j++;
-        } else if(needle[i] !== needle[j]) {
-            if(j === 0) {
-                pattern[i] = 0;
-                i++;
-            } else if(j !== 0 && needle[j] === needle[i]) {
-                pattern[i] = 1 + pattern[j - 1];
-                i++;
-            } else {
-                j = pattern[j - 1];
-            }
-        }
+  let i = 1;
+  let j = 0;
+
+  while (i < needle.length) {
+    if (needle[i] === needle[j]) {
+      pattern[i] = j + 1;
+      i++;
+      j++;
+      continue;
     }
-    
-j = 0;
-i = 0;
-  
-while(i < haystack.length) {
-
-    if(haystack[i] === needle[j]) {
+    if (needle[i] !== needle[j]) {
+      if (j === 0) {
+        pattern[i] = 0;
         i++;
-        j++;
-    } else if(haystack[i] !== needle[j]) {
-        if(j === 0) {
-            i++;
-        } else {
-        j = pattern[j - 1];            
-        }
+        continue;
+      }
+      if (j !== 0 && needle[j] === needle[i]) {
+        pattern[i] = 1 + pattern[j - 1];
+        i++;
+        continue;
+      }
+      j = pattern[j - 1];
     }
-    if(j === needle.length) return i - needle.length;
-}
+  }
 
-return -1;
+  j = 0;
+  i = 0;
+
+  while (i < haystack.length) {
+    if (haystack[i] === needle[j]) {
+      i++;
+      j++;
+    } else if (haystack[i] !== needle[j]) {
+      if (j === 0) {
+        i++;
+      } else {
+        j = pattern[j - 1];
+      }
+    }
+    if (j === needle.length) return i - needle.length;
+  }
+
+  return -1;
 }
