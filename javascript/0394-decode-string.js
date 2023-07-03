c9c15d57962221ca7b439f659c02767687c47460
Aadil <77232799+aadil42@users.noreply.github.com>
2023-07-03 20:04:53 +0530

    Update 0394-decode-string.js
    Getting rid of the else

--- javascript/0394-decode-string.js
+++ javascript/0394-decode-string.js
@@ -7,26 +7,28 @@
  * @return {string}
  */
 var decodeString = function(s) {
-  
-const myStack = [];
-let result = '';
-for(let i = 0; i < s.length; i++) {
+  const myStack = [];
+  let result = '';
+
+  for(let i = 0; i < s.length; i++) {
     if(s[i] !== ']') {
-        myStack.push(s[i]);
-    } else {
-        let subStr = '';
-        while(myStack[myStack.length - 1] !== '[') {
-            subStr = myStack.pop() + subStr;
-        }
-        myStack.pop();
-        let k = '';
-        while(!isNaN(myStack[myStack.length - 1])) {
-            k = myStack.pop() + k;
-        }
-       myStack.push(subStr.repeat(+k));
+      myStack.push(s[i]);
+      continue;
+    } 
+
+    let subStr = '';
+    while(myStack[myStack.length - 1] !== '[') {
+      subStr = myStack.pop() + subStr;
     }
-}  
-return myStack.join('');
+    myStack.pop();
+
+    let k = '';
+    while(!isNaN(myStack[myStack.length - 1])) {
+      k = myStack.pop() + k;
+    }
+
+    myStack.push(subStr.repeat(+k));
+  }  
+
+  return myStack.join('');
 };
-const myStr = "2[abc]3[cd]ef"; 
-console.log(decodeString(myStr));
