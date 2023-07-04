6bafb1f104186b524e6d92eadf6b6f3def6c0e43
Aadil <77232799+aadil42@users.noreply.github.com>
2023-07-04 20:59:44 +0530

    Update 1898-maximum-number-of-removable-characters.js
    Avoiding else case and defining function outside the main solution function.

--- javascript/1898-maximum-number-of-removable-characters.js
+++ javascript/1898-maximum-number-of-removable-characters.js
@@ -9,37 +9,39 @@
  * @return {number}
  */
 var maximumRemovals1 = function(s, p, removable) {
+  let k = 0;
+  // removable.reverse();
+  s = s.split('');
+  p = p.split('');
 
-    let k = 0;
-    // removable.reverse();
-    s = s.split('');
-    p = p.split('');
-    for(let i = 0; i < removable.length; i++) {
-        s[removable[i]] = -1;
-        if(isSubSet()) {
-            k++;
-        } else {
-            return k;
-        }
+  for (let i = 0; i < removable.length; i++) {
+    s[removable[i]] = -1;
+    if (isSubSet1(s, p)) {
+      k++;
+    } else {
+      return k;
     }
-
-  function isSubSet() {
-      let i = 0;
-      let j = 0;
-
-      while(i < s.length && j < p.length) {
-          if(s[i] === p[j]) {
-              i++;
-              j++;
-          } else {
-              i++;
-          }
-      }
-      return j === p.length
   }
 
-  return k;  
+  return k;
 };
+
+// helper function.
+function isSubSet1(s, p) {
+  let i = 0;
+  let j = 0;
+
+  while (i < s.length && j < p.length) {
+    if (s[i] === p[j]) {
+      i++;
+      j++;
+    } else {
+      i++;
+    }
+  }
+
+  return j === p.length;
+}
 
 
 /**
@@ -53,37 +55,42 @@ var maximumRemovals1 = function(s, p, removable) {
  * @return {number}
  */
 var maximumRemovals = function(s, p, removable) {
+  s = s.split('');
+  p = p.split('');
+  let left = 0;
+  let right = removable.length - 1;
+  let k = 0;
 
-    s = s.split('');
-    p = p.split('');
-    let left = 0;
-    let right = removable.length - 1;
-    let k = 0;
-    while(left <= right) {
-        const mid = Math.floor((left+right)/2);
-        const hash = new Set(removable.slice(0, mid + 1));
-        if(isSubSet(hash))  {
-            k = Math.max(k, mid + 1);
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const hash = new Set(removable.slice(0, mid + 1));
+
+    if (isSubSet(hash, s, p)) {
+      k = Math.max(k, mid + 1);
+      left = mid + 1;
+      continue;
     }
 
-  function isSubSet(hash) {
-      let i = 0;
-      let j = 0;
-
-      while(i < s.length && j < p.length) {
-          if(s[i] === p[j] && !hash.has(i)) {
-              i++;
-              j++;
-          } else {
-              i++;
-          }
-      }
-      return j === p.length
+    right = mid - 1;
   }
 
-  return k;  
+  return k;
 };
+
+// helper function.
+function isSubSet(hash, s, p) {
+  let i = 0;
+  let j = 0;
+
+  while (i < s.length && j < p.length) {
+    if (s[i] === p[j] && !hash.has(i)) {
+      i++;
+      j++;
+      continue;
+    }
+
+    i++;
+  }
+
+  return j === p.length;
+}
