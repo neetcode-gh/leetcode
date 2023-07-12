d4dee29e618ef62b62055f2f7815cd38d971511b
Shubham Patrick <ahthneeuhl@gmail.com>
2023-07-12 11:59:36 +0530

    Update: 1768 Merge Strings Alternately - TypeScript & JavaScript

--- javascript/1768-merge-strings-alternately.js
+++ javascript/1768-merge-strings-alternately.js
@@ -1,10 +1,13 @@
+// Time complexity: O(n)
+// Space complexity: O(n)
+
 var mergeAlternately = function (word1, word2) {
-    let res = '';
+    const buffer = [];
 
     for (let i = 0; i < word1.length || i < word2.length; i++) {
-        if (i < word1.length) res += word1[i];
-        if (i < word2.length) res += word2[i];
+        if (i < word1.length) buffer.push(word1[i]);
+        if (i < word2.length) buffer.push(word2[i]);
     }
 
-    return res;
+    return buffer.join('');
 };
