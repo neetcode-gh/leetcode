3c1231a99a0cdf62b4fcbcd62e4c6767a877b722
Aadil <77232799+aadil42@users.noreply.github.com>
2023-07-20 19:22:01 +0530

    Update 0187-repeated-dna-sequences.js
    fixing conflict. adding alternative code.

--- javascript/0187-repeated-dna-sequences.js
+++ javascript/0187-repeated-dna-sequences.js
@@ -2,7 +2,7 @@
  * https://leetcode.com/problems/repeated-dna-sequences/
  * Hashing
  * s = the number of letters in the sequance. In our case it's 10. so the time complexity would be 10*n which boils down to n.
- * Time O(s*n) | Space O(n)
+ * Time O(n) | Space O(n)
  * @param {string} s
  * @return {string[]}
  */
@@ -28,4 +28,30 @@ var findRepeatedDnaSequences = function(s) {
   function getSubSequance(s,i,len) {
       return s.slice(i, i + len);
   }
-  
+
+// an alternative code with the same approch.
+/**
+ * https://leetcode.com/problems/repeated-dna-sequences/
+ * Hashing
+ * s = the number of letters in the sequance. In our case it's 10. so the time complexity would be 10*n which boils down to n.
+ * Time O(n) | Space O(n)
+ * @param {string} s
+ * @return {string[]}
+ */
+var findRepeatedDnaSequences1 = function (s) {
+const seen = new Set();
+const res = new Set();
+const arr = Array.from(s);
+
+for (let l = 0; l < arr.length - 9; l++) {
+    const sequence = s.slice(l, l + 10);
+
+    if (seen.has(sequence)) {
+        res.add(sequence);
+    } else {
+        seen.add(sequence);
+    }
+}
+
+return Array.from(res);
+};
