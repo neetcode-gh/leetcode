d531c7a82f85ceffc8ed8b928067676256f32a6b
Sujal Goswami <107000806+sujal-goswami@users.noreply.github.com>
2023-07-28 20:46:42 +0530

    Update 0701-insert-into-a-binary-search-tree.java

--- java/0701-insert-into-a-binary-search-tree.java
+++ java/0701-insert-into-a-binary-search-tree.java
@@ -21,14 +21,17 @@ class Solution {
         return root;
     }
 
-    // Using Recursive Solution
-    //  public TreeNode insertIntoBST(TreeNode root, int val) {
-    //     if(root == null) return new TreeNode(val);
-    //     if(root.val <= val){
-    //         root.right = insertIntoBST(root.right, val); 
-    //     }else{
-    //         root.left = insertIntoBST(root.left, val);
-    //     }
-    //     return root;
-    // }
+  /*          Using Recursive Solution
+  -------------------------------------------------------------------
+     public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root == null) return new TreeNode(val);
+        if(root.val <= val){
+            root.right = insertIntoBST(root.right, val); 
+        }else{
+            root.left = insertIntoBST(root.left, val);
+        }
+        return root;
+    }
+  -------------------------------------------------------------------
+  */  
 }
