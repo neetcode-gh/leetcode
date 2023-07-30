a65ea37214535d4a6c745d1c5381e6c59a7a4b5b
NAGARAJU SWARNA <36414037+nagswarnaa@users.noreply.github.com>
2023-07-30 00:13:41 -0500

    Update 0139-word-break.java
    for loop of  initialization is redundant because in java for boolean variables default initialization is false.

--- java/0139-word-break.java
+++ java/0139-word-break.java
@@ -3,11 +3,6 @@ class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         boolean[] dp = new boolean[s.length() + 1];
 
-
-        for (int i = 0; i < dp.length; i++) {
-            dp[i] = false;
-        }
-
         dp[s.length()] = true;
 
         for (int i = s.length() - 1; i >= 0; i--) {
