f1f89420a8fc67c7d9b823e2fa82247be9455f09
Sujal Goswami <107000806+sujal-goswami@users.noreply.github.com>
2023-08-04 15:45:14 +0530

    Update 0139-word-break.java

--- java/0139-word-break.java
+++ java/0139-word-break.java
@@ -1,21 +1,15 @@
 class Solution {
-
     public boolean wordBreak(String s, List<String> wordDict) {
         boolean[] dp = new boolean[s.length() + 1];
-
         dp[s.length()] = true;
 
-        for (int i = s.length() - 1; i >= 0; i--) {
-            for (String word : wordDict) {
-                if (
-                    ((i + word.length()) <= s.length()) &&
-                    (s.substring(i, i + word.length()).startsWith(word))
-                ) {
-                    dp[i] = dp[i + word.length()];
+        for(int i = s.length()-1; i >= 0; i--){
+            for(String w: wordDict){
+                if((i + w.length()) <= s.length() && s.startsWith(w, i)){
+                    dp[i] = dp[i + w.length()];
                 }
-                if (dp[i]) {
+                if(dp[i])
                     break;
-                }
             }
         }
         return dp[0];
