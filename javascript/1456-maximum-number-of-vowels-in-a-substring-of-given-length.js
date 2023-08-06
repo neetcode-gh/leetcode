e708a87fe0d058509e6090fe7af67bc575b625e5
Shubham Patrick <ahthneeuhl@gmail.com>
2023-08-06 17:04:33 +0530

    Update: 1456 - Maximum Number of Vowels in a Substring of Given Length

--- javascript/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
+++ javascript/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -9,17 +9,18 @@
 
 var maxVowels = function (s, k) {
     const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
-    let left = 0;
+    let [left, right] = [0, 0];
     let count = 0;
     let result = 0;
 
-    for (let right = 0; right < s.length; right++) {
+    while (right < s.length) {
         count += vowels.has(s[right]) ? 1 : 0;
 
         if (right - left + 1 > k) {
             count -= vowels.has(s[left]) ? 1 : 0;
             left++;
         }
+        right += 1;
 
         result = Math.max(result, count);
     }
--- typescript/1456-maximum-number-of-vowels-in-a-substring-of-given-length.ts
+++ typescript/1456-maximum-number-of-vowels-in-a-substring-of-given-length.ts
@@ -3,17 +3,18 @@
 
 function maxVowels(s: string, k: number): number {
     const vowels: Set<string> = new Set(['a', 'e', 'i', 'o', 'u']);
-    let left: number = 0;
+    let [left, right]: [number, number] = [0, 0];
     let count: number = 0;
     let result: number = 0;
 
-    for (let right: number = 0; right < s.length; right++) {
+    while (right < s.length) {
         count += vowels.has(s[right]) ? 1 : 0;
 
         if (right - left + 1 > k) {
             count -= vowels.has(s[left]) ? 1 : 0;
             left++;
         }
+        right += 1;
 
         result = Math.max(result, count);
     }
