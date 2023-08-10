7684408616dc465f835f8c685b1603e4622e3cc2
Sarthak Sarangi <77694787+sarthaksarangi@users.noreply.github.com>
2023-08-11 01:41:47 +0530

    Update 0049-group-anagrams.java
    Added a more efficient Java solution which beats 99.55% (5ms) 
    of users with Java compared to 27%  (18ms) in the original code without using any complex methods.

--- java/0049-group-anagrams.java
+++ java/0049-group-anagrams.java
@@ -1,19 +1,19 @@
 class Solution {
-
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> res = new ArrayList<>();
-        if (strs.length == 0) return res;
-        HashMap<String, List<String>> map = new HashMap<>();
-        for (String s : strs) {
-            int[] hash = new int[26];
-            for (char c : s.toCharArray()) {
-                hash[c - 'a']++;
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+            
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
             }
-            String key = new String(Arrays.toString(hash));
-            map.computeIfAbsent(key, k -> new ArrayList<>());
-            map.get(key).add(s);
+            
+            map.get(sortedWord).add(word);
         }
-        res.addAll(map.values());
-        return res;
+        
+        return new ArrayList<>(map.values());
     }
 }
