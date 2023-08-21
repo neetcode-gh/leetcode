b17b980ba457d9bc62931f3b13589f5217e5259b
Ali <aviamirswe@gmail.com>
2023-08-21 15:13:39 +0200

    Update 0139-word-break.kt

--- kotlin/0139-word-break.kt
+++ kotlin/0139-word-break.kt
@@ -1,17 +1,48 @@
+//DP
 class Solution {
     fun wordBreak(s: String, wordDict: List<String>): Boolean {
-        val cache = BooleanArray(s.length+1)
-        cache[s.length] = true // lastPos is true, meaning [array of word length], lastPos] since string "" can be segmented of any list of any words (our dp basecase)
-        for(i in s.length-1 downTo 0){
-            for(word in wordDict){
-                if(word.length+i <= s.length){//enough chars in s for this word to be viable ?
-                    if(word == s.substring(i, i+word.length)){// its the same word char by char?
-                        if(cache[i+word.length] == true) //this cache index is true only if the cache at index [i+word.length is true]
+        val cache = BooleanArray (s.length + 1).apply {
+            this[s.length] = true 
+        }
+        
+        for (i in s.lastIndex downTo 0) {
+            for (word in wordDict) {
+                if (word.length + i <= s.length) {
+                    if (word == s.substring(i, i + word.length)) {
+                        if (cache[i + word.length] == true) 
                             cache[i] = true
                     }
                 }
             }
         }
         return cache[0]
+    }
+}
+
+//Recursive + memoization
+class Solution {
+    fun wordBreak(s: String, wordDict: List<String>): Boolean {
+        val cache = IntArray (s.length + 1) { -1 }
+        
+        fun dfs(i: Int): Int {
+            if (i == s.length) return 1
+            if (cache[i] != -1) return cache[i]
+
+            cache[i] = 0
+
+            for (word in wordDict) {
+                if (word.length + i <= s.length && 
+                    word == s.substring(i, i + word.length)) {
+                    if (dfs(i + word.length) == 1) {
+                        cache[i] = 1
+                        return 1
+                    }
+                }
+            }
+
+            return 0
+        }
+
+        return if (dfs(0) == 1) true else false
     }
 }
