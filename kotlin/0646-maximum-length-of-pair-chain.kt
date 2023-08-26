b6e7f6690d6618042d4266fc1f42b01a752739e4
Ali <aviamirswe@gmail.com>
2023-08-26 11:54:53 +0200

    Update 0646-maximum-length-of-pair-chain.kt

--- kotlin/0646-maximum-length-of-pair-chain.kt
+++ kotlin/0646-maximum-length-of-pair-chain.kt
@@ -34,3 +34,31 @@ class Solution {
         return dp[n - 1]
     }
 }
+
+//recursion + memoization O(n^2)
+class Solution {
+    fun findLongestChain(_pairs: Array<IntArray>): Int {
+        val pairs = _pairs.sortedWith(compareBy({ it[0] }, { it[1] }))
+        val dp = IntArray (pairs.size) { -1 }
+        
+        fun dfs(i: Int): Int {
+            if (i == pairs.size) return 0
+            if (dp[i] != -1) return dp[i]
+
+            var res = 1
+            for (j in 0 until i) {
+                if (pairs[i][0] > pairs[j][1])
+                    res = maxOf(res, dfs(j) + 1)
+            }
+
+            dp[i] = res
+            return res
+        }
+        
+        var res = 1
+        for (i in 0 until pairs.size)
+            res = maxOf(res, dfs(i))
+        
+        return res
+    }
+}
