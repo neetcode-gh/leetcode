b8beb67e6b958d64b6f1816bd5b2f254982932b4
Chris Cooper <christopher.p.cooper@gmail.com>
2023-09-03 15:19:38 -0700

    Use recursion to minimize the code and it is surprisingly fast.

--- python/0021-merge-two-sorted-lists.py
+++ python/0021-merge-two-sorted-lists.py
@@ -4,22 +4,11 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
-        dummy = ListNode()
-        tail = dummy
-
-        while list1 and list2:
-            if list1.val < list2.val:
-                tail.next = list1
-                list1 = list1.next
-            else:
-                tail.next = list2
-                list2 = list2.next
-            tail = tail.next
-
-        if list1:
-            tail.next = list1
-        elif list2:
-            tail.next = list2
-
-        return dummy.next
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        lil, big = (list1, list2) if list1.val < list2.val else (list2, list1)
+        lil.next = self.mergeTwoLists(lil.next, big)
+        return lil
