3e8f1a23cb963f57f1cd11f5c35e689f1c35bedd
Chris Cooper <christopher.p.cooper@gmail.com>
2023-09-04 09:04:29 -0700

    Moved the original solution to the top and the new one to the bottom.

--- python/0021-merge-two-sorted-lists.py
+++ python/0021-merge-two-sorted-lists.py
@@ -4,17 +4,6 @@
 #         self.val = val
 #         self.next = next
 
-# Recursive
-class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if not list1:
-            return list2
-        if not list2:
-            return list1
-        lil, big = (list1, list2) if list1.val < list2.val else (list2, list1)
-        lil.next = self.mergeTwoLists(lil.next, big)
-        return lil
-
 # Iterative
 class Solution:
     def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
@@ -32,3 +21,14 @@ class Solution:
         node.next = list1 or list2
 
         return dummy.next
+    
+# Recursive
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        lil, big = (list1, list2) if list1.val < list2.val else (list2, list1)
+        lil.next = self.mergeTwoLists(lil.next, big)
+        return lil
