cb95cde111e4c79532fd2228c26ba02abdead852
Chris Cooper <christopher.p.cooper@gmail.com>
2023-09-04 09:00:32 -0700

    Add Iterative along with Recursive solution

--- python/0021-merge-two-sorted-lists.py
+++ python/0021-merge-two-sorted-lists.py
@@ -3,6 +3,8 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# Recursive
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         if not list1:
@@ -12,3 +14,21 @@ class Solution:
         lil, big = (list1, list2) if list1.val < list2.val else (list2, list1)
         lil.next = self.mergeTwoLists(lil.next, big)
         return lil
+
+# Iterative
+class Solution:
+    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
+        dummy = node = ListNode()
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                node.next = list1
+                list1 = list1.next
+            else:
+                node.next = list2
+                list2 = list2.next
+            node = node.next
+
+        node.next = list1 or list2
+
+        return dummy.next
