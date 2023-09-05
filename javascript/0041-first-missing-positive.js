373d910a958f9f38e21caee9d70c12a6716013e4
aadil42 <77232799+aadil42@users.noreply.github.com>
2023-09-05 14:00:28 +0530

    Update 0041-first-missing-positive.js
    Adding cyclical sort to solve the problem.

--- javascript/0041-first-missing-positive.js
+++ javascript/0041-first-missing-positive.js
@@ -1,46 +1,56 @@
 /**
- * https://leetcode.com/problems/first-missing-positive/
- * Time O(n) | Space O(1)
+ * Cyclic Sort
+ * Time O(N) | Space O(1)
+ * https://leetcode.com/problems/first-missing-positive
  * @param {number[]} nums
  * @return {number}
  */
-var firstMissingPositive = function(nums) {
-    //-1 1 3 4
-    const swap = (a, b) => {
-        const temp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = temp;
-    }
- 
-    // swap positions
-    for (let i = 0; i < nums.length; i++) {
-        const el = nums[i];
-        const chair = el - 1;
-        if (el >= 1 && el <= nums.length + 1 && nums[chair] !== el) {
-            swap(chair, i);
-            i--; // this decrement is important // check this input [3,4,-1,1]
-        }
-    }
+var firstMissingPositive = (nums) => {
+  cyclicSort(nums);
 
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== i + 1) return i + 1;
-    } 
-
-    return nums.length + 1;
+  return search(nums);
 };
 
-/**
- * Time O(n) | Space O(n)
- * @param {number[]} nums
- * @return {number}
- */
-var firstMissingPositive1 = function(nums) {
-    const numberSet = new Set(nums);
-      
-    let i = 1;
-    while (numberSet.has(i)) {
-        i++;
+const cyclicSort = (nums, index = 0) => {
+  while (index < nums.length) {
+    const num = nums[index];
+    const indexKey = (num - 1);
+    const indexNum = nums[indexKey];
+
+    if (canSwap(nums, num, indexNum)) {
+      swap(nums, index, indexKey);
+      continue;
     }
- 
-    return i;
-};
+
+    index += 1;
+  }
+}
+
+const search = (nums, index = 0) => {
+  while (index < nums.length) {
+    const num = nums[index];
+    const indexKey = (index + 1);
+
+    if (!isEqual(num, indexKey)) return indexKey;
+
+    index += 1;
+  }
+
+  return (nums.length + 1);
+}
+
+const canSwap = (nums, num, indexNum) =>
+  isPositive(num) &&
+  isInBound(num, nums) &&
+  !isEqual(num, indexNum);
+
+const swap = (nums, index, indexKey) => 
+  [nums[index], nums[indexKey]] = [nums[indexKey], nums[index]];
+
+const isPositive = (num) => (0 < num);
+
+const isInBound = (num, nums) => (num <= nums.length);
+
+const isEqual = (num, indexNum) => (num === indexNum);
+
+
