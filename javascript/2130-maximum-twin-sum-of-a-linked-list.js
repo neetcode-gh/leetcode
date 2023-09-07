ff3a05d07bebc8dff7f7cf186df229631e091247
aadil42 <77232799+aadil42@users.noreply.github.com>
2023-09-07 13:03:23 +0530

    Update 2130-maximum-twin-sum-of-a-linked-list.js
    Modularizing the code.

--- javascript/2130-maximum-twin-sum-of-a-linked-list.js
+++ javascript/2130-maximum-twin-sum-of-a-linked-list.js
@@ -14,28 +14,30 @@
  */
 var pairSum = function (head) {
   const mid = llLength(head) / 2;
-
-  // get the right starting pointer
-  let count = 0;
-  let rightPointer = head;
-  while (count < mid) {
-    rightPointer = rightPointer.next;
-    count++;
-  }
-
-  // reverse the left portion of the array
-  leftPointer = reverseLL(head, mid);
-
-  let max = 0;
-  while (leftPointer && rightPointer) {
-    max = Math.max(leftPointer.val + rightPointer.val, max);
-    leftPointer = leftPointer.next;
-    rightPointer = rightPointer.next;
-  }
-
-  return max;
+  const rightPointer = getRightPointer(head, mid);
+  const leftPointer = reverseLL(head, mid);
+  
+  return getMax(leftPointer, rightPointer);
 };
 
+var getMax = (leftPointer, rightPointer) => {
+    let max = 0;
+    while (leftPointer && rightPointer) {
+      max = Math.max(leftPointer.val + rightPointer.val, max);
+      leftPointer = leftPointer.next;
+      rightPointer = rightPointer.next;
+    }
+    return max;
+}
+var getRightPointer = (head, mid) => {
+    let count = 0;
+    let rightPointer = head;
+    while (count < mid) {
+    rightPointer = rightPointer.next;
+      count++;
+    }
+    return rightPointer;
+}
 
 var llLength = (head) => {
   let count = 0;
