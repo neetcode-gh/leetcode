dbdc94575c8c706ba98f6188764650f0d75f0da4
Chris Cooper <christopher.p.cooper@gmail.com>
2023-09-10 20:39:40 -0700

    Change variable name from c to count

--- python/0621-task-scheduler.py
+++ python/0621-task-scheduler.py
@@ -26,6 +26,6 @@ class Solution(object):
         counter = collections.Counter(tasks)
         max_count = max(counter.values())
         min_time = (max_count - 1) * (n + 1) + \
-                    sum(map(lambda c: c == max_count, counter.values()))
+                    sum(map(lambda count: count == max_count, counter.values()))
     
         return max(min_time, len(tasks))
