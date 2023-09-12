9e12abecb768b7dac5396efa1d1c605283a5dfb7
Sujal Goswami <107000806+sujal-goswami@users.noreply.github.com>
2023-09-12 19:11:57 +0530

    Update 1647-minimum-deletions-to-make-character-frequencies-unique.java

--- java/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ java/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,24 +1,19 @@
 class Solution {
     public int minDeletions(String s) {
-        int[] chars = new int[26];
-        for (char c : s.toCharArray()) {
-            chars[c - 'a']++;
+        // This Map stores the frequency of characters in String s.
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char c: s.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
-
-        int[] arr = Arrays.stream(chars).boxed()
-                    .sorted(Collections.reverseOrder())
-                    .mapToInt(Integer::intValue)
-                    .toArray();
-        int frequency = arr[0];
+        // This set stores the frequency of Characters that we encounter 
+        HashSet<Integer> set = new HashSet<>();
         int res = 0;
-        for (int i : arr) {
-            if (i > frequency) {
-                if (frequency > 0)
-                    res += (i - frequency);
-                else
-                    res += i;
+        for (char c: map.keySet()){
+            while (map.get(c) > 0 && set.contains(map.get(c))){
+                map.put(c, map.get(c)-1);
+                res++;
             }
-            frequency = Math.min(frequency - 1, i - 1);
+            set.add(map.get(c));
         }
         return res;
     }
