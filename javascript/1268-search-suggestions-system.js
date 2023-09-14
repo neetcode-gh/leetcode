98ab2bc8187ad8307769af8baa8f4514aaa904b0
aadil42 <77232799+aadil42@users.noreply.github.com>
2023-09-14 19:18:01 +0530

    Update 1268-search-suggestions-system.js
    Added Trie solution in addition to binary-search.

--- javascript/1268-search-suggestions-system.js
+++ javascript/1268-search-suggestions-system.js
@@ -44,3 +44,114 @@ var suggestedProducts = function(products, searchWord) {
 
     return result;
 };
+
+/**
+ * DFS - Trie
+ * Time O(N * M) | Space O(N)
+ * https://leetcode.com/problems/search-suggestions-system/
+ * @param {string[]} products
+ * @param {string} searchWord
+ * @return {string[][]}
+ */
+var suggestedProducts1 = (products, searchWord) => new Trie()
+    .buildTrie(products)
+    .searchWord(searchWord);
+
+class Node {
+    constructor () {
+        this.children = new Map();
+        this.isWord = false;
+    }
+};
+
+class Trie {
+    constructor () {
+        this.root = new Node();
+    }
+
+    buildTrie (products) {
+        for (const word of products) {
+            this.insert(word);
+        }
+
+        return this;
+    }
+    
+    insert (word, { root: node } = this) {        
+        for (const char of word.split('')) {
+            const child = (node.children.get(char) ?? new Node());
+            
+            node.children.set(char, child);
+
+            node = child;
+        }
+
+        node.isWord = true;
+    }
+    
+    searchWord (searchWord, buffer = [], suggestions = []) {
+        for (const char of searchWord.split('')) {
+            const prefix = this.getPrefix(buffer, char);
+            const words = this.getSuggestions(prefix);
+
+            suggestions.push(words);
+        }
+        
+        return suggestions;
+    }
+
+    getPrefix (buffer, char) {
+        buffer.push(char);
+
+        return buffer.join('');
+    }
+    
+    getSuggestions (prefix, words = []) {
+        const node = this.getPrefixNode(prefix);
+
+        const isInvalidPrefix = (node === null);
+        if (isInvalidPrefix) return words
+        
+        return this.search(node, prefix, words);
+    }
+
+    getPrefixNode (prefix, { root: node } = this) {
+        for (const char of prefix.split('')) {
+            const child = (node.children.get(char) ?? null);
+
+            const isLeafNode = (child === null);
+            if (isLeafNode) return null;
+
+            node = child;
+        }
+
+        return node;
+    }
+
+    search (node, word, words) {
+        const isBaseCase = (words.length === 3);
+        if (isBaseCase) return words;
+
+        if (node.isWord) words.push(word);
+
+        return this.dfs(node, word, words);
+    }
+
+    dfs (node, word, words) {
+        for (const char of this.getChars()) {
+            const child = (node.children.get(char) ?? null);
+
+            const isLeafNode = (child === null);
+            if (isLeafNode) continue;
+        
+            this.search(child, (word + char), words);
+        }
+
+        return words;
+    }
+
+    getChars () {
+        return new Array(26).fill()
+            .map((_, index) => String.fromCharCode((index + 97)));
+    }
+};
