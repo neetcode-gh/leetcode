c289a7b87d7cccb5f937eaa39bc236e78590b84c
Ali <aviamirswe@gmail.com>
2023-10-01 10:39:27 +0200

    Create 0557-reverse-words-in-a-string-iii.kt

---
+++ kotlin/0557-reverse-words-in-a-string-iii.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun reverseWords(s: String): String {
+        val words = s.split(" ")
+        val res = LinkedList<String>()
+
+        for (word in words) {
+            if (res.isNotEmpty())
+                res.add(" ")
+            val sb = StringBuilder()
+            for (i in word.lastIndex downTo 0) {
+                sb.append(word[i])
+            }
+            res.add(sb.toString())
+        }
+
+        return res.joinToString("")
+    }
+}
