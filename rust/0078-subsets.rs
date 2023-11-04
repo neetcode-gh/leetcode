b13208d52e8fee4baa50d478173d421d347ad58a
Emanuele Micheletti <micheletti.emanuele@hotmail.com>
2023-11-04 12:23:24 +0100

    Update 0078-subsets.rs
    https://leetcode.com/problems/subsets/submissions/1091222208/

--- rust/0078-subsets.rs
+++ rust/0078-subsets.rs
@@ -1,3 +1,24 @@
+// Backtracking solution
+impl Solution {
+    pub fn subsets(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
+        let mut res = vec![];
+        let mut curr = vec![];
+
+        fn backtrack(nums: &Vec<i32>, current: &mut Vec<i32>, result: &mut Vec<Vec<i32>>, index: usize){
+           result.push(current.clone()); 
+           for i in index..nums.len(){
+               current.push(nums[i]);
+               backtrack(nums, current, result, i+1);
+               current.pop();
+           }
+        }
+
+        backtrack(&nums, &mut curr, &mut res, 0);
+        res
+    }
+}
+
+// Alternative solution without backtracking
 impl Solution {
     pub fn subsets(nums: Vec<i32>) -> Vec<Vec<i32>> {
        let n = nums.len();
