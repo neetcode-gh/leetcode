89c632fd44b8626e2274ead83ca7c8a00da604c3
Emanuele Micheletti <micheletti.emanuele@hotmail.com>
2023-11-04 11:56:31 +0100

    Update 0078-subsets.rs

--- rust/0078-subsets.rs
+++ rust/0078-subsets.rs
@@ -1,14 +1,11 @@
-use std::iter::FromIterator;
-
 impl Solution {
     pub fn subsets(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        let n = nums.len();
-
-        Vec::from_iter((0..1 << n).map(|bitmask| {
-            Vec::from_iter((0..n).filter_map(|i| match (bitmask >> i) & 1 != 0 {
+       let n = nums.len();
+       (0..1<<n).map(|bitmask| {
+            (0..n).filter_map(|i| match (bitmask >> i)&1 != 0 {
                 true => Some(nums[i]),
                 false => None
-            }))
-        }))
+                }).collect()
+       }).collect()
     }
 }
