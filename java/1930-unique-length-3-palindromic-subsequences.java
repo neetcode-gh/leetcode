e3375052797477548cec08e596ff5838bf573461
Sujal Goswami <107000806+sujal-goswami@users.noreply.github.com>
2023-11-16 15:59:21 +0530

    Update 1930-unique-length-3-palindromic-subsequences.java

--- java/1930-unique-length-3-palindromic-subsequences.java
+++ java/1930-unique-length-3-palindromic-subsequences.java
@@ -30,3 +30,24 @@ class Solution {
         return res.size();
     }
 }
+/*                 Alternative solution
+------------------------------------------------------------------------*/
+class Solution {
+    public int countPalindromicSubsequence(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c: s.toCharArray())
+            map.put(c, map.getOrDefault(c, 0) + 1);
+
+        int res = 0;
+        for(char c : map.keySet()){
+            if (map.get(c) >= 2){
+                int fo = s.indexOf(c), lo = s.lastIndexOf(c);
+                Set<Character> between = new HashSet<>();
+                for(int i = fo+1; i < lo; i++)
+                    between.add(s.charAt(i));
+                res += between.size();    
+            }
+        }
+        return res; 
+    }
+}
