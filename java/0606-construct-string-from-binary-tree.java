027cb6cbde6efe0af5edd9839d6594088c3173a9
Sujal Goswami <107000806+sujal-goswami@users.noreply.github.com>
2023-12-08 17:22:13 +0530

    Update 0606-construct-string-from-binary-tree.java

--- java/0606-construct-string-from-binary-tree.java
+++ java/0606-construct-string-from-binary-tree.java
@@ -9,6 +9,25 @@ class Solution {
 
         return output;
     }
+}
+/*                Alternative Solution
+-----------------------------------------------------------------*/
+class Solution {
+    public String tree2str(TreeNode root) {
+        StringBuilder res = new StringBuilder();
+        dfs(root, res);
+        return res.toString().substring(1, res.length()-1);
+    }
+    private void dfs(TreeNode root, StringBuilder res){
+        if(root == null)
+            return;
 
-    
+        res.append("(");
+        res.append(root.val);
+        if(root.left == null && root.right != null)
+            res.append("()");
+        dfs(root.left, res);
+        dfs(root.right, res);
+        res.append(")");
+    }
 }
