3c7a43fb856ef9cad86009867737f114b7a56152
Bliznichenka <l.bliznichenko@mail.ru>
2023-12-09 23:37:21 +0100

    Updated variable name

--- java/0904-fruit-into-baskets.java
+++ java/0904-fruit-into-baskets.java
@@ -1,17 +1,17 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        Map<Integer, Integer> fruitTypeToCount = new HashMap<>();
+        Map<Integer, Integer> fruitTypeToItsCount = new HashMap<>();
         int maxTotalFruits = 0;
         int l = 0;
         int r = 0;
 
         while (r < fruits.length) {
-            fruitTypeToCount.put(fruits[r], fruitTypeToCount.getOrDefault(fruits[r], 0) + 1);
+            fruitTypeToItsCount.put(fruits[r], fruitTypeToItsCount.getOrDefault(fruits[r], 0) + 1);
 
-            while (fruitTypeToCount.size() > 2) {
-                fruitTypeToCount.put(fruits[l], fruitTypeToCount.get(fruits[l]) - 1);
-                if (fruitTypeToCount.get(fruits[l]) == 0) {
-                    fruitTypeToCount.remove(fruits[l]);
+            while (fruitTypeToItsCount.size() > 2) {
+                fruitTypeToItsCount.put(fruits[l], fruitTypeToItsCount.get(fruits[l]) - 1);
+                if (fruitTypeToItsCount.get(fruits[l]) == 0) {
+                    fruitTypeToItsCount.remove(fruits[l]);
                 }
                 l++;
             }
