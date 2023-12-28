9163e9a105e4714a5976771bfc820db76541f8a1
Leif Elliott <69766831+JustLeif@users.noreply.github.com>
2023-12-28 14:46:45 -0500

    Fix typo / small syntax improvement

--- rust/0206-reverse-linked-list.rs
+++ rust/0206-reverse-linked-list.rs
@@ -1,11 +1,11 @@
 impl Solution {
-    pub fn reverse_list(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
         let (mut prev, mut curr) = (None, head);
         while let Some(mut node) = curr{
             curr = node.next;
             node.next = prev;
             prev = Some(node);
         } 
-        pre
+        prev
     }
 }
