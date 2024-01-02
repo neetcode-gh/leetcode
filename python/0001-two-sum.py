5bc81881c8998d371506786499e6f34483775ae1
Nirajan Mahara <69881638+NirajanMahara@users.noreply.github.com>
2024-01-01 22:34:29 -0500

    Update 0001-two-sum.py

--- python/0001-two-sum.py
+++ python/0001-two-sum.py
@@ -7,3 +7,5 @@ class Solution:
             if diff in prevMap:
                 return [prevMap[diff], i]
             prevMap[n] = i
+
+# use hash map to instantly check for difference value, map will add index of last occurrence of a num, don’t use same element twice;
