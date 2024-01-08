8785da973d3762989a9b00f34bf45ac2a44131ce
Sudhanshu Raj Singh <rajsinghsudhanshu930@gmail.com>
2024-01-08 14:26:00 -0500

    Update 0200-number-of-islands.py
    For the dfs version, I remove the need for maintaining a visit set. I am modifying the matrix to store this information.

--- python/0200-number-of-islands.py
+++ python/0200-number-of-islands.py
@@ -28,6 +28,24 @@ class Solution:
                     dfs(r, c)
         return islands
 
+#DFS O(1) Space and much less code
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        rows,cols=len(grid),len(grid[0])
+        def dfs(r,c):
+            if not 0<=r<len(grid) or not 0<=c<len(grid[0])or grid[r][c]=='0':
+                return 0
+            grid[r][c]='0'
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+            return 1
+        count=0
+        for r in range(rows):
+            for c in range(cols):
+                count+=dfs(r,c)
+        return count
 
 # BFS Version From Video
 class SolutionBFS:
