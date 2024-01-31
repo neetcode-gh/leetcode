ca2c275a1c5082d3233bd8068707941e6bd5367d
Rakesh Tirumala <rakeshus2002@gmail.com>
2024-01-31 17:09:07 -0500

    Update 0450-delete-node-in-a-bst.java
    The earlier code provided wasn't working for all cases. I have edited the code.

--- java/0450-delete-node-in-a-bst.java
+++ java/0450-delete-node-in-a-bst.java
@@ -1,32 +1,24 @@
-/** 
- * TC : log (n)
- * 
- * */ 
 class Solution {
-    public TreeNode minimumVal(TreeNode root) {
-        TreeNode curr = root;
-        while (curr != null && curr.left != null) {
-            curr = curr.left;
+    public TreeNode findMin(TreeNode node) {
+        TreeNode current = node;
+        while (current.left != null) {
+            current = current.left;
         }
-        return curr;
+        return current;
     }
 
     public TreeNode deleteNode(TreeNode root, int key) {
         if (root == null) return null;
 
-        if (key > root.val) {
-            root.right = deleteNode(root.right, key);
-        } else if (key < root.val) {
-            root.left = deleteNode(root.left, key);
-        } else {
-            if (root.left == null) {
-                return root.right;
-            } else if (root.right == null) {
-                return root.left;
-            } else {
-                TreeNode minVal = minimumVal(root);
-                root.val = minVal.val;
-                root.right = deleteNode(root.right, minVal.val);
+        if (root.val > key) root.left = deleteNode(root.left, key);
+        else if (root.val < key) root.right = deleteNode(root.right, key);
+        else {
+            if (root.left == null) return root.right;
+            else if (root.right == null) return root.left;
+            else {
+                TreeNode successor = findMin(root.right);
+                root.val = successor.val;
+                root.right = deleteNode(root.right, successor.val);
             }
         }
         return root;
