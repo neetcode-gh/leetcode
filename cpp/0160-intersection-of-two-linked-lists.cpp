efd1159134dad816493760bd175a89a51bb94196
Winfred Lu <winfred.lu@gmail.com>
2024-02-01 11:42:38 +0800

    Fix typo.

--- cpp/0160-intersection-of-two-linked-lists.cpp
+++ cpp/0160-intersection-of-two-linked-lists.cpp
@@ -13,8 +13,8 @@ public:
         ListNode *trevA = headA, *trevB = headB;
 
         while (trevA != trevB) {
-            trevA = (trevA != NULL) ? trevA->next : headA;
-            trevB = (trevB != NULL) ? trevB->next : headB;
+            trevA = (trevA != NULL) ? trevA->next : headB;
+            trevB = (trevB != NULL) ? trevB->next : headA;
         }
         return trevA;
     }
