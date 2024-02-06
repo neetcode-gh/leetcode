46ce266330c115975f2aff5d234a0c39a59829de
Bobzero <108892319+Bobzero23@users.noreply.github.com>
2024-02-06 14:55:31 +0300

    Update 0003-longest-substring-without-repeating-characters.java
    increased readability and code documentation

--- java/0003-longest-substring-without-repeating-characters.java
+++ java/0003-longest-substring-without-repeating-characters.java
@@ -5,11 +5,9 @@ class Solution {
             int largestlength = 0;
             for(int right = 0; right < s.length(); right++) {
                 if(substringL.contains(s.charAt(right))) { 
-                    // get the index of the char
-                    int index = substringL.indexOf(s.charAt(right));
-                    substringL.remove(index);
-                    if(index > 0)
-                        substringL.subList(0, index).clear();
+                    int index = substringL.indexOf(s.charAt(right)); // get the index of the element
+                    substringL.remove(index); 
+                    if(index > 0) substringL.subList(0, index).clear(); // in case the repeated element appears in the middle of substring we remove all the elements come bofore it
                 }
                 substringL.add(s.charAt(right));
                 largestlength = Math.max(largestlength, substringL.size());
