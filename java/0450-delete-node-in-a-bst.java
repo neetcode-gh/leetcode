a9e1566f8f331d597cbe31d2c62b529b5d955b73
Mohd Zia <ziamd@student.iul.ac.in>
2024-02-07 12:24:05 +0530

    fixed issue

--- java/0450-delete-node-in-a-bst.java
+++ java/0450-delete-node-in-a-bst.java
@@ -1,14 +1,20 @@
-/** 
- * TC : log (n)
- * 
- * */ 
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
+
 class Solution {
-    public TreeNode minimumVal(TreeNode root) {
+    public int minimumVal(TreeNode root) {
         TreeNode curr = root;
         while (curr != null && curr.left != null) {
             curr = curr.left;
         }
-        return curr;
+        return curr.val;
     }
 
     public TreeNode deleteNode(TreeNode root, int key) {
@@ -24,9 +30,9 @@ class Solution {
             } else if (root.right == null) {
                 return root.left;
             } else {
-                TreeNode minVal = minimumVal(root);
-                root.val = minVal.val;
-                root.right = deleteNode(root.right, minVal.val);
+                int minVal = minimumVal(root.right); // Use minimumVal on the right subtree
+                root.val = minVal;
+                root.right = deleteNode(root.right, minVal);
             }
         }
         return root;
