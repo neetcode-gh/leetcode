34e4d9bcfb795eb3ae1e9bc1fc63d978a5af2da5
Mohd Zia <ziamd@student.iul.ac.in>
2024-02-07 12:13:41 +0530

    optimised the code
    optimised the solution reduced time complexity

--- python/0001-two-sum.py
+++ python/0001-two-sum.py
@@ -1,9 +1,13 @@
+from typing import List
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prevMap = {}  # val -> index
+        value_to_index = {}  # Store value -> index mapping
 
         for i, n in enumerate(nums):
-            diff = target - n
-            if diff in prevMap:
-                return [prevMap[diff], i]
-            prevMap[n] = i
+            complement = target - n
+            if complement in value_to_index:
+                return [value_to_index[complement], i]
+            value_to_index[n] = i
+
+        return []
