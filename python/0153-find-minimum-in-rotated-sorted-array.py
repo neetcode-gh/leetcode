c32c81dffda7154a83e6de9ca90d9b8f52b705a8
Trisha Pan <29530389+trisha@users.noreply.github.com>
2024-02-20 20:52:52 -0500

    Update 0153-find-minimum-in-rotated-sorted-array.py
    fix algebra mistake

--- python/0153-find-minimum-in-rotated-sorted-array.py
+++ python/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,11 +1,11 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        start , end = 0, len(nums) - 1 
+        start, end = 0, len(nums) - 1 
         curr_min = float("inf")
         
         while start  <  end :
-            mid = start + (end - start ) // 2
-            curr_min = min(curr_min,nums[mid])
+            mid = (start + end) // 2
+            curr_min = min(curr_min, nums[mid])
             
             # right has the min 
             if nums[mid] > nums[end]:
@@ -15,4 +15,4 @@ class Solution:
             else:
                 end = mid - 1 
                 
-        return min(curr_min,nums[start])
+        return min(curr_min, nums[start])
