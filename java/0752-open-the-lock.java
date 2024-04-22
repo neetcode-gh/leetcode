392f5042bab5146592d145da1fdb48e5958f0258
Sujal Goswami <107000806+sujal-goswami@users.noreply.github.com>
2024-04-22 22:46:29 +0530

    Update 0752-open-the-lock.java

--- java/0752-open-the-lock.java
+++ java/0752-open-the-lock.java
@@ -1,48 +1,47 @@
-//Simple BFS solution
-
 class Solution {
-
     public int openLock(String[] deadends, String target) {
-        String start = "0000";
-        int ans = 0;
-        Queue<String> q = new LinkedList<>();
-        HashSet<String> visited = new HashSet<>();
-        //Add all the deadends in the visited set so we can ignore them and visited values altogether.
-        for (String s : deadends) visited.add(s);
-        q.offer(start);
-        while (!q.isEmpty()) {
-            int size = q.size();
-            for (int j = 0; j < size; j++) {
-                String str = q.poll();
-                StringBuilder cur = new StringBuilder(str);
-                if (str.equals(target)) return ans;
-                if (!visited.contains(cur.toString())) {
-                    for (int i = 0; i < start.length(); i++) {
-                        //edge case for 0
-                        if (cur.charAt(i) == '0') {
-                            cur.setCharAt(i, '1');
-                            q.offer(cur.toString());
-                            cur.setCharAt(i, '9');
-                            q.offer(cur.toString());
-                        } else if (cur.charAt(i) == '9') { //edge case for 9
-                            cur.setCharAt(i, '0');
-                            q.offer(cur.toString());
-                            cur.setCharAt(i, '8');
-                            q.offer(cur.toString());
-                        } else {
-                            cur.setCharAt(i, ((char) (cur.charAt(i) + 1)));
-                            q.offer(cur.toString());
-                            cur.setCharAt(i, ((char) (cur.charAt(i) - 2)));
-                            q.offer(cur.toString());
-                        }
-                        visited.add(str);
-                        cur.setLength(0);
-                        cur.append(str);
+        Set<String> visited = new HashSet<>();
+        for (String deadend : deadends) {
+            if (deadend.equals("0000")) {
+                return -1;
+            }
+            visited.add(deadend);
+        }
+
+        Queue<String> queue = new LinkedList<>();
+        queue.offer("0000");
+        visited.add("0000");
+
+        int turns = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String lock = queue.poll();
+                if (lock.equals(target)) {
+                    return turns;
+                }
+                List<String> children = generateChildren(lock);
+                for (String child : children) {
+                    if (!visited.contains(child)) {
+                        visited.add(child);
+                        queue.offer(child);
                     }
                 }
             }
-            ans++;
+            turns++;
         }
         return -1;
+    }
+
+    private List<String> generateChildren(String lock) {
+        List<String> children = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            char[] digits = lock.toCharArray();
+            digits[i] = (char)(((digits[i] - '0' + 1) % 10) + '0');
+            children.add(new String(digits));
+            digits[i] = (char)(((digits[i] - '0' - 2 + 10) % 10) + '0');
+            children.add(new String(digits));
+        }
+        return children;
     }
 }
