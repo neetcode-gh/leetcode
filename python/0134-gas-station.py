54d7609f4c86e24b850cc333ca897ecf8b6d7a6e
snehd-eep <53041298+snehd-eep@users.noreply.github.com>
2024-04-25 02:28:27 +0530

    Gas station fixed

--- python/0134-gas-station.py
+++ python/0134-gas-station.py
@@ -1,6 +1,12 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         start, end = len(gas) - 1, 0
+
+        #What is happening is that we cannot start from the last index if fuel and cost both are 0
+        #So we set the start to index where either of fuel and cost are not 0
+        while(gas[start] == 0 and cost[start]==0):
+            start -= 1
+
         total = gas[start] - cost[start]
 
         while start >= end:
