e514a717c3f37176642edc6bf8f95dcbc8942add
jhunde <jena.kelbessa@gmail.com>
2024-05-01 00:02:11 -0700

    Updated solution for a more readable and easy to follow solution.

--- python/0130-surrounded-regions.py
+++ python/0130-surrounded-regions.py
@@ -1,5 +1,28 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
+        rows, cols = len(board), len(board[0])
+        flag = set()
+
+        def dfs(r,c):
+            if not(r in range(rows) and c in range(cols)) or board[r][c] != 'O' or (r,c) in flag:
+                return
+            flag.add((r,c))
+            return (dfs(r+1, c), dfs(r-1, c), dfs(r, c+1), dfs(r, c-1))
+
+        # traverse through the board
+        for r in range(rows):
+            for c in range(cols):
+                if( (r == 0 or c == 0 or r == rows-1 or c == cols-1) and board[r][c] == 'O'):
+                    dfs(r,c)
+
+        # set all of the 'X's to 'O's
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == 'O' and (r,c) not in flag:
+                    board[r][c] = 'X'
+
+    '''
+    def solve(self, board: List[List[str]]) -> None:
         ROWS, COLS = len(board), len(board[0])
 
         def capture(r, c):
@@ -28,3 +51,4 @@ class Solution:
             for c in range(COLS):
                 if board[r][c] == "T":
                     board[r][c] = "O"
+    '''
