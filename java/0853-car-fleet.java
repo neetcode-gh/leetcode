25a1784eb46ca3e7190adf487d19bc54ad113569
numerous <ivankin1995rus@gmail.com>
2024-07-02 23:00:07 +0200

    feat_0853: car fleet

--- java/0853-car-fleet.java
+++ java/0853-car-fleet.java
@@ -1,24 +1,30 @@
 class Solution {
 
     public int carFleet(int target, int[] position, int[] speed) {
-        if (position.length == 1) return 1;
-        Stack<Double> stack = new Stack<>();
-        int[][] combine = new int[position.length][2];
-        for (int i = 0; i < position.length; i++) {
+        int n = position.length;
+        if (n == 1) {
+            return 1;
+        }
+
+        double[][] combine = new double[n][2];
+        for (int i = 0; i < n; i++) {
             combine[i][0] = position[i];
             combine[i][1] = speed[i];
         }
 
-        Arrays.sort(combine, java.util.Comparator.comparingInt(o -> o[0]));
-        for (int i = combine.length - 1; i >= 0; i--) {
-            double currentTime = (double) (target - combine[i][0]) /
-            combine[i][1];
-            if (!stack.isEmpty() && currentTime <= stack.peek()) {
+        Arrays.sort(combine, (d1, d2) -> Double.compare(d1[0], d2[0]));
+
+        Deque<Double> d = new LinkedList<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            double currentSpeed = (target - combine[i][0]) / combine[i][1];
+            if (!d.isEmpty() && currentSpeed <= d.peek()) {
                 continue;
             } else {
-                stack.push(currentTime);
+                d.push(currentSpeed);
             }
         }
-        return stack.size();
+
+        return d.size();
     }
 }
