9a6cd20ec86e11bd3be8ffb686d609eeb6d5d9fa
aadil42 <77232799+aadil42@users.noreply.github.com>
2024-07-15 17:29:01 +0530

    Create 1921-eliminate-maximum-number-of-monsters.js

---
+++ javascript/1921-eliminate-maximum-number-of-monsters.js
@@ -0,0 +1,23 @@
+
+/**
+ * Greedy.
+ * Time O(n) | Space O(1)
+ * @param {number[]} dist
+ * @param {number[]} speed
+ * @return {number}
+ */
+var eliminateMaximum = function(dist, speed) {
+    
+    const time = dist.map((d, i) => {
+        return d / speed[i];
+    });
+
+    let monsterSlyed = 1;
+    time.sort((a,b) => a-b);
+    for(let i = 1; i < time.length; i++) {
+        if(time[i] <= monsterSlyed) return monsterSlyed;
+        monsterSlyed++;
+    }
+
+    return monsterSlyed;
+};
