79a9e5c615de5778e1ce4d73e5176e37e7bab006
yogeSHarma1386 <yogeshsharma1386@gmail.com>
2024-09-27 18:10:38 +0530

    island code fix

--- python/0200-number-of-islands.py
+++ python/0200-number-of-islands.py
@@ -1,6 +1,6 @@
-from typing import List, Optional
 from collections import deque
-from utils import TreeNode, SLLNode as ListNode
+from typing import List
+
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
@@ -43,28 +43,27 @@ class SolutionBFS:
         visited=set()
         islands=0
 
-         def bfs(r,c):
-             q = deque()
-             visited.add((r,c))
-             q.append((r,c))
+        def bfs(r,c):
+            q = deque()
+            visited.add((r,c))
+            q.append((r,c))
 
-             while q:
-                 row,col = q.popleft()
-                 directions= [[1,0],[-1,0],[0,1],[0,-1]]
+            while q:
+             row,col = q.popleft()
+             directions= [[1,0],[-1,0],[0,1],[0,-1]]
 
-                 for dr,dc in directions:
-                     r,c = row + dr, col + dc
-                     if (r) in range(rows) and (c) in range(cols) and grid[r][c] == '1' and (r ,c) not in visited:
+             for dr,dc in directions:
+                 r,c = row + dr, col + dc
+                 if (r) in range(rows) and (c) in range(cols) and grid[r][c] == '1' and (r ,c) not in visited:
+                     q.append((r , c ))
+                     visited.add((r, c ))
 
-                         q.append((r , c ))
-                         visited.add((r, c ))
+        for r in range(rows):
+            for c in range(cols):
 
-         for r in range(rows):
-             for c in range(cols):
-               
-                 if grid[r][c] == "1" and (r,c) not in visited:
-                     bfs(r,c)
-                     islands +=1 
+                if grid[r][c] == "1" and (r,c) not in visited:
+                    bfs(r,c)
+                    islands +=1
 
-         return islands
+        return islands
 
