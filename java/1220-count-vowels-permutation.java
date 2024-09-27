0bc94bdc6f2ad826440e6db9601bf455ac717b26
yogeSHarma1386 <yogeshsharma1386@gmail.com>
2024-09-27 16:57:17 +0530

    modifier

--- java/1220-count-vowels-permutation.java
+++ java/1220-count-vowels-permutation.java
@@ -39,19 +39,41 @@ class Solution {
     int MOD = (int) 1e9+7; 
 
     public int countVowelPermutation(int n) {
-        long[] counts = getBaseCounts();
-        if(n == 1) {
-            return getSum(counts);
-        }
-        
-        Map<Integer, List<Integer>> mapNextCounting;
-        mapNextCounting = getNextCountMapping();
-    
-        for(int i=1; i<n; i++) {
-            counts = getNextCounts(counts, mapNextCounting);
-        }
-        
-        return getSum(counts);
+        long ans = 0;
+        ans = (ans + dfs('a', n, 1)) % MOD;
+        ans = (ans + dfs('e', n, 1)) % MOD;
+        ans = (ans + dfs('i', n, 1)) % MOD;
+        ans = (ans + dfs('o', n, 1)) % MOD;
+        ans = (ans + dfs('u', n, 1)) % MOD;
+        return (int)ans;
     }
 
+    private int dfs(char c, int n, int l){
+        if(l == n)
+            return 1;
+        
+        String key = c + "_" + l;
+        if (memo.containsKey(key)) return memo.get(key);
+
+        long res = 0;
+        if(c == 'a') {
+            res = dfs('e', n, l+1);
+        } else if(c == 'e') {
+            res = (res + dfs('a', n, l+1)) % MOD;
+            res = (res + dfs('i', n, l+1)) % MOD;
+        } else if(c == 'i') {
+            res = (res + dfs('a', n, l+1)) % MOD;
+            res = (res + dfs('e', n, l+1)) % MOD;
+            res = (res + dfs('o', n, l+1)) % MOD;
+            res = (res + dfs('u', n, l+1)) % MOD;
+        } else if(c == 'o') {
+            res = (res + dfs('i', n, l+1)) % MOD;
+            res = (res + dfs('u', n, l+1)) % MOD;
+        } else {
+            res = dfs('a', n, l+1);
+        }
+
+        memo.put(key, (int)(res % MOD));
+        return (int)(res % MOD);    
+    }
 }
