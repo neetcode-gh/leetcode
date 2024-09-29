3a2f439f957e0833264ac84f6cf504e152080dc0
Yaseen Khan <kyaseen49@yahoo.com>
2024-09-28 21:29:11 -0700

    Fixed Formatting 1094-car-pooling.js

--- javascript/1094-car-pooling.js
+++ javascript/1094-car-pooling.js
@@ -10,19 +10,19 @@ var carPooling = function(trips, capacity) {
     
     const minQ = new MinPriorityQueue({
         compare: (e1, e2) => {
-            return e1[0]  - e2[0];
+            return e1[0] - e2[0];
         }
     });
 
-    trips.sort((a,b) => a[1]-b[1]);
+    trips.sort((a, b) => a[1] - b[1]);
 
-    for(let i = 0; i < trips.length; i++) {
-        while(!minQ.isEmpty() && minQ.front()[0] <= trips[i][1]) {
+    for (let i = 0; i < trips.length; i++) {
+        while (!minQ.isEmpty() && minQ.front()[0] <= trips[i][1]) {
             capacity += minQ.dequeue()[1];
         };
 
         capacity -= trips[i][0];
-        if(capacity < 0) return false;
+        if (capacity < 0) return false;
         minQ.enqueue([trips[i][2], trips[i][0]]);
     }
 
