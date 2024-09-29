5f4082f80057515cce77659428541fda59314977
Yaseen Khan <kyaseen49@yahoo.com>
2024-09-28 22:04:32 -0700

    Fixed formatting 0978-longest-turbulent-subarray.js

--- javascript/0978-longest-turbulent-subarray.js
+++ javascript/0978-longest-turbulent-subarray.js
@@ -12,10 +12,10 @@ var maxTurbulenceSize = function(arr) {
         let i = start;
         let shouldBeLow = true;
 
-        while(i + 1 < arr.length) {
-            if(shouldBeLow && arr[i+1] > arr[i]) break;
-            if(!shouldBeLow && arr[i+1] < arr[i]) break;
-            if(arr[i+1] === arr[i]) break;
+        while (i + 1 < arr.length) {
+            if (shouldBeLow && arr[i + 1] > arr[i]) break;
+            if (!shouldBeLow && arr[i + 1] < arr[i]) break;
+            if (arr[i + 1] === arr[i]) break;
             shouldBeLow = !shouldBeLow;
             i++;
         }
@@ -30,10 +30,10 @@ var maxTurbulenceSize = function(arr) {
         let i = start;
         let shouldBeHigh = true;
 
-        while(i + 1 < arr.length) {
-            if(shouldBeHigh && arr[i+1] < arr[i]) break;
-            if(!shouldBeHigh && arr[i+1] > arr[i]) break;
-            if(arr[i+1] === arr[i]) break;
+        while (i + 1 < arr.length) {
+            if (shouldBeHigh && arr[i + 1] < arr[i]) break;
+            if (!shouldBeHigh && arr[i + 1] > arr[i]) break;
+            if (arr[i + 1] === arr[i]) break;
             shouldBeHigh = !shouldBeHigh;
             i++;
         }
@@ -46,25 +46,25 @@ var maxTurbulenceSize = function(arr) {
     let right = 1;
     let max = 1;
 
-    while(right < arr.length) {
+    while (right < arr.length) {
 
-        if(arr[left] > arr[right]) {
+        if (arr[left] > arr[right]) {
             right = higherAndLower(left);
-            max = Math.max(right-left+1, max);
+            max = Math.max(right - left + 1, max);
             left = right;
-            right = right+1;
+            right = right + 1;
             continue;
         }
 
-        if(arr[left] < arr[right]) {
+        if (arr[left] < arr[right]) {
             right = lowerAndHigher(left);
-            max = Math.max(right-left+1, max);
+            max = Math.max(right - left + 1, max);
             left = right;
-            right = right+1;
+            right = right + 1;
             continue;
         }
 
-        if(arr[left] === arr[right]) {
+        if (arr[left] === arr[right]) {
             left++;
             right++;
         }
