139261ee5f084f727373d3b49a446c273f95569f
Yaseen Khan <kyaseen49@yahoo.com>
2024-09-29 09:17:13 -0700

    Fixed Formatting 0130-surrounded-regions.cpp

--- cpp/0130-surrounded-regions.cpp
+++ cpp/0130-surrounded-regions.cpp
@@ -65,13 +65,13 @@ private:
         q.push({row, col});
 
         vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-        while(!q.empty()) {
+        while (!q.empty()) {
             auto [r, c] = q.front();
             q.pop();
-            for(const auto &direction : directions) {
+            for (const auto &direction : directions) {
                 int newRow = r + direction.first;
                 int newCol = c + direction.second;
-                if(newRow < rows && newRow >= 0 && newCol < cols && newCol >= 0 && board[newRow][newCol] == 'O') {
+                if (newRow < rows && newRow >= 0 && newCol < cols && newCol >= 0 && board[newRow][newCol] == 'O') {
                     board[newRow][newCol] = 'E';
                     q.push({newRow, newCol});
                 }
@@ -85,22 +85,22 @@ public:
         rows = board.size();    
         cols = board[0].size();
 
-        for(int row = 0; row < rows; ++row) {
-            if(board[row][0] == 'O') bfs(row, 0, board);
-            if(board[row][cols - 1] == 'O') bfs(row, cols - 1, board);
+        for (int row = 0; row < rows; ++row) {
+            if (board[row][0] == 'O') bfs(row, 0, board);
+            if (board[row][cols - 1] == 'O') bfs(row, cols - 1, board);
         }
 
-        for(int col = 0; col < cols; ++col) {
-            if(board[0][col] == 'O') bfs(0, col, board);
-            if(board[rows - 1][col] == 'O') bfs(rows - 1, col, board);
+        for (int col = 0; col < cols; ++col) {
+            if (board[0][col] == 'O') bfs(0, col, board);
+            if (board[rows - 1][col] == 'O') bfs(rows - 1, col, board);
         }
 
-        for(int row = 0; row < rows; ++row) {
-            for(int col = 0; col < cols; ++col) {
-                if(board[row][col] == 'O') {
+        for (int row = 0; row < rows; ++row) {
+            for (int col = 0; col < cols; ++col) {
+                if (board[row][col] == 'O') {
                     board[row][col] = 'X';
                 }
-                else if(board[row][col] == 'E') {
+                else if (board[row][col] == 'E') {
                     board[row][col] = 'O';
                 }
             }
