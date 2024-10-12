5b2d718bb254a00f3ba03683a865ab88f2b52777
Yaseen Khan <kyaseen49@yahoo.com>
2024-10-11 20:44:26 -0700

    Added space

--- go/0219-contains-duplicate-ii.go
+++ go/0219-contains-duplicate-ii.go
@@ -2,7 +2,7 @@ func containsNearbyDuplicate(nums []int, k int) bool {
 	seen := make(map[int]int)
 
 	for i, n := range nums {
-		if j, ok := seen[n]; ok && i-j <= k {
+		if j, ok := seen[n]; ok && i - j <= k {
 			return true
 		}
 		seen[n] = i
