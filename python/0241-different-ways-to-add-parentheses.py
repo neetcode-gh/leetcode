2f62b001b0a73433cf20f82ba6bb7ba27647bdb9
Yaseen Khan <kyaseen49@yahoo.com>
2024-10-13 10:19:57 -0700

    Added Spaces

--- python/0241-different-ways-to-add-parentheses.py
+++ python/0241-different-ways-to-add-parentheses.py
@@ -2,10 +2,10 @@ class Solution:
     def diffWaysToCompute(self, s: str) -> List[int]:
         def f(s):
             res = []
-            for i,c in enumerate(s):
+            for i, c in enumerate(s):
                 if c in '+-*':
                     for l in f(s[:i]):
-                        for r in f(s[i+1:]):
+                        for r in f(s[i + 1:]):
                             res.append(eval(f'{l}{c}{r}'))
             
             return res or [int(s)]
