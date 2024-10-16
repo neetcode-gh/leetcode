b6b0090bc41f7d35a8a8e69e1f6bb23c6a8a5546
Yaseen Khan <kyaseen49@yahoo.com>
2024-10-16 06:43:08 -0700

    Fix spacing

--- javascript/0135-candy.js
+++ javascript/0135-candy.js
@@ -11,20 +11,20 @@ var candy = function(ratings) {
     const rtl = new Array(ratings.length).fill(1);
 
     // go from left to right
-    for(let i = 1; i < ratings.length; i++) {
-      if(ratings[i] > ratings[i-1]) {
-        ltr[i] = ltr[i-1] + 1;
+    for (let i = 1; i < ratings.length; i++) {
+      if (ratings[i] > ratings[i - 1]) {
+        ltr[i] = ltr[i - 1] + 1;
       }
     }
     // go from right to left
-    for(let i = ratings.length - 2; i > -1; i--) {
-      if(ratings[i] > ratings[i+1]) {
-        rtl[i] = rtl[i+1] + 1;
+    for (let i = ratings.length - 2; i > -1; i--) {
+      if (ratings[i] > ratings[i + 1]) {
+        rtl[i] = rtl[i + 1] + 1;
       }
     }
     // calc minimum 
     let candy = 0;
-    for(let i = 0; i < ratings.length;  i++) {
+    for (let i = 0; i < ratings.length; i++) {
       candy += Math.max(ltr[i], rtl[i]);
     }
     return candy;
