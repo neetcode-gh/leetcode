f6b85847d6ce4411350a8d21472d2e1380fa4438
Yaseen Khan <kyaseen49@yahoo.com>
2024-10-15 19:32:51 -0700

    Add Spacing

--- javascript/0145-binary-tree-postorder-traversal.js
+++ javascript/0145-binary-tree-postorder-traversal.js
@@ -16,7 +16,7 @@
 var postorderTraversal = function(root) {
     
     const dfs = (node, pot) => {
-        if(!node) return pot;
+        if (!node) return pot;
         dfs(node.left, pot);
         dfs(node.right, pot);
         pot.push(node.val);
