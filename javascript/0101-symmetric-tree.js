09c2620d498958ee1d2ae7544008d3e3a52fe56f
aadil42 <77232799+aadil42@users.noreply.github.com>
2024-10-18 22:47:34 +0530

    Update 0101-symmetric-tree.js. Add space after control flow statement.

--- javascript/0101-symmetric-tree.js
+++ javascript/0101-symmetric-tree.js
@@ -14,17 +14,16 @@
  * @return {boolean}
  */
 var isSymmetric = function(root) {
-    
-    const dfs = (node1, node2) => {
-
-        if(!node1 && !node2) return true;
-
-        if(node1 && !node2) return false;
-        if(!node1 && node2) return false;
-        if(node1.val !== node2.val) return false;
-
-        return dfs(node1.right, node2.left) && dfs(node1.left, node2.right);
-    }
-
     return dfs(root.left, root.right);
 };
+    
+const dfs = (node1, node2) => {
+
+    if (!node1 && !node2) return true;
+
+    if (node1 && !node2) return false;
+    if (!node1 && node2) return false;
+    if (node1.val !== node2.val) return false;
+
+    return dfs(node1.right, node2.left) && dfs(node1.left, node2.right);
+}
