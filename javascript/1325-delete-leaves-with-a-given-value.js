88769c6388348ee103acf4df4e1bbe7e2eee7ea2
aadil42 <77232799+aadil42@users.noreply.github.com>
2024-10-18 09:05:03 +0530

    Add spaces after control flow statements.
    Adding spaces.

--- javascript/1325-delete-leaves-with-a-given-value.js
+++ javascript/1325-delete-leaves-with-a-given-value.js
@@ -17,11 +17,11 @@
 var removeLeafNodes = function(root, target) {
     
     const dfs = (node) => {
-        if(!node) return null;
+        if (!node) return null;
         node.left = dfs(node.left);
         node.right = dfs(node.right);
-        if(!node.left && !node.right) {
-            if(node.val === target) return null;
+        if (!node.left && !node.right) {
+            if (node.val === target) return null;
         }
         return node;
     }
