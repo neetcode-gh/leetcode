8e164e97d32a0b3d8238cb4f0718ab314c434cb6
aadil42 <77232799+aadil42@users.noreply.github.com>
2024-10-18 22:18:44 +0530

    Add spaces after control flow statements.

--- javascript/0450-delete-node-in-a-bst.js
+++ javascript/0450-delete-node-in-a-bst.js
@@ -15,26 +15,28 @@
  * @return {TreeNode}
  */
 var deleteNode = function(root, key) {
-    if(!root) return root;
+    if (!root) return root;
 
-    if(key === root.val) {
-        if(!root.left) return root.right;
-        if(!root.right) return root.left;
+    if (key === root.val) {
+        if (!root.left) return root.right;
+        if (!root.right) return root.left;
 
         // find the smallest val in right bst
         let curr = root.right;
-        while(curr.left) {
+        while (curr.left) {
             curr = curr.left;
         }
         // change the curr value
         root.val = curr.val;
 
         root.right = deleteNode(root.right, root.val);
-    } else if(key < root.val) {
-        root.left = deleteNode(root.left, key);
-    } else {
-        root.right = deleteNode(root.right, key);
-    }
 
+        return root;
+    } 
+    if (key < root.val) {
+        root.left = deleteNode(root.left, key);
+        return root;
+    }
+    root.right = deleteNode(root.right, key);
     return root;
 };
