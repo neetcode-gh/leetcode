6315c299fadf5aac84873c27231e173075d0e577
Yaseen Khan <kyaseen49@yahoo.com>
2024-10-19 06:42:46 -0700

    Added spacing

--- python/0791-custom-sort-string.py
+++ python/0791-custom-sort-string.py
@@ -2,7 +2,7 @@ class Solution:
     def customSortString(self, order: str, s: str) -> str:
         char_count_of_s = {}
         for i in s:
-            char_count_of_s[i] = char_count_of_s.get(i,0) + 1
+            char_count_of_s[i] = char_count_of_s.get(i, 0) + 1
         
         satisfied_string = ""
         for char in order:
@@ -11,6 +11,6 @@ class Solution:
                 del char_count_of_s[char]
         
         for key,val in char_count_of_s.items():
-            satisfied_string += key*val
+            satisfied_string += key * val
 
         return satisfied_string
