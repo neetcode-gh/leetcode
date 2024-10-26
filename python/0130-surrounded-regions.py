eb39b922e7f265a9d5981a290407f30d20ce01fd
Yaseen Khan <kyaseen49@yahoo.com>
2024-10-25 17:17:23 -0700

    Adjusted Spacing

--- python/0130-surrounded-regions.py
+++ python/0130-surrounded-regions.py
@@ -3,22 +3,22 @@ class Solution:
         rows, cols = len(board), len(board[0])
         flag = set()
 
-        def dfs(r,c):
-            if not(r in range(rows) and c in range(cols)) or board[r][c] != 'O' or (r,c) in flag:
+        def dfs(r, c):
+            if not(r in range(rows) and c in range(cols)) or board[r][c] != 'O' or (r, c) in flag:
                 return
-            flag.add((r,c))
-            return (dfs(r+1, c), dfs(r-1, c), dfs(r, c+1), dfs(r, c-1))
+            flag.add((r, c))
+            return (dfs(r + 1, c), dfs(r - 1, c), dfs(r, c + 1), dfs(r, c - 1))
 
         # traverse through the board
         for r in range(rows):
             for c in range(cols):
-                if( (r == 0 or c == 0 or r == rows-1 or c == cols-1) and board[r][c] == 'O'):
-                    dfs(r,c)
+                if( (r == 0 or c == 0 or r == rows - 1 or c == cols - 1) and board[r][c] == 'O'):
+                    dfs(r, c)
 
         # set all of the 'X's to 'O's
         for r in range(rows):
             for c in range(cols):
-                if board[r][c] == 'O' and (r,c) not in flag:
+                if board[r][c] == 'O' and (r, c) not in flag:
                     board[r][c] = 'X'
 
     '''
