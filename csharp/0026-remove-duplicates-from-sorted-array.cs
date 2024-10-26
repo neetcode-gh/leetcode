1ff16a3c4a6266e44a56f54934517568c7ceaa63
Yaseen Khan <kyaseen49@yahoo.com>
2024-10-25 18:00:20 -0700

    Added Space

--- csharp/0026-remove-duplicates-from-sorted-array.cs
+++ csharp/0026-remove-duplicates-from-sorted-array.cs
@@ -2,7 +2,7 @@ public class Solution {
     public int RemoveDuplicates(int[] nums) {
         int l = 1, r = 1;
         while (r < nums.Length) {
-            if (nums[r] != nums[r-1]) {
+            if (nums[r] != nums[r - 1]) {
                 nums[l] = nums[r];
                 ++l;
             }
