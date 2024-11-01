d689a1204b7b9beedd0df42fc35fa928aa400502
Yaseen Khan <kyaseen49@yahoo.com>
2024-11-01 02:54:55 -0700

    Adjust spacing

--- python/0016-3sum-closest.py
+++ python/0016-3sum-closest.py
@@ -4,23 +4,23 @@ class Solution:
         nums.sort()
         best = float('inf')
         
-        for i in range(len(nums)-2):
+        for i in range(len(nums) - 2):
             
             val = nums[i]
-            left = i+1
-            right = len(nums)-1
+            left = i + 1
+            right = len(nums) - 1
             
             while left < right:
                 
-                currentGap = abs(target- (val + nums[left] + nums[right]))
+                currentGap = abs(target - (val + nums[left] + nums[right]))
                 
                 if abs(best - target) > currentGap:
                     best = val + nums[left] + nums[right]
                 if val + nums[left] + nums[right] < target:
-                    left+=1
+                    left += 1
                 elif val + nums[left] + nums[right] > target:
-                    right-=1
-                else:#closest it can get 
+                    right -= 1
+                else: #closest it can get 
                     return target                
                 
         return best
