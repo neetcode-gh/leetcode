f01ac5bcf79781e68c688309f4c36d66faae4bab
Yaseen Khan <kyaseen49@yahoo.com>
2024-10-31 21:20:19 -0700

    Fixed Spacing

--- cpp/1249-minimum-remove-to-make-valid parentheses.cpp
+++ cpp/1249-minimum-remove-to-make-valid parentheses.cpp
@@ -31,20 +31,20 @@ public:
     string minRemoveToMakeValid(string s) {
         stack<pair<char,int>> st;
         
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='('){
-                st.push(make_pair(s[i],i));
-            }else if(s[i]==')'){
-                if(!st.empty() and st.top().first=='('){
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                st.push(make_pair(s[i], i));
+            } else if (s[i] == ')') {
+                if (!st.empty() and st.top().first == '(') {
                     st.pop();                
-                }else{
-                    st.push(make_pair(')',i));
+                } else {
+                    st.push(make_pair(')', i));
                 }
             }
         }
 
-        while(!st.empty()){
-            s.erase(s.begin()+st.top().second);
+        while (!st.empty()) {
+            s.erase(s.begin() + st.top().second);
             st.pop();
         }
 
