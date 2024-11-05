1ff11164403199397d13d8f3c00e1c515f99c166
David M. <davidmedinagodoy@gmail.com>
2024-11-05 08:20:17 +0100

    Update 0280-wiggle-sort.java to fix spacing

--- java/0280-wiggle-sort.java
+++ java/0280-wiggle-sort.java
@@ -1,12 +1,12 @@
 class Solution {
     public void wiggleSort(int[] nums) {
-        for(int i = 1; i < nums.length; i++){
-            if((i % 2 == 0 && nums[i] > nums[i - 1]) 
+        for (int i = 1; i < nums.length; i++) {
+            if ((i % 2 == 0 && nums[i] > nums[i - 1]) 
             || (i % 2 == 1 && nums[i] < nums[i - 1]))
                 swap(nums, i, i - 1);
         }
     }
-    private void swap(int[] nums, int i, int j){
+    private void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
