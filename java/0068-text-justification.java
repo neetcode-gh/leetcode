c11fa52f0aa9537a958ad69744881fd7ee1f29f4
David M. <davidmedinagodoy@gmail.com>
2024-11-05 08:34:22 +0100

    Update 0068-text-justification.java to fix spacing

--- java/0068-text-justification.java
+++ java/0068-text-justification.java
@@ -3,11 +3,11 @@ class Solution {
         int count = 0;
         List<String> currLine = new ArrayList<>();
         List<String> ans = new ArrayList<>();
-        for(String w : words){
-            if(count == 0){
+        for (String w : words) {
+            if (count == 0) {
                 count += w.length();
                 currLine.add(w);
-            } else if(count + 1 + w.length() > maxWidth){
+            } else if (count + 1 + w.length() > maxWidth) {
                 ans.add(getLine(currLine, count, maxWidth));
                 currLine = new ArrayList<>(List.of(w));
                 count = w.length();
@@ -17,24 +17,26 @@ class Solution {
             }
         }
         ans.add(getLastLine(currLine, maxWidth));
-        return ans;        
+        return ans;
     }
-    private String getLine(List<String> words, int count, int maxWidth){
-        if(words.size() == 0) return "";
-        if(words.size() == 1) return words.get(0) + " ".repeat(maxWidth - words.get(0).length());
+
+    private String getLine(List<String> words, int count, int maxWidth) {
+        if (words.size() == 0) return "";
+        if (words.size() == 1) return words.get(0) + " ".repeat(maxWidth - words.get(0).length());
         int extraSpaces = (maxWidth - count) / (words.size() - 1);
         int remSpaces = (maxWidth - count) % (words.size() - 1);
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < words.size() - 1; i++){
+        for (int i = 0; i < words.size() - 1; i++) {
             sb.append(words.get(i)).append(" ");
             sb.append(" ".repeat(extraSpaces));
-            if(remSpaces-- > 0)
+            if (remSpaces-- > 0)
                 sb.append(" ");
         }
         sb.append(words.getLast());
         return sb.toString();
     }
-    private String getLastLine(List<String> words, int maxWidth){
+
+    private String getLastLine(List<String> words, int maxWidth) {
         String wordsStr = String.join(" ", words);
         return wordsStr.toString() + " ".repeat(maxWidth - wordsStr.length());
     }
