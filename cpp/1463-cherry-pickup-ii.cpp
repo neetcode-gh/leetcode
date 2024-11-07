6c92efe64c8f86620f5261ea02b87e863e8b7f72
Yaseen Khan <kyaseen49@yahoo.com>
2024-11-06 21:18:16 -0700

    Fixed file name and removed question description

--- cpp/1463-cherry-pickup-ii.cpp
+++ cpp/1463-cherry-pickup-ii.cpp
@@ -1,23 +1,3 @@
-//   https://leetcode.com/problems/cherry-pickup-ii/
-
-
-// You are given a rows x cols matrix grid representing a field of cherries where grid[i][j] represents the number of cherries that you can collect from the (i, j) cell.
-
-// You have two robots that can collect cherries for you:
-
-// Robot #1 is located at the top-left corner (0, 0), and
-// Robot #2 is located at the top-right corner (0, cols - 1).
-// Return the maximum number of cherries collection using both robots by following the rules below:
-
-// From a cell (i, j), robots can move to cell (i + 1, j - 1), (i + 1, j), or (i + 1, j + 1).
-// When any robot passes through a cell, It picks up all cherries, and the cell becomes an empty cell.
-// When both robots stay in the same cell, only one takes the cherries.
-// Both robots cannot move outside of the grid at any moment.
-// Both robots should reach the bottom row in grid.
-
-
-Solution:
-
 #include <vector>
 #include <algorithm>
 
