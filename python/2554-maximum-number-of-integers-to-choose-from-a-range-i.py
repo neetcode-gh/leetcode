66b72f39862933045dc16ff978e1cbbddd2dd942
Yaseen Khan <kyaseen49@yahoo.com>
2024-11-09 13:39:57 -0700

    Fixed name of file

--- python/2554-maximum-number-of-integers-to-choose-from-a-range-i.py
+++ python/2554-maximum-number-of-integers-to-choose-from-a-range-i.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxCount(self, banned: List[int], n: int, maxSum: int) -> int:
-        nums = {x:1 for x in range(1,n+1)} # hashmap for storing the required elements
+        nums = {x:1 for x in range(1, n + 1)} # hashmap for storing the required elements
         for i in banned:
             if nums.get(i):
                 del nums[i]
