df698ce7597664e5b89181730f375d32c6022f4b
aadil42 <77232799+aadil42@users.noreply.github.com>
2025-01-03 13:45:25 +0530

    Update 0077-combinations.js
    Add another solution with different time complexity.

--- javascript/0077-combinations.js
+++ javascript/0077-combinations.js
@@ -25,3 +25,37 @@ var combine = function(n, k) {
 
     return dfs(1, 0, [], []);
 };
+
+
+/**
+ * Solution #2
+ * BackTracking | Recursion
+ * Time O(n^k) | Space O((n!)/(n-k)!*k!) (Because we'll nedd the number of combinations space to store them in the array)
+ * https://leetcode.com/problems/combinations/
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine1 = function(n, k) {
+    
+    const dfs = (num, comb, allComb) => {
+        if (comb.length === k) {
+            allComb.push(comb);
+            return allComb;
+        }
+        
+        if (num === n+1) return allComb; // this if condition is written here because what if we are at num === n+1
+        // and we have taken valid combo? Instead of adding in the above condition it would have written
+        // immidiatly so we write this if condition after we've taken the combo. Hope it makes sence.
+
+        const comb1 = [num, ...comb]; // take the num here
+        const comb2 = [...comb]; // don't take the num
+        dfs(num+1, comb1, allComb);
+        dfs(num+1, comb2, allComb);
+        
+        return allComb;
+    }
+    
+    return dfs(1, [], []);
+};
+
