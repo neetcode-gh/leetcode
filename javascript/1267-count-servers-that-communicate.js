eda25e1b11f0b330e2c9f77c5318f6d51140ab5c
aadil42 <77232799+aadil42@users.noreply.github.com>
2025-01-23 09:06:14 +0530

    Update 1267-count-servers-that-communicate.js
    Added optimal solution with Hashmap with Time complexity of O(n*m) and Space complexity of O(n*m)

--- javascript/1267-count-servers-that-communicate.js
+++ javascript/1267-count-servers-that-communicate.js
@@ -1,11 +1,44 @@
 /**
- * 2D Matrix 
- * Time O((m*n) * (m+n)) | Space O(1)
+ * 2D Matrix | HashMap
+ * Time O((m*n)) | Space O(n*m)
  * https://leetcode.com/problems/count-servers-that-communicate/
  * @param {number[][]} grid
  * @return {number}
  */
 var countServers = function(grid) {
+    
+    const ROW = grid.length;
+    const COL = grid[0].length;
+    const serverRowHash = {};
+    const serverColHash = {};
+
+    for (let r = 0; r < ROW; r++) {
+        for (let c = 0; c < COL; c++) {
+            if (grid[r][c]) {
+                serverRowHash[r] = (serverRowHash[r] && serverRowHash[r] + 1) || 1;
+                serverColHash[c] = (serverColHash[c] && serverColHash[c] + 1) || 1;
+            }
+        }
+    }
+
+    let count = 0;
+    for (let r = 0; r < ROW; r++) {
+        for (let c = 0; c < COL; c++) {
+            if (grid[r][c] && (serverRowHash[r] > 1 || serverColHash[c] > 1) ) count++; 
+        }
+    }
+
+    return count;
+};
+
+/**
+ * 2D Matrix | Brute Force
+ * Time O((m*n) * (m+n)) | Space O(1)
+ * https://leetcode.com/problems/count-servers-that-communicate/
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var countServersBF = function(grid) {
     
     const ROW = grid.length;
     const COL = grid[0].length;
